450b2ef0232c0ed7a8d235f956715c934a273178
Jaco Greeff <jacogr@gmail.com>
2017-11-28 15:43:32 +0100

    .eslintrc to root

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,8 @@ const config = require('@polkadot/dev/jest.config');
 
 module.exports = Object.assign({}, config, {
   moduleNameMapper: {
-    '@polkadot/api-jsonrpc(.*)$': '<rootDir>/packages/api-jsonrpc/src/$1',
     '@polkadot/api-format(.*)$': '<rootDir>/packages/api-format/src/$1',
+    '@polkadot/api-jsonrpc(.*)$': '<rootDir>/packages/api-jsonrpc/src/$1',
     '@polkadot/api-provider(.*)$': '<rootDir>/packages/api-provider/src/$1'
   }
 });
--- packages/api/jest.config.js
+++ packages/api/jest.config.js
@@ -2,8 +2,8 @@ const config = require('@polkadot/dev/jest.config');
 
 module.exports = Object.assign({}, config, {
   moduleNameMapper: {
-    '@polkadot/api-jsonrpc(.*)$': '<rootDir>/../api-jsonrpc/src/$1',
     '@polkadot/api-format(.*)$': '<rootDir>/../api-format/src/$1',
+    '@polkadot/api-jsonrpc(.*)$': '<rootDir>/../api-jsonrpc/src/$1',
     '@polkadot/api-provider(.*)$': '<rootDir>/../api-provider/src/$1'
   }
 });
