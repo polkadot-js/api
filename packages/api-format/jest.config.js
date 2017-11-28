19229af713d22fd233b704a22bbbfeac121d90e9
Jaco Greeff <jacogr@gmail.com>
2017-11-28 16:13:00 +0100

    Cleanups, build from root

--- packages/api-format/jest.config.js
+++
@@ -1,7 +0,0 @@
-const config = require('@polkadot/dev/jest.config');
-
-module.exports = Object.assign({}, config, {
-  moduleNameMapper: {
-    '@polkadot/api-jsonrpc(.*)$': '<rootDir>/../api-jsonrpc/src/$1'
-  }
-});
--- packages/api/jest.config.js
+++
@@ -1,9 +0,0 @@
-const config = require('@polkadot/dev/jest.config');
-
-module.exports = Object.assign({}, config, {
-  moduleNameMapper: {
-    '@polkadot/api-format(.*)$': '<rootDir>/../api-format/src/$1',
-    '@polkadot/api-jsonrpc(.*)$': '<rootDir>/../api-jsonrpc/src/$1',
-    '@polkadot/api-provider(.*)$': '<rootDir>/../api-provider/src/$1'
-  }
-});
