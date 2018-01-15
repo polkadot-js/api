66ae02ee5dfda5034482c6acdd735b7b6fb04f56
Jaco Greeff <jacogr@gmail.com>
2018-01-15 13:51:52 +0100

    Expand license header

--- packages/api-provider/test/mockWs.js
+++ packages/api-provider/test/mockWs.js
@@ -1,4 +1,6 @@
-// ISC, Copyright 2017-2018 Jaco Greeff
+// Copyright 2017-2018 Jaco Greeff
+// This software may be modified and distributed under the terms
+// of the ISC license. See the LICENSE file for details.
 
 const { Server } = require('mock-socket');
 
