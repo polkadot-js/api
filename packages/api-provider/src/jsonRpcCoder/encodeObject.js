02f4bb24a59a4d561f379c0486f19c0d48f9dc75
Jaco Greeff <jacogr@gmail.com>
2018-03-15 09:17:39 +0100

    Functional JsonRpcCoder

---
+++ packages/api-provider/src/jsonRpcCoder/encodeObject.js
@@ -0,0 +1,16 @@
+// Copyright 2017-2018 Jaco Greeff
+// This software may be modified and distributed under the terms
+// of the ISC license. See the LICENSE file for details.
+// @flow
+
+import type { JsonRpcRequest } from '../types';
+import type { RpcCoderState } from './types';
+
+module.exports = function encodeObject (self: RpcCoderState, method: string, params: Array<mixed>): JsonRpcRequest {
+  return {
+    id: ++self.id,
+    jsonrpc: '2.0',
+    method,
+    params
+  };
+};
---
+++ packages/api-provider/src/jsonRpcCoder/types.js
@@ -0,0 +1,14 @@
+// Copyright 2017-2018 Jaco Greeff
+// This software may be modified and distributed under the terms
+// of the ISC license. See the LICENSE file for details.
+// @flow
+
+export type RpcCoder = {
+  decodeResponse: (response: JsonRpcResponse) => mixed,
+  encodeObject: (method: string, params: Array<mixed>) => JsonRpcRequest,
+  encodeJson: (method: string, params: Array<mixed>) => string
+};
+
+export type RpcCoderState = {
+  id: number
+};
