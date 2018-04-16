73ab21224119da6e03d0ed59cc4b6ffdcca45d2f
Jaco Greeff <jacogr@gmail.com>
2018-04-16 18:02:04 +0200

    Update primitives

--- packages/api-format/src/output.spec.js
+++ packages/api-format/src/output.spec.js
@@ -11,13 +11,13 @@ describe('formatOutput', () => {
     expect(
       JSON.stringify(
         formatOutput({ type: 'Header' }, {
-          parentHash: '0x1234',
-          number: '0x1234',
-          stateRoot: '0x5678',
-          transactionRoot: '0xabcd',
           digest: {
             logs: ['0x5678', '0x789a']
-          }
+          },
+          extrinsicsRoot: '0xabcd',
+          number: '0x1234',
+          parentHash: '0x1234',
+          stateRoot: '0x5678'
         })
       )
     ).toEqual(
@@ -28,10 +28,10 @@ describe('formatOutput', () => {
             new Uint8Array([0x78, 0x9a])
           ]
         },
+        extrinsicsRoot: new Uint8Array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0xab, 0xcd]),
         number: new BN(0x1234),
         parentHash: new Uint8Array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x12, 0x34]),
-        stateRoot: new Uint8Array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x56, 0x78]),
-        transactionRoot: new Uint8Array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0xab, 0xcd])
+        stateRoot: new Uint8Array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x56, 0x78])
       })
     );
   });
