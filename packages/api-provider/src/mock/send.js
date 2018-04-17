23649b47127d5a3d2e41f820a7171311195d6f15
Jaco Greeff <jacogr@gmail.com>
2018-04-17 12:09:25 +0200

    state_getStorage mock

--- packages/api-provider/src/mock/send.js
+++ packages/api-provider/src/mock/send.js
@@ -5,6 +5,10 @@
 
 import type { MockState } from './types';
 
-module.exports = async function send (self: MockState, method: string, params: Array<mixed>): Promise<mixed> {
-  throw new Error('provider.send not implemented');
+module.exports = async function send ({ requests, storage }: MockState, method: string, params: Array<mixed>): Promise<mixed> {
+  if (!requests[method]) {
+    throw new Error(`provider.send: Invalid method '${method}'`);
+  }
+
+  return requests[method](storage, params);
 };
