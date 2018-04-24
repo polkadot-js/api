c7190274ecdc6c88314df985d9d0732bad26592e
Jaco Greeff <jacogr@gmail.com>
2018-04-24 10:47:38 +0200

    Revert isConnected & .on exposure

--- packages/api/src/index.js
+++ packages/api/src/index.js
@@ -16,10 +16,7 @@ const createInterface = require('./create/interface');
 module.exports = function api (provider: ProviderInterface): ApiInterface {
   assert(provider && isFunction(provider.send), 'Expected Provider to API create');
 
-  const exposed: ApiInterface = {
-    isConnected: provider.isConnected,
-    on: provider.on
-  };
+  const exposed: ApiInterface = {};
 
   return Object
     .keys(interfaces)
