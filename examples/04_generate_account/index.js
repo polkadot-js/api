8fa8c9138fef7aecf5da155609b16ea821a2216b
Will <w.kopp@kigroup.de>
2018-09-12 12:54:02 +0200

    Cleanup examples

--- examples/04_generate_account/index.js
+++ examples/04_generate_account/index.js
@@ -1,10 +1,9 @@
+#!/usr/bin/env node
+
 const createKeyring = require('@polkadot/util-keyring').default;
+const u8aToHex = require('@polkadot/util/u8a/toHex').default;
 const Encoder = new TextEncoder(); // always utf-8
 const crypto = require('crypto');
-
-function buf2hex(buffer) {
-  return '0x' + Array.prototype.map.call(new Uint8Array(buffer), x => ('00' + x.toString(16)).slice(-2)).join('');
-}
 
 // A fixed seed from an array
 const SEED1 = new Uint8Array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 0, 0]);
@@ -12,24 +11,19 @@ const SEED1 = new Uint8Array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
 // A fixed seed from a 32 chars string
 const SEED2 = Encoder.encode("correct horse battery and staple")
 
-const msg = Encoder.encode("I am your father")
-
 const keyring = createKeyring();
 const pair1 = keyring.addFromSeed(SEED1);
 const pair2 = keyring.addFromSeed(SEED2);
 
-console.log(pair1.publicKey());
-console.log('Address 1\t', pair1.address(), '\tSeed: ' + buf2hex(SEED1));
-console.log('Address 2\t', pair2.address(), '\tSeed: ' + buf2hex(SEED2));
+console.log('Deterministic addresses (same for each run):');
+console.log(`Address 1\t ${pair1.address()}\t Seed: ${u8aToHex(SEED1)}`);
+console.log(`Address 2\t ${pair2.address()}\t Seed: ${u8aToHex(SEED2)}`);
 
 // A random seed
 const SEED = new Uint8Array(32);
-console.log('Random Addresses:');
-const NUM=10
-for (var i = 3; i < 3+NUM; i++) {
+console.log('Random Addresses (random for each run):');
+for (var i = 3; i < 13; i++) {
   Buffer.from(crypto.randomFillSync(SEED));
   const pair3 = keyring.addFromSeed(SEED);
-  console.log('Address ' + i + '\t',
-    pair3.address(),
-    '\tSeed: ' + buf2hex(SEED));
+  console.log(`Address ${i}\t ${pair3.address()}\t Seed: ${u8aToHex(SEED)}`);
 }
--- examples/README.md
+++ examples/README.md
@@ -6,6 +6,13 @@ For the following examples, you need a local node. It is usually convenient test
 polkadot --dev
 ```
 
+Some of the examples use the following accounts:
+
+- Alice: `5GoKvZWG5ZPYL1WUovuHW3zJBWBP5eT8CbqjdRY4Q6iMaDtZ`
+- Bob: `5Gw3s7q4QLkSWwknsiPtjujPv3XM4Trxi5d4PgKMMk3gfGTE`
+
+Those accounts are easy to add if you don´t have/see them. The seed of Alice´s account is `Alice` and the seed of Bob is... well you guess...
+
 # Install
 
 From each folder, run:
