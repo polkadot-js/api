892ec3f55dd31103af198841c7b37a1afbca3596
Jaco Greeff <jacogr@gmail.com>
2018-09-25 15:30:00 +0200

    Add RawAddress

---
+++ packages/api-codec/src/RawAddress.ts
@@ -0,0 +1,10 @@
+// Copyright 2017-2018 @polkadot/api-codec authors & contributors
+// This software may be modified and distributed under the terms
+// of the ISC license. See the LICENSE file for details.
+
+import Address from './Address';
+
+// Wrapper for a RawAddress.
+// NOTE Not 100% on the differences between this and Address in the runtime
+export default class RawAddress extends Address {
+}
