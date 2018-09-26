6574619c47b2049eeb54d6afd0bbda1698246cc1
Jaco Greeff <jacogr@gmail.com>
2018-09-26 15:43:40 +0200

    Add Moment

---
+++ packages/api-codec/src/Moment.ts
@@ -0,0 +1,70 @@
+// Copyright 2017-2018 @polkadot/api-codec authors & contributors
+// This software may be modified and distributed under the terms
+// of the ISC license. See the LICENSE file for details.
+
+import BN from 'bn.js';
+import isHex from '@polkadot/util/is/hex';
+import bnToBn from '@polkadot/util/bn/toBn';
+import bnToU8a from '@polkadot/util/bn/toU8a';
+import hexToBn from '@polkadot/util/hex/toBn';
+import u8aToBn from '@polkadot/util/u8a/toBn';
+
+import Base from './codec/Base';
+
+const BITLENGTH = 64;
+
+// A wrapper around seconds/timestamps
+export default class Moment extends Base<Date> {
+  // NOTE Unlike normal date constructors, the value here is provided as a per-second value.
+  // For any encoding/decoding the 1000 multiplier would be applied to get it in line with
+  // JavaScript formats
+  constructor (value: Moment | Date | number = 0) {
+    super(
+      value instanceof Date
+        ? new Date(Math.ceil(value.getTime() / 1000) * 1000)
+        : Moment.decode(value)
+    );
+  }
+
+  static decode (value: Moment | number | BN): Date {
+    return value instanceof Moment
+      ? value.raw
+      : new Date(
+        bnToBn(value).toNumber() * 1000
+      );
+  }
+
+  byteLength (): number {
+    return BITLENGTH / 8;
+  }
+
+  fromJSON (input: any): Moment {
+    this.raw = Moment.decode(input);
+
+    return this;
+  }
+
+  fromU8a (input: Uint8Array): Moment {
+    this.raw = Moment.decode(
+      u8aToBn(input.subarray(0, this.byteLength()), true)
+    );
+
+    return this;
+  }
+
+  toJSON (): any {
+    return this.toNumber();
+  }
+
+  toU8a (): Uint8Array {
+    return bnToU8a(this.toNumber(), BITLENGTH, true);
+  }
+
+  toString (): string {
+    return this.raw.toISOString();
+  }
+
+  toNumber (): number {
+    return Math.ceil(this.raw.getTime() / 1000);
+  }
+}
