377b103eb8a63de42a9930e7549114c48f966be0
Amaury Martiny <amaury.martiny@protonmail.com>
2018-09-26 17:27:47 +0100

    Simplify code

--- packages/type-storage/src/wellKnownKeys.ts
+++ packages/type-storage/src/wellKnownKeys.ts
@@ -6,55 +6,52 @@ import Vector from '@polkadot/api-codec/codec/Vector';
 import { createFunction } from './utils/createFunction';
 
 interface WKKMetadata {
-  documentation: Vector<Text>;
-  name: Text;
-  type: StorageFunctionType;
+  documentation: string;
+  type: string;
 }
 
 // Small helper function to factorize code on this page.
-const createWKKFunction = (prefix: Text | U8a, functionMetadata: WKKMetadata) =>
+const createWKKFunction = (prefix: string, functionMetadata: WKKMetadata) =>
   // TODO The expected 2nd argument is a StorageFunctionMetadata, but we
   // actually only need the fields `documentation` and `type` of it, so in
   // order to not input other fields (byteLength, fromJSON...) we lazily cast
   // as any.
   createFunction(
-    prefix,
-    { ...functionMetadata, modifier: new StorageFunctionModifier().fromJSON(0) } as any,
+    new U8a(prefix),
+    {
+      documentation: new Vector(Text, [functionMetadata.documentation]),
+      modifier: new StorageFunctionModifier().fromJSON(0),
+      type: functionMetadata.type
+    } as any,
     { isUnhashed: true }
-    );
+  );
 
-export const code = createWKKFunction(new U8a(':code'), {
-  documentation: new Vector(Text, ['Wasm code of the runtime.']),
-  name: new Text('code'),
-  type: new StorageFunctionType(0, 'Bytes')
+export const code = createWKKFunction(':code', {
+  documentation: 'Wasm code of the runtime.',
+  type: 'Bytes'
 });
 
-export const heapPages = createWKKFunction(new U8a(':heappages'), {
-  documentation: new Vector(Text, ['Number of wasm linear memory pages required for execution of the runtime.']),
-  name: new Text('heapPages'),
-  type: new StorageFunctionType(0, 'u64')
+export const heapPages = createWKKFunction(':heappages', {
+  documentation: 'Number of wasm linear memory pages required for execution of the runtime.',
+  type: 'u64'
 });
 
-export const authorityCount = createWKKFunction(new U8a(':auth:len'), {
-  documentation: new Vector(Text, ['Number of authorities.']),
-  name: new Text('authorityCount'),
-  type: new StorageFunctionType(0, 'u32')
+export const authorityCount = createWKKFunction(':auth:len', {
+  documentation: 'Number of authorities.',
+  type: 'u32'
 });
 
-export const authorityPrefix = createWKKFunction(new U8a(':auth:'), {
-  documentation: new Vector(Text, ['Prefix under which authorities are storied.']),
-  name: new Text('authorityPrefix'),
-  type: new StorageFunctionType(0, 'u32')
+export const authorityPrefix = createWKKFunction(':auth:', {
+  documentation: 'Prefix under which authorities are storied.',
+  type: 'u32'
 });
 
-export const extrinsicIndex = createWKKFunction(new U8a(':extrinsic_index'), {
-  documentation: new Vector(Text, ['Current extrinsic index (u32) is stored under this key.']),
-  name: new Text('extrinsicIndex'),
-  type: new StorageFunctionType(0, 'u32')
+export const extrinsicIndex = createWKKFunction(':extrinsic_index', {
+  documentation: 'Current extrinsic index (u32) is stored under this key.',
+  type: 'u32'
 });
 
-export const changesTrieConfig = createWKKFunction(new U8a(':changes_trie'), {
-  documentation: new Vector(Text, ['Changes trie configuration is stored under this key.']),
-  name: new Text('changesTrieConfig'),
-  type: new StorageFunctionType(0, 'u32')
+export const changesTrieConfig = createWKKFunction(':changes_trie', {
+  documentation: 'Changes trie configuration is stored under this key.',
+  type: 'u32'
 });
