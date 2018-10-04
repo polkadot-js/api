e151aea0cc2e9697cdc4beca1f03ff83da197bae
Jaco Greeff <jacogr@gmail.com>
2018-10-04 08:25:12 +0200

    Update test for full decoding

--- packages/type-extrinsics/src/fromMetadata.spec.js
+++ packages/type-extrinsics/src/fromMetadata.spec.js
@@ -20,9 +20,12 @@ describe('fromMetadata', () => {
     expect(() => newExtrinsics.balances.setBalance('5C62W7ELLAAfix9LYrcx5smtcffbhvThkM5x7xfMeYXCt72s', 2, 3)).not.toThrow();
   });
 
-  it('should return the correct storage key', () => {
-    expect(newExtrinsics.timestamp.set(10101).toU8a(true)).toEqual(
+  it('should return properly-encodec trsnactions', () => {
+    expect(newExtrinsics.timestamp.set([10101]).toU8a()).toEqual(
       new Uint8Array([
+        // length (encoded)
+        11 << 2,
+        // version, no signature
         1,
         // index
         2, 0,
