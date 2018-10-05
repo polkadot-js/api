1e29b0f27fd81018a5b9969ee86ed029d34de322
Amaury Martiny <amaury.martiny@protonmail.com>
2018-10-05 11:19:09 +0200

    Origin

--- packages/types/src/Origin.ts
+++ packages/types/src/Origin.ts
@@ -2,13 +2,9 @@
 // This software may be modified and distributed under the terms
 // of the ISC license. See the LICENSE file for details.
 
-import Base from './codec/Base';
-
 // Where Origin occurs, it should be ignored, so it should never actually be constructed
-export default class Origin extends Base {
+export default class Origin {
   constructor () {
-    super();
-
     throw new Error('Origin should not be constructed, it is only a placeholder for compatibility');
   }
 }
