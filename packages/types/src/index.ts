17aa24c61f6b6670768aa0fcbdded345d73777b7
Jaco Greeff <jacogr@gmail.com>
2018-10-08 15:57:01 +0200

    Lowercase bool type (#225)
    * Lowercase bool
    
    * bool as Bool (keeping to uppercase naming in files)

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -16,7 +16,8 @@ export { default as Balance } from './Balance';
 // export * from './Bft;
 export { default as Block } from './Block';
 export { default as BlockNumber } from './BlockNumber';
-export { default as Bool } from './Bool';
+// NOTE We are exporting Bool as bool to match with Rust
+export { default as bool } from './Bool';
 export { default as Bytes } from './Bytes';
 export { default as Compact16 } from './Compact16';
 export { default as Compact32 } from './Compact32';
