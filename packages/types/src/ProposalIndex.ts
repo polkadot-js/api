d57d6076d669bcd6d6966b54c3d1f6f86e72775b
Luke Schoen <ltfschoen@gmail.com>
2018-10-10 16:35:26 +0200

    fix: Clarifies difference between PropIndex and ProposalIndex from Issue #226

--- packages/types/src/ProposalIndex.ts
+++ packages/types/src/ProposalIndex.ts
@@ -4,6 +4,6 @@
 
 import U32 from './U32';
 
-// An increasing number that represents a specific proposal in the system
+// An increasing number that represents a specific council proposal index in the system
 export default class ProposalIndex extends U32 {
 }
