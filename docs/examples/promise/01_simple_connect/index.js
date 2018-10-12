2e77f6bfc38e547b06c120c925c1cb0c65d20c0d
Jaco Greeff <jacogr@gmail.com>
2018-10-12 12:50:59 +0200

    Add final round of examples

--- docs/examples/promise/01_simple_connect/index.js
+++ docs/examples/promise/01_simple_connect/index.js
@@ -9,10 +9,14 @@ async function main () {
   // create the API and wait until ready
   const api = await ApiPromise.create(provider);
 
-  // retrieve the chain information (rpc call)
-  const chain = await api.rpc.system.chain();
+  // retrieve the chain & node information information via rpc calls
+  const [chain, nodeName, nodeVersion] = await Promise.all([
+    api.rpc.system.chain(),
+    api.rpc.system.name(),
+    api.rpc.system.version()
+  ]);
 
-  console.log(`You are connected to chain: ${chain}`);
+  console.log(`You are connected to chain ${chain} using ${nodeName} v${nodeVersion}`);
 }
 
 main().catch(console.error).finally(() => process.exit());
---
+++ docs/examples/promise/07_transfer_dots/index.js
@@ -0,0 +1,36 @@
+// Import our API, Keyring and some utility functions
+const { ApiPromise } = require('@polkadot/api');
+const Keyring = require('@polkadot/util-keyring').default;
+const u8aFromUtf8 = require('@polkadot/util/u8a/fromUtf8').default;
+
+const ALICE_SEED = 'Alice                           ';
+const BOB_ADDR = '5Gw3s7q4QLkSWwknsiPtjujPv3XM4Trxi5d4PgKMMk3gfGTE';
+
+async function main () {
+  // create an instance of the keyring
+  const keyring = new Keyring();
+
+  // Add Alice to our keyring (with the known seed for the account)
+  const alice = keyring.addFromSeed(u8aFromUtf8(ALICE_SEED));
+
+  // instantiate the API
+  const api = await ApiPromise.create();
+
+  // retrieve the nonce for Alice, so we can sign the transaction properly
+  const aliceNonce = await api.st.system.accountNonce(alice.address());
+
+  // Create a extrinsic, transferring 12345 units to Bob. We can also create, sign and send in one
+  // operation (as per the samples in the Api documentation, here we split it out for the
+  // sake of readability)
+  const transfer = api.tx.balances.transfer(BOB_ADDR, 12345);
+
+  // sign the transaction using our account
+  transfer.sign(alice, aliceNonce);
+
+  // send the transaction and retrieve the resulting Hash
+  const hash = await transfer.send();
+
+  console.log(`transfer 12345 to Bob with hash ${hash}`);
+}
+
+main().catch(console.error).finally(_ => process.exit());
