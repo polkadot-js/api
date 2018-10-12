64c58681104e37a1e94bfaaa080ec77a54c7d277
Jaco Greeff <jacogr@gmail.com>
2018-10-12 14:52:37 +0200

    Round out ApiPromise examples (#263)
    * Return value from array on subscription
    
    * Add final round of examples
    
    * Use padEnd instead of spaces...
    
    * Small cleanups

--- docs/examples/promise/02_listen_to_blocks/index.js
+++ docs/examples/promise/02_listen_to_blocks/index.js
@@ -5,13 +5,13 @@ async function main () {
   // Here we don't pass the (optional) provider, connecting directly to the default
   // node/port, i.e. `ws://127.0.0.1:9944`. Await for the isReady promise to ensure
   // the API has connected to the node and completed the initialisation process
-  const api = await new ApiPromise().isReady;
+  const api = await ApiPromise.create();
 
   // Subscribe to the new headers on-chain. The callback is fired when new headers
   // are found, the call itself returns a promise with a subscription that can be
   // used to unsubscribe from the newHead subscription
   const subsciptionId = await api.rpc.chain.newHead((header) => {
-    console.log(`best #${header.blockNumber.toString()}`);
+    console.log(`best #${header.blockNumber}`);
   });
 
   // id for the subscription, we can cleanup and unsubscribe via
