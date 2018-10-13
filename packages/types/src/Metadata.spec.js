9d525c799314aa5693d43a6a69ebe4845ce57258
Jaco Greeff <jacogr@gmail.com>
2018-10-13 17:24:11 +0200

    Metadata allows both length and non-length prefixed

--- packages/types/src/Metadata.spec.js
+++ packages/types/src/Metadata.spec.js
@@ -2,6 +2,8 @@
 // This software may be modified and distributed under the terms
 // of the ISC license. See the LICENSE file for details.
 
+import hexToU8a from '@polkadot/util/hex/toU8a';
+
 import Metadata from './Metadata';
 import rpcdata from './Metadata.rpc';
 
@@ -13,6 +15,13 @@ describe('Metadata', () => {
     console.error(str);
     console.error(decoded.getUniqTypes());
 
-    expect(str).not.toBe(null);
+    expect(decoded.events.length).not.toBe(0);
+  });
+
+  it('decodes when length not present (HACK)', () => {
+    const u8a = hexToU8a(rpcdata);
+    const decoded = new Metadata().fromU8a(u8a.subarray(2));
+
+    expect(decoded.events.length).not.toBe(0);
   });
 });
