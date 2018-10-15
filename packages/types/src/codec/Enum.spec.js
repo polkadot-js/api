9a005c83ba59706f511c6e71fe7ade9e37424451
Amaury Martiny <amaury.martiny@protonmail.com>
2018-10-15 12:16:06 +0200

    Fix tests

--- packages/types/src/codec/Enum.spec.js
+++ packages/types/src/codec/Enum.spec.js
@@ -2,32 +2,55 @@
 // This software may be modified and distributed under the terms
 // of the ISC license. See the LICENSE file for details.
 
-import EnumType from './EnumType';
-import Text from '../Text';
-import U32 from '../U32';
+import Enum from './Enum';
 
-describe('Struct', () => {
-  it('provides a clean toString() (raw)', () => {
+const testDecode = (type, input, expected) =>
+  it(`can decode from ${type}`, () => {
+    const e = new Enum(['foo', 'bar'], input);
+    expect(e.toString()).toBe(expected);
+  });
+
+const testEncode = (to, expected) =>
+  it(`can encode ${to}`, () => {
+    const e = new Enum(['foo', 'bar'], 1);
+    expect(e[to]()).toEqual(expected);
+  });
+
+describe('Enum', () => {
+
+  testDecode('Enum', undefined, 'foo');
+  testDecode('Enum', new Enum([], 1), 'bar');
+  testDecode('number', 0, 'foo');
+  testDecode('number', 1, 'bar');
+  testDecode('Uint8Array', Uint8Array.from([0]), 'foo');
+  testDecode('Uint8Array', Uint8Array.from([1]), 'bar');
+
+  testEncode('toJSON', 1);
+  testEncode('toNumber', 1);
+  testEncode('toString', 'bar');
+  testEncode('toU8a', Uint8Array.from([1]));
+
+  it('provides a clean toString()', () => {
     expect(
-      new EnumType(
-        [Text, U32]
-      ).fromU8a(new Uint8Array([0, 2 << 2, 49, 50])).raw.toString()
-    ).toEqual('12');
+      new Enum(['foo', 'bar']).toString()
+    ).toEqual('foo');
   });
 
   it('provides a clean toString() (enum)', () => {
     expect(
-      new EnumType(
-        [Text, U32]
-      ).fromU8a(new Uint8Array([1, 2 << 2, 49, 50])).toString()
-    ).toEqual('U32');
+      new Enum(['foo', 'bar'], new Enum([], 1)).toNumber()
+    ).toEqual(1);
   });
 
-  it('allows checking against defined indexes', () => {
+  it('converts to and from U8a', () => {
     expect(
-      new EnumType(
-        { 1: Text, 5: U32 }
-      ).fromU8a(new Uint8Array([1, 2 << 2, 49, 50])).toString()
-    ).toEqual('Text');
+      new Enum(['foo', 'bar']).fromU8a(new Uint8Array([1])).toU8a()
+    ).toEqual(new Uint8Array([1]));
+  });
+
+  it('converts from JSON', () => {
+    expect(
+      new Enum(['foo', 'bar']).fromJSON(5).toString()
+    ).toEqual('5');
   });
 });
