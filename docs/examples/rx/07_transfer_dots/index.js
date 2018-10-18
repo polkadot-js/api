d75593d7ee8c95d0df2639689ccca5788dae1e39
Luke Schoen <ltfschoen@gmail.com>
2018-10-19 00:04:45 +0200

    WIP: example rx 07. Unable to get .send to work. Unsure if hash returned correct

--- docs/examples/rx/07_transfer_dots/index.js
+++ docs/examples/rx/07_transfer_dots/index.js
@@ -1,9 +1,10 @@
-import { switchMap } from 'rxjs/operators';
+const { combineLatest } = require('rxjs');
+const { switchMap, map } = require('rxjs/operators');
 
 // Import the API, Keyring and some utility functions
 const { ApiRx } = require('@polkadot/api');
 const Keyring = require('@polkadot/keyring').default;
-const u8aFromUtf8 = require('@polkadot/util/u8a/fromUtf8').default;
+const { stringToU8a } = require('@polkadot/util');
 
 const ALICE_SEED = 'Alice'.padEnd(32, ' ');
 const BOB_ADDR = '5Gw3s7q4QLkSWwknsiPtjujPv3XM4Trxi5d4PgKMMk3gfGTE';
@@ -13,35 +14,47 @@ async function main () {
   const keyring = new Keyring();
 
   // Add Alice to our keyring (with the known seed for the account)
-  const alice = keyring.addFromSeed(u8aFromUtf8(ALICE_SEED));
+  const alice = keyring.addFromSeed(stringToU8a(ALICE_SEED));
 
   // Instantiate the API via Promise
-  const api = await ApiRx.create().toPromise();
+  const api = await ApiRx.create();
 
-  // Retrieve the nonce for Alice, to be used to sign the transaction.
   // Use the Storage chain state (runtime) Node Interface.
-  api.query.system
-    .accountNonce(alice.address())
+  api
     .pipe(
-      // Pipe nonce into transfer.
-      // Use the Extrinsics (runtime) Node Interface.
-      switchMap((aliceNonce) =>
-        api.tx.balances
-          // Create an extrinsic, transferring 12345 units to Bob.
-          .transfer(BOB_ADDR, 12345)
-          // Sign the transaction using our account keypair, nonce,
-          // and optionally the block hash
-          .sign(alice, aliceNonce)
-          // Send the transaction (optional status callback)
-          .send((status) => {
-            console.log(`current status ${status.type}`);
-          })
+      switchMap((api) =>
+        combineLatest([
+          // Retrieve the nonce for Alice, to be used to sign the transaction.
+          api.query.system.accountNonce(alice.address()),
+          api.rpc.chain.getBlockHash(0)
+            .pipe(
+              // Pipe nonce into transfer.
+              // Use the Extrinsics (runtime) Node Interface.
+              switchMap((aliceNonce, blockHash) => {
+                // Retrieve the balances for all validators
+                return combineLatest(
+                  [aliceNonce].map((aliceNonce) =>
+                    api.tx.balances
+                      // Create an extrinsic, transferring 12345 units to Bob.
+                      .transfer(BOB_ADDR, 12345)
+                      // Sign the transaction using our account keypair, nonce,
+                      // and optionally the block hash
+                      .sign(alice, aliceNonce, blockHash)
+                      // Send the transaction (optional status callback)
+                      // .send((status) => {
+                      //   console.log(`current status ${status.type}`);
+                      // })
+                  )
+                );
+              })
+            )
+        ])
       )
     )
     // Subscribe to overall resulting Hash
-    .subscribe((hash) => {
+    .subscribe(([hash]) => {
       console.log(`submitted transfer 12345 to Bob with hash ${hash}`);
     });
 }
 
-main().catch(console.error).finally(_ => process.exit());
+main().catch(console.error);
