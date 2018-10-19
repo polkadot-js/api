36bfc4cb9d96122cd910f0998d335b5804c78ead
Luke Schoen <ltfschoen@gmail.com>
2018-10-19 17:38:19 +0200

    refactor: Remove unsubscribe from example promises 02

--- docs/examples/promise/02_listen_to_blocks/index.js
+++ docs/examples/promise/02_listen_to_blocks/index.js
@@ -8,26 +8,12 @@ async function main () {
   const api = await ApiPromise.create();
 
   // Subscribe to the new headers on-chain. The callback is fired when new headers
-  // are found, the call itself returns a promise with a subscription that can be
-  // used to unsubscribe from the newHead subscription.
+  // are found, the call itself returns a promise with a subscription id. We show how
+  // to unsubscribe from subscription in subsequent examples.
   // Use the RPC Node Interface
-  const subscriptionId = await api.rpc.chain.subscribeNewHead((header) => {
+  await api.rpc.chain.subscribeNewHead((header) => {
     console.log(`Chain is at best block #${header.blockNumber}`);
   });
-
-  // Id for the subscription, we can cleanup and unsubscribe via
-  // `api.chain.newHead.unsubscribe(subscriptionId)`
-  console.log(`subscriptionId: ${subscriptionId}`);
-
-  // Cleanup and unsubscribe from the subscription after 20 seconds
-  // Uses ES6 Promise for anonymous function since setTimeout is not
-  // an async function and cannot be used with ES7 async-await
-  const isUnsubscribed = await new Promise(resolve => {
-    setTimeout(() => {
-      resolve(api.rpc.chain.subscribeNewHead.unsubscribe(subscriptionId));
-    }, 20000);
-  });
-  console.log(`isUnsubscribed: ${isUnsubscribed}`);
 }
 
 main().catch(console.error);
