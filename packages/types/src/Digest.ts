e7c51e7f7b2eb53f012b8ee3374b8be555c75187
Amaury Martiny <amaury.martiny@protonmail.com>
2018-10-19 15:55:24 +0200

    Add comment

--- packages/types/src/Digest.ts
+++ packages/types/src/Digest.ts
@@ -3,9 +3,9 @@
 // of the ISC license. See the LICENSE file for details.
 
 import AuthorityId from './AuthorityId';
-import Bytes from './Bytes';
 import EnumType from './codec/EnumType';
 import Hash from './Hash';
+import Null from './Null';
 import Struct from './codec/Struct';
 import Vector from './codec/Vector';
 
@@ -15,7 +15,8 @@ export class AuthoritiesChange extends Vector.with(AuthorityId) {
 export class ChangesTrieRoot extends Hash {
 }
 
-export class Other extends Bytes {
+// FIXME Once we have Events, have a look on how to decode them
+export class Other extends Null {
 }
 
 export class DigestItem extends EnumType<AuthoritiesChange | ChangesTrieRoot | Other> {
