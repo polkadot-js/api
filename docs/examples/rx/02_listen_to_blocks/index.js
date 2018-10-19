47088c0f3ca779773926723b6d6a3727780e1c61
Luke Schoen <ltfschoen@gmail.com>
2018-10-19 15:02:35 +0200

    refactor: Use switchMap for api query instead of doing later in subscribe block

--- docs/examples/rx/02_listen_to_blocks/index.js
+++ docs/examples/rx/02_listen_to_blocks/index.js
@@ -1,3 +1,4 @@
+const { switchMap } = require('rxjs/operators');
 const readline = require('readline');
 
 // Import the API Rx
@@ -8,12 +9,15 @@ const wsProvider = new WsProvider('ws://127.0.0.1:9944');
 
 async function main () {
   // Here we pass the (optional) provider
-  const subscriptionApiRx = ApiRx.create(wsProvider).subscribe((api) => {
-    // Use the RPC Node Interface
-    api.rpc.chain.subscribeNewHead().subscribe((header) => {
+  const subscriptionApiRx = ApiRx
+    .create(wsProvider)
+    .pipe(
+      // Here we subscribe to the chain header. Use the RPC Node Interface
+      switchMap((api) => api.rpc.chain.subscribeNewHead())
+    )
+    .subscribe((header) => {
       console.log(`Chain is at best block #${header.blockNumber.toNumber()}`);
     });
-  });
 
   // Support for detecting when CTRL+C (interrupt signal) received on Windows
   if (process.platform === 'win32') {
