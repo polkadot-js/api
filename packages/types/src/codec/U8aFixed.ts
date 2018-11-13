fa31a26419f6bdab397382d5cc9f509ae5acaa53
Amaury Martiny <amaury.martiny@protonmail.com>
2018-11-13 09:24:34 +0100

    Remove isBuffer

--- packages/types/src/codec/U8aFixed.ts
+++ packages/types/src/codec/U8aFixed.ts
@@ -2,7 +2,7 @@
 // This software may be modified and distributed under the terms
 // of the ISC license. See the LICENSE file for details.
 
-import { isBuffer, isString, isU8a, u8aToU8a } from '@polkadot/util';
+import { isString, isU8a, u8aToU8a } from '@polkadot/util';
 
 import { AnyU8a } from '../types';
 
@@ -22,7 +22,7 @@ export default class U8aFixed extends U8a {
   private static decodeU8aFixed (value: AnyU8a, bitLength: BitLength = 256): AnyU8a {
     if (isU8a(value)) {
       return value.subarray(0, bitLength / 8);
-    } else if (Array.isArray(value) || isBuffer(value) || isString(value)) {
+    } else if (Array.isArray(value) || isString(value)) {
       return U8aFixed.decodeU8aFixed(u8aToU8a(value), bitLength);
     }
 
