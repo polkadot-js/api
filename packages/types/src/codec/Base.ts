9e6c144100888355d3fc082dd6332fb73050b200
Amaury Martiny <amaury.martiny@protonmail.com>
2018-11-15 18:25:14 +0100

    Remove useless imports

--- packages/types/src/codec/Base.ts
+++ packages/types/src/codec/Base.ts
@@ -2,10 +2,6 @@
 // This software may be modified and distributed under the terms
 // of the ISC license. See the LICENSE file for details.
 
-import { logger } from '@polkadot/util';
-
-export const l = logger('codec');
-
 /**
  * A type extends the Base class, when it holds a value
  */
