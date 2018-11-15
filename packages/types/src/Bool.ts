b580c1214952c82222865c03aa68577195de2be6
Amaury Martiny <amaury.martiny@protonmail.com>
2018-11-15 14:55:50 +0100

    Bool extends Boolean (#401)
    * Bool extends Boolean
    
    * Private static
    
    * Add tests for Bool
    
    * Type

--- packages/types/src/Bool.ts
+++ packages/types/src/Bool.ts
@@ -2,18 +2,22 @@
 // This software may be modified and distributed under the terms
 // of the ISC license. See the LICENSE file for details.
 
-import { isU8a } from '@polkadot/util';
+import { isU8a, u8aToHex } from '@polkadot/util';
 
-import Base from './codec/Base';
+import { Codec } from './types';
 
-export default class Bool extends Base<boolean> {
-  constructor (value: Bool | Boolean | Uint8Array | boolean = false) {
+export default class Bool extends Boolean implements Codec {
+  public raw: Boolean; // FIXME Remove this once we convert all types out of Base
+
+  constructor (value: Bool | Boolean | Uint8Array | boolean | number = false) {
     super(
       Bool.decodeBool(value)
     );
+
+    this.raw = this;
   }
 
-  static decodeBool (value: any): boolean {
+  private static decodeBool (value: any): boolean {
     if (value instanceof Bool || value instanceof Boolean) {
       return value.valueOf();
     } else if (isU8a(value)) {
@@ -28,18 +32,18 @@ export default class Bool extends Base<boolean> {
   }
 
   toJSON (): any {
-    return this.raw;
+    return this.valueOf();
+  }
+
+  toHex (): string {
+    return u8aToHex(this.toU8a());
   }
 
   toU8a (isBare?: boolean): Uint8Array {
-    return new Uint8Array([this.raw ? 1 : 0]);
+    return new Uint8Array([this ? 1 : 0]);
   }
 
   toString (): string {
-    return `${this.raw}`;
-  }
-
-  valueOf (): boolean {
-    return this.raw;
+    return `${this.toJSON()}`;
   }
 }
