4805f9cb36c6927a050ac40310aabaeaa1ccd5ce
Jaco Greeff <jacogr@gmail.com>
2018-11-21 13:37:27 +0100

    Update LICENSE and README (#408)
    * Update LICENSE and README
    
    * Bump

--- packages/types/src/Type.ts
+++ packages/types/src/Type.ts
@@ -1,6 +1,6 @@
 // Copyright 2017-2018 @polkadot/types authors & contributors
 // This software may be modified and distributed under the terms
-// of the ISC license. See the LICENSE file for details.
+// of the Apache-2.0 license. See the LICENSE file for details.
 
 import Compact, { DEFAULT_LENGTH_BITS } from './codec/Compact';
 import U8a from './codec/U8a';
--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,6 +1,6 @@
 // Copyright 2017-2018 @polkadot/types authors & contributors
 // This software may be modified and distributed under the terms
-// of the ISC license. See the LICENSE file for details.
+// of the Apache-2.0 license. See the LICENSE file for details.
 
 // NOTE We are not re-exporting the index.ts from codec here. The reasoning being
 // that these should be enough to actually _use_ the codec, i.e. from a api-user
