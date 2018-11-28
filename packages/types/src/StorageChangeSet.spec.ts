922a715eafefb680fc6045311f01df88914f0fe4
Jaco Greeff <jacogr@gmail.com>
2018-11-28 11:55:29 +0100

    Unused import

--- packages/types/src/StorageChangeSet.spec.ts
+++ packages/types/src/StorageChangeSet.spec.ts
@@ -4,7 +4,6 @@
 
 import { createType } from '@polkadot/types/codec';
 import json from './json/StorageChangeSet.001.json';
-import Bytes from './Bytes';
 import StorageChangeSet from './StorageChangeSet';
 
 describe('StorageChangeSet', () => {
