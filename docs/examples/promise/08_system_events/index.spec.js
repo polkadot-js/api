5d4f3772928ad7e753d46d332a404105bc2b0d0d
Luke Schoen <ltfschoen@gmail.com>
2018-11-30 07:33:46 +0100

    test: Add test for promises 08 system events

---
+++ docs/examples/promise/08_system_events/index.spec.js
@@ -0,0 +1,22 @@
+// Copyright 2017-2018 @polkadot/api authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+const ApiPromise = require('../../../../packages/api/src/promise').default;
+
+describe('promise - 08 system events', () => {
+  let api;
+
+  beforeEach(async () => {
+    api = await ApiPromise.create();
+    jest.setTimeout(30000);
+  });
+
+  afterEach(() => {
+    jest.setTimeout(5000);
+  });
+
+  it('makes api.query.system.events available', () => {
+    expect(api.query.system.events).toBeDefined();
+  });
+});
