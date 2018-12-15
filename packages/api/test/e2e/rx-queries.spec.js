c3fa4c9c7459968f39af82d78c7c54d29004f2bd
Jaco Greeff <jacogr@gmail.com>
2018-12-15 15:30:36 +0100

    Add at variant for api/rx

--- packages/api/test/e2e/rx-queries.spec.js
+++ packages/api/test/e2e/rx-queries.spec.js
@@ -2,6 +2,7 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
+import { switchMap } from 'rxjs/operators';
 import testingPairs from '@polkadot/keyring/testingPairs';
 
 import Api from '../../src/rx';
@@ -14,7 +15,7 @@ describe.skip('e2e queries', () => {
   beforeEach(async () => {
     api = await Api.create().toPromise();
 
-    console.error(api);
+    // console.error(api);
   });
 
   it('makes the runtime, rpc, state & extrinsics available', () => {
@@ -34,5 +35,19 @@ describe.skip('e2e queries', () => {
 
       done();
     });
+  });
+
+  it('makes a query at a specific block', (done) => {
+    api.rpc.chain
+      .getHeader()
+      .pipe(
+        switchMap((header) =>
+          api.query.system.events.at(header.hash)
+        )
+      )
+      .subscribe((events) => {
+        expect(events.length).not.toEqual(0);
+        done();
+      });
   });
 });
