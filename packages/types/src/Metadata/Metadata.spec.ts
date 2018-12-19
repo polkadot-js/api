003a88eb67218bcbf44a64c51619521a9f8fd02c
Amaury Martiny <amaury.martiny@protonmail.com>
2018-12-19 11:59:09 +0100

    Remove useless tests

--- packages/types/src/Metadata/Metadata.spec.ts
+++ packages/types/src/Metadata/Metadata.spec.ts
@@ -2,26 +2,12 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import bbqBirch from '../json/Metadata.bbq.json';
 import compacted from '../json/Metadata.latest.json';
 import Metadata from './index';
-import rpcdata from './static';
 import latestParsed from './latest.substrate.json';
 
 describe('Metadata', () => {
-  it('decodes properly', () => {
-    const decoded = new Metadata(rpcdata);
-
-    expect(decoded.events.length).not.toBe(0);
-  });
-
-  it.skip('decodes BBQ properly', () => {
-    const decoded = new Metadata(bbqBirch.result);
-
-    expect(decoded.events.length).not.toBe(0);
-  });
-
-  it.skip('decodes latest properly', () => {
+  it('decodes latest properly', () => {
     const decoded = new Metadata(compacted.result);
     const str = JSON.stringify(decoded.toJSON());
 
