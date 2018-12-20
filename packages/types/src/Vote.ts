d0165d872fcdeba05c33cf9b12d2fd66a12fe535
Amaury Martiny <amaury.martiny@protonmail.com>
2018-12-20 16:59:02 +0100

    Add missing types

---
+++ packages/types/src/Vote.ts
@@ -0,0 +1,13 @@
+// Copyright 2017-2018 @polkadot/types authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+import I8 from './I8';
+
+/**
+ * @name Vote
+ * @description
+ * A number of lock periods, plus a vote, one way or the other.
+ */
+export default class Vote extends I8 {
+}
