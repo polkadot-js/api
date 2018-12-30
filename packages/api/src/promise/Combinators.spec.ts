8e9d38f74da0781da6b4150beb2be8b95a07b2d8
Jaco Greeff <jacogr@gmail.com>
2018-12-30 14:33:20 +0100

    auto-imports removed

--- packages/api/src/promise/Combinators.spec.ts
+++ packages/api/src/promise/Combinators.spec.ts
@@ -3,7 +3,6 @@
 // of the Apache-2.0 license. See the LICENSE file for details.
 
 import Combinator from './Combinator';
-import { doesNotReject } from 'assert';
 
 describe('Combinator', () => {
   it('it triggers on all values', (done) => {
