6678d7255c03a3783fe2f483864de1557af52907
Jaco Greeff <jacogr@gmail.com>
2019-01-04 16:18:44 +0100

    Remove api-observable (#530)
    * 2019
    
    * Remove api-observable
    
    * tuples (SomeLonelyType) -> SomeLonelyType
    
    * Bump to 0.37.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+# 0.37.1
+
+api-observable has been removed. This was only used in /apps and inconsistent with the api/rx and api/promise APIs. Future work will include derivates like was included in api-observable into the base.
+
+Tuples now return single types when only one type is available, i.e. `(AccountId)` would now resolve as `AccountId`. The extra type wrapper adds no benefit to users here.
+
 # 0.36.1
 
 Api Promise has been updated in the way we deal with subscriptions. Previously a subscription returned `Promise<number>` where the caller was to keep track of the id and use it in subsequent unsubscribes. Now any subscriptions return an unsubscribe/destroy function `(): void`, that is use for removing the subscriptions, e.g.
--- packages/types/src/Type.spec.ts
+++ packages/types/src/Type.spec.ts
@@ -1,4 +1,4 @@
-// Copyright 2017-2018 @polkadot/types authors & contributors
+// Copyright 2017-2019 @polkadot/types authors & contributors
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
@@ -10,6 +10,18 @@ describe('Type', () => {
     expect(
       () => new Type('Box<Proposal')
     ).toThrow(/find closing matching/);
+  });
+
+  it('cleans up tuples with a single value', () => {
+    expect(
+      new Type('(AccountId)').toString()
+    ).toEqual('AccountId');
+  });
+
+  it('does not touch tuples with multiple values', () => {
+    expect(
+      new Type('(AccountId, Balance)').toString()
+    ).toEqual('(AccountId,Balance)');
   });
 
   it('handles nested types', () => {
@@ -46,6 +58,12 @@ describe('Type', () => {
     expect(
       new Type('(Vec<u8>, PairOf<T::Balance>, Vec<AccountId>)').toString()
     ).toEqual('(Bytes,(Balance,Balance),Vec<AccountId>)');
+  });
+
+  it('changes () -> Null', () => {
+    expect(
+      new Type('()').toString()
+    ).toEqual('Null');
   });
 
   it('does not allow toU8a', () => {
