577d81ef67d51771db2fdf8f8abaa9bceb52b775
Jaco Greeff <jacogr@gmail.com>
2019-01-08 12:33:08 +0100

    Remove unused Codec import

--- packages/types/src/Metadata/Modules.ts
+++ packages/types/src/Metadata/Modules.ts
@@ -2,7 +2,7 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import { AnyNumber, Codec } from '../types';
+import { AnyNumber } from '../types';
 
 import Bytes from '../Bytes';
 import Enum from '../codec/Enum';
