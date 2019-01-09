2719d2716b159692e6c8cc33e19fff3983108240
Jaco Greeff <jacogr@gmail.com>
2019-01-09 12:56:49 +0100

    Default storage test

--- packages/types/src/Metadata/Metadata.spec.ts
+++ packages/types/src/Metadata/Metadata.spec.ts
@@ -2,19 +2,44 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
+import extrinsicsFromMeta from '@polkadot/extrinsics/fromMetadata';
+
+import createType from '../codec/createType';
+import Method from '../Method';
+
 import Metadata from './index';
 import latestParsed from './latest.substrate.json';
 import rpcData from './static';
 
 describe('Metadata', () => {
   it('decodes latest properly', () => {
-    const decoded = new Metadata(rpcData);
-    const str = JSON.stringify(decoded.toJSON());
+    const metadata = new Metadata(rpcData);
+    const str = JSON.stringify(metadata.toJSON());
 
     console.error(str);
-    console.error(decoded.getUniqTypes());
+    console.error(metadata.getUniqTypes());
 
-    expect(decoded.events.length).not.toBe(0);
+    expect(metadata.events.length).not.toBe(0);
     expect(str).toEqual(JSON.stringify(latestParsed));
+  });
+
+  describe('storage with default values', () => {
+    const metadata = new Metadata(rpcData);
+
+    Method.injectMethods(extrinsicsFromMeta(metadata));
+
+    metadata.modules.forEach((mod) => {
+      if (mod.storage.isNone) {
+        return;
+      }
+
+      mod.storage.unwrap().functions.forEach((fn) => {
+        it(`creates default types for ${mod.prefix}.${fn.name}, type ${fn.type}`, () => {
+          expect(
+            () => createType(fn.type.toString(), fn.default)
+          ).not.toThrow();
+        });
+      });
+    });
   });
 });
