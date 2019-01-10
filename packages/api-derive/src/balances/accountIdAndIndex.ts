b069a73168da5747aa0e70d3823d200e6599d47e
Amaury Martiny <amaury.martiny@protonmail.com>
2019-01-10 11:01:53 +0100

    votingBalance

--- packages/api-derive/src/balances/accountIdAndIndex.ts
+++ packages/api-derive/src/balances/accountIdAndIndex.ts
@@ -2,7 +2,7 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import { of } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 import ApiRx from '@polkadot/api/rx';
 import { decodeAddress } from '@polkadot/keyring';
@@ -14,10 +14,11 @@ import { accountIndexToId } from './accountIndexToId';
 export type IdAndIndex = [AccountId | undefined, AccountIndex | undefined];
 
 export function accountIdAndIndex (api: ApiRx) {
-  return (address: AccountId | AccountIndex | string | null | undefined) => {
+  return (address: AccountId | AccountIndex | string | null | undefined)
+    : Observable<(AccountIndex | AccountId | undefined)[]> => {
     try {
       // yes, this can fail, don't care too much, catch will catch it
-      const length = decodeAddress((address as any).toString()).length;
+      const { length } = decodeAddress((address as any).toString());
 
       if (length === 32) {
         const accountId = new AccountId(address as string);
