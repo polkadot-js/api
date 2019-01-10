81aa5ae0de04a08f77573d8a4e5e40fd3e0e679e
Amaury Martiny <amaury.martiny@protonmail.com>
2019-01-10 11:22:43 +0100

    validatingBalance

---
+++ packages/api-derive/src/balances/validatingBalance.ts
@@ -0,0 +1,37 @@
+// Copyright 2017-2019 @polkadot/api-derive authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+import BN from 'bn.js';
+import { combineLatest, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import ApiRx from '@polkadot/api/rx';
+import { AccountId, Balance } from '@polkadot/types/index';
+
+import { votingBalance } from './votingBalance';
+import { votingBalancesNominatorsFor } from './votingBalancesNominatorsFor';
+
+import { DerivedBalances } from '../types';
+
+export function validatingBalance (api: ApiRx) {
+  return (address: AccountId | string): Observable<DerivedBalances> =>
+    combineLatest(
+      votingBalance(api)(address),
+      votingBalancesNominatorsFor(api)(address)
+    ).pipe(
+      map(([balance, nominators]) => {
+        const nominatedBalance = nominators.reduce((total: BN, nominatorBalance: DerivedBalances) => {
+          return total.add(nominatorBalance.votingBalance);
+        }, new BN(0));
+
+        return {
+          ...balance,
+          nominators,
+          nominatedBalance: new Balance(nominatedBalance),
+          stakingBalance: new Balance(
+            nominatedBalance.add(balance.votingBalance)
+          )
+        };
+      })
+    );
+}
