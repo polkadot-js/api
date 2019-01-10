ce170911c0e634030d157e4b96a3b83e69d66289
Amaury Martiny <amaury.martiny@protonmail.com>
2019-01-10 11:35:19 +0100

    eraLength

---
+++ packages/api-derive/src/session/eraLength.ts
@@ -0,0 +1,25 @@
+// Copyright 2017-2019 @polkadot/api-derive authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+import BN from 'bn.js';
+import { combineLatest, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import ApiRx from '@polkadot/api/rx';
+import { BlockNumber } from '@polkadot/types/index';
+
+import { drr } from '../util/drr';
+
+export function eraLength (api: ApiRx) {
+  return (): Observable<BN> =>
+    (combineLatest([
+      api.query.session.sessionLength(),
+      api.query.staking.sessionsPerEra()
+    ]) as Observable<[BlockNumber?, BlockNumber?]>).pipe(
+      map(
+        ([sessionLength, sessionsPerEra]) =>
+          (sessionLength || new BN(1)).mul(sessionsPerEra || new BN(1))
+      ),
+      drr()
+    );
+}
