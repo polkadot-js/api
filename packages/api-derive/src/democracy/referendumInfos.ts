1c820bf50d9bcf75c0344e79f75851594e33cbdd
Amaury Martiny <amaury.martiny@protonmail.com>
2019-01-10 11:50:18 +0100

    referendumInfos

---
+++ packages/api-derive/src/democracy/referendumInfos.ts
@@ -0,0 +1,28 @@
+// Copyright 2017-2019 @polkadot/api-derive authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+import BN from 'bn.js';
+import { combineLatest, Observable } from 'rxjs';
+import { filter } from 'rxjs/operators';
+import ApiRx from '@polkadot/api/rx';
+import { ReferendumInfo } from '@polkadot/types/index';
+
+import { drr } from '../util/drr';
+
+export function referendumInfos (api: ApiRx) {
+  return (...params: Array<any>): Observable<Array<ReferendumInfo>> => {
+    const ids: Array<BN | number> = params.slice(0, params.length - 1);
+
+    return (combineLatest(
+      ids.map(
+        (id) => (api.query.democracy.referendumInfoOf(id) as Observable<ReferendumInfo>).pipe(
+          filter((info) => !!info)
+        )
+      )
+    )).pipe(
+      drr()
+    );
+  };
+
+}
