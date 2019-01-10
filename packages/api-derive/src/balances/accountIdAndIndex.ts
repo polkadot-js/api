3f4977a5d5c93579ab0a49f266ae2f4257ad3ad7
Amaury Martiny <amaury.martiny@protonmail.com>
2019-01-10 10:37:19 +0100

    accountIdAndIndex

--- packages/api-derive/src/balances/accountIdAndIndex.ts
+++ packages/api-derive/src/balances/accountIdAndIndex.ts
@@ -2,4 +2,38 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-export * from './accountIdAndIndex';
+import { of } from 'rxjs';
+import { map } from 'rxjs/operators';
+import ApiRx from '@polkadot/api/rx';
+import { decodeAddress } from '@polkadot/keyring';
+import { AccountId, AccountIndex } from '@polkadot/types/index';
+
+import { accountIdToIndex } from './accountIdToIndex';
+import { accountIndexToId } from './accountIndexToId';
+
+export type IdAndIndex = [AccountId | undefined, AccountIndex | undefined];
+
+export function accountIdAndIndex (api: ApiRx) {
+  return (address: AccountId | AccountIndex | string | null | undefined) => {
+    try {
+      // yes, this can fail, don't care too much, catch will catch it
+      const length = decodeAddress((address as any).toString()).length;
+
+      if (length === 32) {
+        const accountId = new AccountId(address as string);
+
+        return accountIdToIndex(api)(accountId).pipe(
+          map((accountIndex) => [accountId, accountIndex])
+        );
+      }
+
+      const accountIndex = new AccountIndex(address as string);
+
+      return accountIndexToId(api)(accountIndex).pipe(
+        map((accountId) => [accountId, accountIndex])
+      );
+    } catch (error) {
+      return of([undefined, undefined]);
+    }
+  };
+}
