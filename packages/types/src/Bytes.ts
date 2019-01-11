b92607ab766cde0231010ecb76558d1f7669fb65
Jaco Greeff <jacogr@gmail.com>
2019-01-11 15:37:28 +0100

    Remove unused imports

--- packages/types/src/Bytes.ts
+++ packages/types/src/Bytes.ts
@@ -2,7 +2,7 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import { hexToU8a, isHex, isString, isU8a, u8aToU8a } from '@polkadot/util';
+import { isString, isU8a, u8aToU8a } from '@polkadot/util';
 
 import { AnyU8a } from './types';
 import Compact from './codec/Compact';
