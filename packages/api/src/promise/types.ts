5b34d1c3931c3100cbbbd8872fa72ff2f4c49bd8
Jaco Greeff <jacogr@gmail.com>
2019-01-16 17:18:59 +0100

    Some extra (convience) types

--- packages/api/src/promise/types.ts
+++ packages/api/src/promise/types.ts
@@ -3,8 +3,10 @@
 // of the Apache-2.0 license. See the LICENSE file for details.
 
 import { Codec } from '@polkadot/types/types';
+import { QueryableStorageFunction as QueryableStorageFunctionBase, SubmittableExtrinsicFunction as SubmittableExtrinsicFunctionBase } from '../types';
 
 import ApiBase from '../Base';
+import SubmittableExtrinsicBase from '../SubmittableExtrinsic';
 
 export type OnCall = Promise<Codec | null | undefined> | PromiseSubscription;
 
@@ -13,3 +15,7 @@ export type PromiseSubscription = Promise<() => any>;
 export interface ApiPromiseInterface extends ApiBase<OnCall> {
   readonly isReady: Promise<ApiPromiseInterface>;
 }
+
+export type QueryableStorageFunction = QueryableStorageFunctionBase<OnCall>;
+export type SubmittableExtrinsic = SubmittableExtrinsicBase<OnCall>;
+export type SubmittableExtrinsicFunction = SubmittableExtrinsicFunctionBase<OnCall>;
