5fa4a6dcba54205f97a4287f7eef07550d4b606a
Stefanie Doll <stefie.doll@googlemail.com>
2019-01-17 10:56:01 +0100

    [Docs] ApiRx "make transfer" example

--- docs/examples/rx/06_make_transfer/index.js
+++ docs/examples/rx/06_make_transfer/index.js
@@ -2,6 +2,7 @@
 const { ApiPromise } = require('@polkadot/api');
 const { Keyring } = require('@polkadot/keyring');
 const { stringToU8a } = require('@polkadot/util');
+const { switchMap, first } = require('rxjs/operators');
 
 const ALICE_SEED = 'Alice'.padEnd(32, ' ');
 const BOB_ADDR = '5Gw3s7q4QLkSWwknsiPtjujPv3XM4Trxi5d4PgKMMk3gfGTE';
@@ -14,24 +15,24 @@ async function main () {
   const alice = keyring.addFromSeed(stringToU8a(ALICE_SEED));
 
   // Instantiate the API
-  const api = await ApiPromise.create();
+  const api = await ApiRx.create().toPromise();
 
   // Retrieve the nonce for Alice, to be used to sign the transaction
-  const aliceNonce = await api.query.system.accountNonce(alice.address());
-
-  // Create a extrinsic, transferring 12345 units to Bob. We can also create,
-  // sign and send in one operation (as per the samples in the Api documentation),
-  // here we split it out for the sake of readability
-  const transfer = api.tx.balances.transfer(BOB_ADDR, 12345);
-
-  // Sign and send the transaction using our account. Send the transaction and retrieve the resulting Hash
-  transfer.signAndSend(alice, ({ status, type }) => {
-    if (type === 'Finalised') {
-      console.log(`Successful transfer of 12345 from Alice to Bob with hash ${status.value.toHex()}`);
-    } else {
-      console.log(`Pending transfer of 12345 from Alice to Bob`);
-    }
-  });
+  // and create a extrinsic, transferring 12345 units to Bob.
+  api.query.system.accountNonce(alice.address()).pipe(first(),
+    // pipe nonce into transfer
+    switchMap(nonce => api.tx.balances
+      // create transfer
+      .transfer(BOB_ADDR, 12345)
+      // Sign and send the transcation
+      .signAndSend(alice)))
+    .subscribe(({ status, type }) => {
+      if (type === 'Finalised') {
+        console.log(`Successful transfer of 12345 from Alice to Bob with hash ${status.value.toHex()}`);
+      } else {
+        console.log(`Pending transfer of 12345 from Alice to Bob`);
+      }
+    });
 }
 
 main().catch(console.error).finally(_ => process.exit());
