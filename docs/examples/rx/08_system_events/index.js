c5fc97a1b770c793288c2ebf4bd34342cb9aae89
Stefanie Doll <stefie.doll@googlemail.com>
2019-01-17 10:56:24 +0100

    [Docs] ApiRx "system events" example

---
+++ docs/examples/rx/08_system_events/index.js
@@ -0,0 +1,31 @@
+// Import the API
+const { ApiRx } = require('@polkadot/api');
+
+async function main () {
+  // Create our API with a default connection to the local node
+  ApiRx.create().subscribe((api) => {
+  // subscribe to system events via storage
+    api.query.system.events().subscribe((events) => {
+      console.log(`\nReceived ${events.length} events:`);
+      // loop through the Vec<EventRecord>
+      events.forEach((record) => {
+        // extract the phase, event and the event types
+        const { event, phase } = record;
+        const types = event.typeDef;
+        
+        // show what we are busy with
+        console.log(`\t${event.section}:${event.method}:: (phase=${phase.toString()})`);
+        console.log(`\t\t${event.meta.documentation.toString()}`);
+
+        // loop through each of the parameters, displaying the type and data
+        event.data.forEach((data, index) => {
+          console.log(`\t\t\t${types[index].type}: ${data.toString()}`);
+        });
+      });
+    });
+  });
+
+main().catch((error) => {
+  console.error(error);
+  process.exit(-1);
+});
