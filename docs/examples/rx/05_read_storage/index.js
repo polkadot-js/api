a2a5823b61267240dacbcd3e69f94acd01a73338
Stefanie Doll <stefie.doll@googlemail.com>
2019-01-21 23:36:38 +0100

    [Docs] Read storage example with piped switchMap()

--- docs/examples/rx/05_read_storage/index.js
+++ docs/examples/rx/05_read_storage/index.js
@@ -1,7 +1,7 @@
 // Import the API
 const { ApiRx } = require('@polkadot/api');
 // Import dependencies from RxJs
-const { combineLatest, of, zip } = require('rxjs');
+const { combineLatest, of } = require('rxjs');
 const { first, switchMap } = require('rxjs/operators');
 
 // Our address for Alice on the dev chain
@@ -9,38 +9,39 @@ const Alice = '5GoKvZWG5ZPYL1WUovuHW3zJBWBP5eT8CbqjdRY4Q6iMaDtZ';
 
 function main () {
   // Create our API with a default connection to the local node
-  new ApiRx().isReady
+  new ApiRx(provider).isReady
     .pipe(
-      switchMap((api) =>
-        combineLatest(
-          of(api),
+      switchMap((api) => combineLatest(
+        of(api),
+        api.query.session.validators().pipe(first())
+      )),
+      switchMap(([api, validators]) => {
+        // If the node has active validators, get the balances
+        const balances = (validators && validators.length > 0)
+          ? combineLatest(validators.map(authorityId => api.query.balances.freeBalance(authorityId).pipe(first())))
+          : null;
+
+        // If there are no validators, we're not returning any balances
+        return combineLatest(
           api.query.system.accountNonce(Alice).pipe(first()),
           api.query.timestamp.blockPeriod().pipe(first()),
-          api.query.session.validators().pipe(first())
-        )
-      )
+          of(validators),
+          balances
+        );
+      })
     )
     // Then we're subscribing to the emitted results
-    .subscribe(([api, accountNonce, blockPeriod, validators]) => {
+    .subscribe(([accountNonce, blockPeriod, validators, validatorBalances]) => {
       console.log(`accountNonce(${Alice}) ${accountNonce}`);
       console.log(`blockPeriod ${blockPeriod.toNumber()} seconds`);
 
-      // If the node has active validators
-      if (validators && validators.length > 0) {
-        // We're using RxJs 'zip()' combination operator together with first()
-        // to get the first emitted values of multiple observables as an array
-        const observable = zip(validators.map(authorityId =>
-          api.query.balances.freeBalance(authorityId).pipe(first())
-        ));
-
+      if (validatorBalances) {
         // And lastly we're subscribing to the observable and print out
         // the authorityIds and balances of all validators
-        observable.subscribe(validatorBalances => {
-          console.log('validators', validators.map((authorityId, index) => ({
-            address: authorityId.toString(),
-            balance: validatorBalances[index].toString()
-          })));
-        });
+        console.log('validators', validators.map((authorityId, index) => ({
+          address: authorityId.toString(),
+          balance: validatorBalances[index].toString()
+        })));
       }
     });
 }
