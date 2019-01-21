798080cd7784985c38aa58b6d58c5f83e634d3a5
Stefanie Doll <stefie.doll@googlemail.com>
2019-01-21 11:44:54 +0100

    [Docs] Fix Rx read_storage example

--- docs/examples/rx/05_read_storage/index.js
+++ docs/examples/rx/05_read_storage/index.js
@@ -11,32 +11,31 @@ function main () {
   ApiRx.create().subscribe(async (api) => {
     // We're using RxJs 'zip()' combination operator together with first()
     // to get the first emitted values of multiple observables as an array
-    zip(
+    const observable = zip(
       api.query.system.accountNonce(Alice).pipe(first()),
       api.query.timestamp.blockPeriod().pipe(first()),
       api.query.session.validators().pipe(first())
-    )
+    );
     // Then we're subscribing to the emitted results
-    .subscribe(([accountNonce, blockPeriod, validators]) => {
+    observable.subscribe(([accountNonce, blockPeriod, validators]) => {
       console.log(`accountNonce(${Alice}) ${accountNonce}`);
       console.log(`blockPeriod ${blockPeriod.toNumber()} seconds`);
 
       // If the node has active validators
       if (validators && validators.length > 0) {
-        createLog('Validators', wrapper, 'highlight');
-
         // We're getting the the balances of all validators, again using 'zip()'
-        validators.map(authorityId => zip(
-            api.query.balances.freeBalance(authorityId).pipe(first())
-          )
-          .subscribe(validatorBalances => {
-            // And lastly we print out the authorityIds and balances of all validators
-            console.log('validators', validators.map((authorityId, index) => ({
-              address: authorityId.toString(),
-              balance: validatorBalances[index].toString()
-            })));
-          }
+        const observable = zip(validators.map(authorityId =>
+          api.query.balances.freeBalance(authorityId).pipe(first())
         ));
+
+        // And lastly we're subscribing to the observable and print out
+        // the authorityIds and balances of all validators
+        observable.subscribe(validatorBalances => {
+          console.log('validators', validators.map((authorityId, index) => ({
+            address: authorityId.toString(),
+            balance: validatorBalances[index].toString()
+          })));
+        });
       }
     });
   });
