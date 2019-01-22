7d57cde64913e18a07c124af0438e081c09eaeff
Stefanie Doll <stefie.doll@googlemail.com>
2019-01-22 11:37:12 +0100

    [Docs] removed tap() example

--- docs/examples/rx/05_read_storage/index.js
+++ docs/examples/rx/05_read_storage/index.js
@@ -48,47 +48,4 @@ function main () {
     });
 }
 
-function mainTap () {
-    new ApiRx(provider).isReady
-      .pipe(
-        // Here we ake our basic chain state/storage queries, all in one go
-        switchMap((api) => combineLatest(
-          // We're combining the results together with the emitted value 'api',
-          // which we're turning back into an observable using of()
-          of(api),
-          api.query.session.validators().pipe(first()),
-          api.query.system.accountNonce(ALICE).pipe(first()),
-          api.query.timestamp.blockPeriod().pipe(first())
-        )),
-        tap(([,, accountNonce, blockPeriod]) => {
-          // We're using tap() to print out the results of our account nonce and block period queries
-          console.log(`accountNonce(${Alice}) ${accountNonce}`);
-          console.log(`blockPeriod ${blockPeriod.toNumber()} seconds`);
-
-        }),
-        switchMap(([api, validators]) => {
-          // In the next step, we're checking if the node has active validators.
-          // If it does, we're making another call to the api to get the balances for all validators
-          const balances = (validators && validators.length > 0)
-            ? combineLatest(validators.map(authorityId => api.query.balances.freeBalance(authorityId).pipe(first())))
-            : of(null);
-          // If there are no validators, we're not returning any balances
-          return combineLatest(
-            of(validators),
-            balances
-          );
-        })
-      )
-      // Then we're subscribing to the emitted results
-      .subscribe(([validators, validatorBalances]) => {
-        if (validatorBalances) {
-          // And lastly we print out the authorityIds and balances of all validators
-          console.log('validators', validators.map((authorityId, index) => ({
-            address: authorityId.toString(),
-            balance: validatorBalances[index].toString()
-          })));
-        }
-      });
-}
-
 main().catch(console.error).finally(_ => process.exit());
