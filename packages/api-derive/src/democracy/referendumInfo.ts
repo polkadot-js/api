b40d7a301ea9e7db49643f0356d4ec3825691f89
Jaco Greeff <jacogr@gmail.com>
2019-02-09 07:54:50 +0200

    Return ReferendumIndex along with info (derive) (#669)
    * Return ReferendumIndex along with info (derive)
    
    * Rename index
    
    * Doc rename

---
+++ packages/api-derive/src/democracy/referendumInfo.ts
@@ -0,0 +1,44 @@
+// Copyright 2017-2019 @polkadot/api-derive authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+import BN from 'bn.js';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { ApiInterface$Rx } from '@polkadot/api/types';
+import { ReferendumInfo, ReferendumIndex } from '@polkadot/types/index';
+
+import { drr } from '../util/drr';
+
+/**
+ * @name ReferendumInfoExtended
+ * @description
+ * A [[ReferendumInfo]] with an additional `index` field
+ */
+export class ReferendumInfoExtended extends ReferendumInfo {
+  private _index: ReferendumIndex;
+
+  constructor (info: ReferendumInfo, index: BN | number) {
+    super(info);
+
+    this._index = new ReferendumIndex(index);
+  }
+
+  /**
+   * @description Convenience getter, returns the referendumIndex
+   */
+  get index (): ReferendumIndex {
+    return this._index;
+  }
+}
+
+export function referendumInfo (api: ApiInterface$Rx) {
+  return (id: BN | number): Observable<ReferendumInfoExtended> => {
+    return (api.query.democracy.referendumInfoOf(id) as Observable<ReferendumInfo>)
+      .pipe(
+        map((info) => new ReferendumInfoExtended(info, id)),
+        drr()
+      );
+  };
+
+}
