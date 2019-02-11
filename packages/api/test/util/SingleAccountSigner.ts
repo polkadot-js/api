092129c28e8fba64522d570eb9b6fe77e7080695
Jaco Greeff <jacogr@gmail.com>
2019-02-11 12:51:41 +0100

    Add Signer with status updates (#675)
    * Add Signer with status updates
    
    * Remove useless (now) updateId check
    
    * Don't send updates for non-owned extrinsics
    
    * opt -> options

---
+++ packages/api/test/util/SingleAccountSigner.ts
@@ -0,0 +1,27 @@
+// Copyright 2017-2019 @polkadot/api authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+import { KeyringPair } from '@polkadot/keyring/types';
+import { SignatureOptions } from '@polkadot/types/ExtrinsicSignature';
+import { Extrinsic } from '@polkadot/types/index';
+
+let id = 0;
+
+export default class SingleAccountSigner {
+  private keyringPair: KeyringPair;
+
+  constructor (keyringPair: KeyringPair) {
+    this.keyringPair = keyringPair;
+  }
+
+  async sign (extrinsic: Extrinsic, address: string, options: SignatureOptions) {
+    if (!this.keyringPair || String(address) !== this.keyringPair.address()) {
+      throw new Error('does not have the keyringPair');
+    }
+
+    extrinsic.sign(this.keyringPair, options);
+
+    return ++id;
+  }
+}
