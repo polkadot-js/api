c74a1f6cf997c0895be9f24fd35454fce026904d
Jaco Greeff <jacogr@gmail.com>
2019-02-12 13:44:45 +0100

    Export WsProvider from api (single-require use)

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -2,6 +2,8 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
+export { WsProvider } from '@polkadot/rpc-provider/index';
+
 export { default as ApiPromise } from './promise';
 export { default as ApiRx } from './rx';
 export { default as SubmittableExtrinsic, SubmittableResult } from './SubmittableExtrinsic';
