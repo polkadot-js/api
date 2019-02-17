722f14e0abfd862bb6c7a6764fb29deced428ab0
Jaco Greeff <jacogr@gmail.com>
2019-02-17 08:25:15 +0100

    Remov extra files

--- packages/types/src/scripts/MetadataMdWrapper.js
+++ packages/types/src/scripts/MetadataMdWrapper.js
@@ -3,5 +3,15 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-require('./babelConfig.js');
+require('@babel/register')({
+  extensions: ['.js', '.ts'],
+  plugins: [
+    ['module-resolver', {
+      alias: {
+        '^@polkadot/types(.*)': './packages/types/src\\2'
+      }
+    }]
+  ]
+});
+
 require('./MetadataMd.ts');
