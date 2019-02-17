cca6a2ff6553fac67ac3ea9b2b27156957490b1a
Jaco Greeff <jacogr@gmail.com>
2019-02-17 08:14:20 +0100

    Cleanup md rendering, remove empty sections

--- packages/types/src/scripts/MetadataMdWrapper.js
+++ packages/types/src/scripts/MetadataMdWrapper.js
@@ -3,5 +3,5 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-require('../../babelConfig.js');
+require('./babelConfig.js');
 require('./MetadataMd.ts');
--- packages/types/src/scripts/babelConfig.js
+++ packages/types/src/scripts/babelConfig.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+// @ts-check
 // Copyright 2017-2019 @polkadot/types authors & contributors
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
