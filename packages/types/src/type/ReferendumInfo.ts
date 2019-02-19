cc673de56bca9c3054c8e5f987364f283bdc5861
Jaco Greeff <jacogr@gmail.com>
2019-02-19 15:02:37 +0100

    Fix type import

--- packages/types/src/type/ReferendumInfo.ts
+++ packages/types/src/type/ReferendumInfo.ts
@@ -2,10 +2,10 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import { Codec } from '../codec/types';
+import { Codec } from '../types';
 
-import BlockNumber from './BlockNumber';
 import Struct from '../codec/Struct';
+import BlockNumber from './BlockNumber';
 import Proposal from './Proposal';
 import VoteThreshold from './VoteThreshold';
 
