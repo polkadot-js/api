0ca8e94a51d3476c268c896d0cde4196433410f7
Jaco Greeff <jacogr@gmail.com>
2019-04-01 16:18:10 +0200

    Exapnd e2e tests

--- packages/api/test/e2e/promise-contract.spec.js
+++ packages/api/test/e2e/promise-contract.spec.js
@@ -12,6 +12,8 @@ import json from '../data/erc20.json';
 import Api from '../../src/promise';
 
 describe.skip('e2e contracts', () => {
+  let addrress;
+  let codeHash;
   let keyring;
   let abi;
   let api;
@@ -37,12 +39,42 @@ describe.skip('e2e contracts', () => {
   it('allows putCode', (done) => {
     const code = fs.readFileSync(path.join(__dirname, '../data/erc20.wasm')).toString('hex');
 
-    api.tx.contract.putCode(200000, `0x${code}`).signAndSend(keyring.eve, (result) => {
-      console.error(JSON.stringify(result));
+    api.tx.contract
+      .putCode(200000, `0x${code}`)
+      .signAndSend(keyring.eve, (result) => {
+        console.error('putCode', JSON.stringify(result));
 
-      if (result.status.isFinalized) {
-        done();
-      }
-    });
+        if (result.status.isFinalized) {
+          done();
+        }
+      });
+  });
+
+  it('allows contract create', (done) => {
+    expect(codeHash).toBeDefined();
+
+    api.tx.contract
+      .create(1234, 12345, codeHash, abi.deploy(12345))
+      .signAndSend(keyring.bob, (result) => {
+        console.error('create', JSON.stringify(result));
+
+        if (result.status.isFinalized) {
+          done();
+        }
+      });
+  });
+
+  it('allows contract call', (done) => {
+    expect(address).toBeDefined();
+
+    api.tx.contract
+      .call(address, 123, 12345, abi.messages.balanceOf(keyring.alice.address()))
+      .signAndSend(keyring.bob, (result) => {
+        console.error('call', JSON.stringify(result));
+
+        if (result.status.isFinalized) {
+          done();
+        }
+      });
   });
 });
