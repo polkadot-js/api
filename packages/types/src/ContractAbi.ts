4aa1783e3c2e2fe00f5035add2889eada07d38b4
Jaco Greeff <jacogr@gmail.com>
2019-04-12 07:57:20 +0200

    Remove contract encoding debug (confirmed working) (#836)
    * Remove contract encoding debug (confirmed working)
    
    * Remove (now-unused) import

--- packages/types/src/ContractAbi.ts
+++ packages/types/src/ContractAbi.ts
@@ -4,7 +4,7 @@
 
 import { CodecArg, Constructor } from './types';
 
-import { assert, isNumber, isNull, isString, isUndefined, stringCamelCase, u8aToHex } from '@polkadot/util';
+import { assert, isNumber, isNull, isString, isUndefined, stringCamelCase } from '@polkadot/util';
 
 import Compact from './codec/Compact';
 import { createClass } from './codec/createType';
@@ -133,8 +133,6 @@ export default class ContractAbi implements Contract {
           return mapped;
         }, { ...baseStruct })
       ).toU8a();
-
-      console.error('hexToU8a(encoded)', u8aToHex(u8a));
 
       return Compact.addLengthPrefix(u8a);
     };
