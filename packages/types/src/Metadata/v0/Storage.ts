ddf036948f88d78b5d76f5977e5a390abee788c6
Amaury Martiny <amaury.martiny@protonmail.com>
2019-05-08 16:48:31 +0200

    Linked map in v0

--- packages/types/src/Metadata/v0/Storage.ts
+++ packages/types/src/Metadata/v0/Storage.ts
@@ -115,9 +115,15 @@ export class StorageFunctionType extends EnumType<PlainType | MapType> {
    * @description Returns the string representation of the value
    */
   toString (): string {
-    return this.isMap
-      ? this.asMap.value.toString()
-      : this.asType.toString();
+    if (this.isMap) {
+      if (this.asMap.isLinked) {
+        return `(${this.asMap.value.toString()}, Linkage<${this.asMap.key.toString()}>)`;
+      }
+
+      return this.asMap.value.toString();
+    }
+
+    return this.asType.toString();
   }
 }
 
