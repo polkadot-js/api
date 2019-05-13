683ab23cd42230c390a2e212afa61dab5b343da5
Stefanie Doll <stefie.doll@googlemail.com>
2019-05-13 17:53:35 +0200

    Fixed existing accounts tests and added tests for all accounts.derive methods

--- packages/api-derive/test/e2e/rx.spec.ts
+++ packages/api-derive/test/e2e/rx.spec.ts
@@ -5,13 +5,17 @@
 import BN from 'bn.js';
 import ApiRx from '@polkadot/api/rx/Api';
 import { ApiInterface$Rx } from '@polkadot/api/types';
-import { BlockNumber } from '@polkadot/types';
+import { AccountId, AccountIndex, BlockNumber } from '@polkadot/types';
 import { WsProvider } from '@polkadot/rpc-provider';
 
 const WS_LOCAL = 'ws://127.0.0.1:9944/';
 // const WS_POC3 = 'wss://poc3-rpc.polkadot.io/';
 
-describe.skip('derive e2e', () => {
+// Dev account Alice
+const ID = '5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY';
+const IX = 'F7Hs';
+
+describe('derive e2e', () => {
   let api: ApiInterface$Rx;
 
   beforeAll(() => {
@@ -21,6 +25,109 @@ describe.skip('derive e2e', () => {
   beforeEach(async (done) => {
     api = await ApiRx.create(new WsProvider(WS_LOCAL)).toPromise();
     done();
+  });
+
+  // These derive.accounts tests only work on localhost, not the poc-3 URL
+  // (and it is assuming it sent at least 1 tx)
+  describe('accounts', () => {
+    it('looks up AccountId & AccountIndex from AccountId', async (done) => {
+      // @ts-ignore silence warning until we have static types here
+      api.derive.accounts.idAndIndex(ID).subscribe(([accountId, accountIndex]) => {
+        expect(accountId.toString()).toEqual(ID);
+
+        // The first emitted value for ix is undefined when passing the ID
+        if (accountIndex) {
+          expect(accountIndex.toString()).toEqual(IX);
+        } else {
+          expect(accountIndex).toEqual(undefined);
+        }
+        done();
+      });
+    });
+
+    it('looks up AccountId & AccountIndex from AccountIndex', async (done) => {
+      // @ts-ignore silence warning until we have static types here
+      api.derive.accounts.idAndIndex(IX).subscribe(([accountId, accountIndex]) => {
+        // The first emitted value for id is undefined when passing the IX
+        if (accountId) {
+          expect(accountId.toString()).toEqual(ID);
+        } else {
+          expect(accountId).toEqual(undefined);
+        }
+        expect(accountIndex.toString()).toEqual(IX);
+        done();
+      });
+    });
+
+    it('looks up AccountId from AccountIndex', async (done) => {
+      // @ts-ignore silence warning until we have static types here
+      api.derive.accounts.indexToId(IX).subscribe((accountId) => {
+        // The first emitted value for accountId is undefined when passing the IX
+        if (accountId) {
+          expect(accountId instanceof AccountId).toBe(true);
+          expect(accountId.toString()).toEqual(ID);
+        } else {
+          expect(accountId).toEqual(undefined);
+        }
+        done();
+      });
+    });
+
+    it('looks up AccountIndex from AccountId', async (done) => {
+      // @ts-ignore silence warning until we have static types here
+      api.derive.accounts.idToIndex(ID).subscribe((accountIndex) => {
+        // The first emitted value for AccountIndex is undefined when passing the ID
+        if (accountIndex) {
+          expect(accountIndex instanceof AccountIndex).toBe(true);
+          expect(accountIndex.toString()).toEqual(IX);
+        } else {
+          expect(accountIndex).toEqual(undefined);
+        }
+        done();
+      });
+    });
+
+    it('looks up all AccountIndexes', async (done) => {
+      // @ts-ignore silence warning until we have static types here
+      api.derive.accounts.indexes().subscribe((accountIndexes) => {
+        // A local dev chain should have the AccountIndex of Alice
+        expect(accountIndexes).toHaveProperty(
+          ID,
+          new AccountIndex(IX)
+        );
+        done();
+      });
+    });
+  });
+
+    // these only work on localhost, not the poc-3 URL
+  // (and it is assuming it sent at least 1 tx)
+  describe('balances', () => {
+    it('It returns an object with all relevant fees of type BN', async (done) => {
+      api.derive.balances.fees().subscribe((fees) => {
+        expect(fees).toEqual(expect.objectContaining({
+          creationFee: expect.any(BN),
+          existentialDeposit: expect.any(BN),
+          transactionBaseFee: expect.any(BN),
+          transactionByteFee: expect.any(BN),
+          transferFee: expect.any(BN)
+        }));
+        done();
+      });
+    });
+    
+    it('It returns an object with all relevant fees of type BN', async (done) => {
+      api.derive.balances.fees().subscribe((fees) => {
+        expect(fees).toEqual(expect.objectContaining({
+          creationFee: expect.any(BN),
+          existentialDeposit: expect.any(BN),
+          transactionBaseFee: expect.any(BN),
+          transactionByteFee: expect.any(BN),
+          transferFee: expect.any(BN)
+        }));
+        done();
+      });
+    });
   });
 
   it('derive.chain.bestNumber', async (done) => {
@@ -42,31 +149,6 @@ describe.skip('derive e2e', () => {
     api.derive.staking.intentionsBalances().subscribe((balances) => {
       expect(Object.keys(balances as object)).not.toHaveLength(0);
       done();
-    });
-  });
-
-  // these only work on localhost, not the poc-3 URL
-  // (and it is assuming it sent at least 1 tx)
-  describe('accounts', () => {
-    const ID = '5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY';
-    const IX = 'F7Gh';
-
-    it('looks up id & index from id', async (done) => {
-      // @ts-ignore silence warning until we have static types here
-      api.derive.accounts.idAndIndex(ID).subscribe(([id, ix]) => {
-        expect(id.toString()).toEqual(ID);
-        expect(ix.toString()).toEqual(IX);
-        done();
-      });
-    });
-
-    it('looks up id & index from index', async (done) => {
-      // @ts-ignore silence warning until we have static types here
-      api.derive.accounts.idAndIndex(IX).subscribe(([id, ix]) => {
-        expect(id.toString()).toEqual(ID);
-        expect(ix.toString()).toEqual(IX);
-        done();
-      });
     });
   });
 
