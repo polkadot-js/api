c840207d71ff812e70a22c8d1e1117eea8a3076a
Stefanie Doll <stefie.doll@googlemail.com>
2019-05-13 19:46:17 +0200

    Removed unused import

--- packages/api-derive/test/e2e/rx.spec.ts
+++ packages/api-derive/test/e2e/rx.spec.ts
@@ -3,7 +3,6 @@
 // of the Apache-2.0 license. See the LICENSE file for details.
 
 import BN from 'bn.js';
-import { first } from 'rxjs/operators';
 
 import ApiRx from '@polkadot/api/rx/Api';
 import { ApiInterface$Rx } from '@polkadot/api/types';
