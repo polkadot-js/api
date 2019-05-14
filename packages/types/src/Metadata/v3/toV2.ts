57715c50c0f011081b959cddcdccf520ba60c41e
Jaco Greeff <jacogr@gmail.com>
2019-05-14 16:21:32 +0200

    Revert "Update extrinsics FromMetadata. Drop storages with doublemap type when converting v3 to v2 (#888)" (#903)
    This reverts commit 97032e7e86c684211468a7876fa0ab0d5dd28738.

--- packages/types/src/Metadata/v3/toV2.ts
+++ packages/types/src/Metadata/v3/toV2.ts
@@ -2,37 +2,9 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import Option from '../../codec/Option';
-import Vector from '../../codec/Vector';
 import MetadataV2 from '../v2/Metadata';
 import MetadataV3 from '.';
-import { MetadataModule } from './Metadata';
-import { StorageFunctionMetadata } from './Storage';
 
-function storageV2 (storage: Option<Vector<StorageFunctionMetadata>>): Option<Vector<StorageFunctionMetadata>> {
-  if (storage.isNone) {
-    return storage;
-  }
-
-  const storageV2 = storage.unwrap().filter(({ type }) => !type.isDoubleMap);
-
-  return new Option<Vector<StorageFunctionMetadata>>(
-    Vector.with(StorageFunctionMetadata),
-    new Vector<StorageFunctionMetadata>(StorageFunctionMetadata, storageV2)
-  );
-}
-
-function modulesV2 (v3: MetadataV3): Array<MetadataModule> {
-  return v3.modules.map((mod) =>
-    new MetadataModule({
-      name: mod.name,
-      prefix: mod.prefix,
-      storage: storageV2(mod.storage)
-    }));
-}
-
-export default function toV2 (v3: MetadataV3): MetadataV2 {
-  return new MetadataV2({
-    modules: modulesV2(v3)
-  });
+export default function toV2 (v2: MetadataV3): MetadataV2 {
+  return new MetadataV2(v2);
 }
