f070b637f7b6a4905a4b15ffec0d11905fae1c0c
Jaco Greeff <jacogr@gmail.com>
2019-05-16 11:18:49 +0200

    Remove struct map naming overrides (#918)

--- packages/types/src/rpc/RuntimeVersion.spec.ts
+++ packages/types/src/rpc/RuntimeVersion.spec.ts
@@ -2,30 +2,30 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import rpc from '../json/RuntimeVersion.001.json';
+import rpc from '../json/RuntimeVersion.002.json';
 import RuntimeVersion from './RuntimeVersion';
 
 describe('RuntimeVersion', () => {
   const version = new RuntimeVersion(rpc.result);
 
   it('has the correct authoring', () => {
-    expect(version.authoringVersion.toNumber()).toEqual(1);
+    expect(version.authoringVersion.toNumber()).toEqual(10);
   });
 
   it('has the apis', () => {
     const api = version.apis[0];
 
-    expect(api.id.toHex()).toEqual('0x696e686572656e74');
-    expect(api.version.toNumber()).toEqual(1);
+    expect(api.id.toHex()).toEqual('0xdf6acb689907609b');
+    expect(api.version.toNumber()).toEqual(2);
   });
 
   it('has the correct implementation', () => {
     expect(version.implName.toString()).toEqual('substrate-node');
-    expect(version.implVersion.toNumber()).toEqual(0);
+    expect(version.implVersion.toNumber()).toEqual(60);
   });
 
   it('has the correct spec', () => {
     expect(version.specName.toString()).toEqual('node');
-    expect(version.specVersion.toNumber()).toEqual(1);
+    expect(version.specVersion.toNumber()).toEqual(60);
   });
 });
