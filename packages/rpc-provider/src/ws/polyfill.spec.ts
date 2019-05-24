fbdc7e665772eb5cdaf853b3c4f7c2e9b7c2796e
Amaury Martiny <amaury.martiny@protonmail.com>
2019-05-24 17:53:43 +0200

    Add more constructor

--- packages/rpc-provider/src/ws/polyfill.spec.ts
+++ packages/rpc-provider/src/ws/polyfill.spec.ts
@@ -2,12 +2,14 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
+import { Constructor } from '@polkadot/types/types';
+
 import { Global } from './../mock/types';
 
 declare let global: Global;
 
 describe('ws/polyfill', () => {
-  let origWs: WebSocket;
+  let origWs: Constructor<WebSocket>;
 
   beforeEach(() => {
     origWs = global.WebSocket;
--- packages/rpc-provider/src/ws/subscribe.spec.ts
+++ packages/rpc-provider/src/ws/subscribe.spec.ts
@@ -2,6 +2,8 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
+import { Constructor } from '@polkadot/types/types';
+
 import WsProvider from './';
 import { Global, Mock } from './../mock/types';
 import { mockWs, TEST_WS_URL } from '../../test/mockWs';
@@ -21,7 +23,7 @@ function createWs (autoConnect: boolean = true) {
 }
 
 describe('subscribe', () => {
-  let globalWs: WebSocket;
+  let globalWs: Constructor<WebSocket>;
 
   beforeEach(() => {
     globalWs = global.WebSocket;
