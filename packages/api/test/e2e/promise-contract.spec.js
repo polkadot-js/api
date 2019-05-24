ff5c49e020cedbaf6a8aaeab310685644ac1bf3d
YJ <yjkimjunior@gmail.com>
2019-05-24 13:13:47 +0200

    merge;

--- packages/api/test/e2e/promise-contract.spec.js
+++ packages/api/test/e2e/promise-contract.spec.js
@@ -22,7 +22,7 @@ describe('e2e contracts', () => {
   beforeEach(async (done) => {
     if (!api) {
       api = await Api.create();
-
+      
       keyring = testingPairs({ type: 'sr25519' });
     }
 
@@ -30,104 +30,24 @@ describe('e2e contracts', () => {
     done();
   });
 
-  describe.skip('incrementer', () => {
-    let abi;
-
-    beforeAll(() => {
-      abi = new ContractAbi(incrementer);
-    });
-
-    it('allows putCode', (done) => {
-      const code = fs.readFileSync(path.join(__dirname, '../data/incrementer-opt.wasm')).toString('hex');
-
-      api.tx.contract
-        .putCode(200000, `0x${code}`)
-        .signAndSend(keyring.eve, (result) => {
-          console.error('putCode', JSON.stringify(result));
-
-          if (result.status.isFinalized) {
-            const record = result.findRecord('contract', 'CodeStored');
-
-            if (record) {
-              codeHash = record.event.data[0];
-
-              done();
-            }
-          }
-        });
-    });
-
-    it('allows contract create', (done) => {
-      expect(codeHash).toBeDefined();
-
-      api.tx.contract
-        .create(12345, 500000, codeHash, abi.deploy(12345))
-        .signAndSend(keyring.bob, (result) => {
-          console.error('create', JSON.stringify(result));
-
-          if (result.status.isFinalized) {
-            const record = result.findRecord('contract', 'Instantiated');
-
-            if (record) {
-              address = record.event.data[1];
-
-              done();
-            }
-          }
-        });
-    });
-
-    it('allows contract call', (done) => {
-      expect(address).toBeDefined();
-
-      api.tx.contract
-        .call(address, 12345, 500000, abi.messages.inc(123))
-        .signAndSend(keyring.bob, (result) => {
-          console.error('call', JSON.stringify(result));
-
-          if (result.status.isFinalized && result.findRecord('system', 'ExtrinsicSuccess')) {
-            done();
-          }
-        });
-    });
-  });
-
-  describe.skip('erc20', () => {
-    let abi;
-
-    beforeAll(() => {
-      abi = new ContractAbi(erc20);
-    });
-
-    it('has the attached methods', () => {
-      expect(Object.keys(abi.messages)).toEqual(
-        ['totalSupply', 'balanceOf', 'allowance', 'transfer', 'approve', 'transferFrom']
-      );
-    });
-  });
-
   // A Tuple ret_type from the contract is represented as a JS array of types
-    // e.g. Basic Tuple
-    // "return_type": [
-    //   "u32",
-    //   "u32"
-    // ]
+  // e.g. Basic Tuple
+  // "return_type": [
+  //   "u32",
+  //   "u32"
+  // ]
   // A Vector ret_type from the contract is represented as as JS array of objects of paramName and the generic types
-    // e.g. Nested Vector
-    // "return_type": {
-    //   "Vec<T>": {
-    //     "T": {
-    //       "Vec<T>": {
-    //         "T": "i32"
-    //       }
-    //     }
-    //   }
-    // }
-  describe.only('generic vec and tuple return types', () => {
-    let address;
-    let codeHash;
-    let keyring;
-    let api;
+  // e.g. Nested Vector
+  // "return_type": {
+  //   "Vec<T>": {
+  //     "T": {
+  //       "Vec<T>": {
+  //         "T": "i32"
+  //       }
+  //     }
+  //   }
+  // }
+  describe('generic vec and tuple return types', () => {
     let abi;
 
     beforeAll(() => {
@@ -206,13 +126,89 @@ describe('e2e contracts', () => {
 
     });
 
-    
+
     it('should decode nested array return type', () => {
 
     });
 
     it('should decode array of tuples return type', () => {
 
+    });
+  });
+
+  describe.skip('incrementer', () => {
+    let abi;
+
+    beforeAll(() => {
+      abi = new ContractAbi(incrementer);
+    });
+
+    it('allows putCode', (done) => {
+      const code = fs.readFileSync(path.join(__dirname, '../data/incrementer-opt.wasm')).toString('hex');
+
+      api.tx.contract
+        .putCode(200000, `0x${code}`)
+        .signAndSend(keyring.eve, (result) => {
+          console.error('putCode', JSON.stringify(result));
+
+          if (result.status.isFinalized) {
+            const record = result.findRecord('contract', 'CodeStored');
+
+            if (record) {
+              codeHash = record.event.data[0];
+
+              done();
+            }
+          }
+        });
+    });
+
+    it('allows contract create', (done) => {
+      expect(codeHash).toBeDefined();
+
+      api.tx.contract
+        .create(12345, 500000, codeHash, abi.deploy(12345))
+        .signAndSend(keyring.bob, (result) => {
+          console.error('create', JSON.stringify(result));
+
+          if (result.status.isFinalized) {
+            const record = result.findRecord('contract', 'Instantiated');
+
+            if (record) {
+              address = record.event.data[1];
+
+              done();
+            }
+          }
+        });
+    });
+
+    it('allows contract call', (done) => {
+      expect(address).toBeDefined();
+
+      api.tx.contract
+        .call(address, 12345, 500000, abi.messages.inc(123))
+        .signAndSend(keyring.bob, (result) => {
+          console.error('call', JSON.stringify(result));
+
+          if (result.status.isFinalized && result.findRecord('system', 'ExtrinsicSuccess')) {
+            done();
+          }
+        });
+    });
+  });
+
+  describe.skip('erc20', () => {
+    let abi;
+
+    beforeAll(() => {
+      abi = new ContractAbi(erc20);
+    });
+
+    it('has the attached methods', () => {
+      expect(Object.keys(abi.messages)).toEqual(
+        ['totalSupply', 'balanceOf', 'allowance', 'transfer', 'approve', 'transferFrom']
+      );
     });
   });
 });
