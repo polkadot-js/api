1a959f19e3e1970f2d9dc4213176c5cebc26fbcc
Stefanie Doll <stefie.doll@googlemail.com>
2019-05-27 15:29:27 +0200

    .skip

--- packages/api/test/e2e/rx-tx.spec.ts
+++ packages/api/test/e2e/rx-tx.spec.ts
@@ -8,7 +8,7 @@ import testingPairs from '@polkadot/keyring/testingPairs';
 
 import Api from './../../src/rx';
 
-describe('Rx e2e transactions', () => {
+describe.skip('Rx e2e transactions', () => {
   const keyring = testingPairs({ type: 'ed25519' });
   let api: Api;
 
