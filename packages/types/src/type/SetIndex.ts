6771f9948bcdb9af4235c91970a523a172bd79e4
Stefanie Doll <stefie.doll@googlemail.com>
2019-06-12 19:43:41 +0200

    Added types for ApprovalFlag, SetIndex, VoterInfo (#996)
    * Added types for ApprovalFlag, SetIndex, VoterInfo
    
    * Delete VoterInfo
    
    * Fixed typo ApprovalIndex  => ApprovalFlag
    
    * Added VoterInfo
    
    * newline & whitespace

---
+++ packages/types/src/type/SetIndex.ts
@@ -0,0 +1,13 @@
+// Copyright 2017-2019 @polkadot/types authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+import U32 from '../primitive/U32';
+
+/**
+ * @name SetIndex
+ * @description
+ * Set index, implemented as a [[U32]]
+ */
+export default class SetIndex extends U32 {
+}
---
+++ packages/types/src/type/VoterInfo.ts
@@ -0,0 +1,53 @@
+// Copyright 2017-2019 @polkadot/types authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+import Struct from '../codec/Struct';
+import Balance from './Balance';
+import VoteIndex from './VoteIndex';
+
+/**
+ * @name VoterInfo
+ * @description
+ * The activity status of a voter.
+ */
+
+export default class VoterInfo extends Struct {
+  constructor (value?: any) {
+    super({
+      lastActive: VoteIndex,
+      lastWin: VoteIndex,
+      pot: Balance,
+      stake: Balance
+    }, value);
+  }
+
+  /**
+   * @description Last VoteIndex in which this voter assigned (or initialized) approvals.
+   */
+  get lastActive (): VoteIndex {
+    return this.get('lastActive') as VoteIndex;
+  }
+
+  /**
+   * @description  Last VoteIndex in which one of this voter's approvals won.
+   * Note that `last_win = N` indicates a last win at index `N-1`, hence `last_win = 0` means no win ever.
+   */
+  get lastWin (): VoteIndex {
+    return this.get('lastWin') as VoteIndex;
+  }
+
+  /**
+   * @description The amount of stored weight as a result of not winning but changing approvals.
+   */
+  get pot (): Balance {
+    return this.get('pot') as Balance;
+  }
+
+  /**
+   * @description Current staked amount. A lock equal to this value always exists.
+   */
+  get stake (): Balance {
+    return this.get('stake') as Balance;
+  }
+}
