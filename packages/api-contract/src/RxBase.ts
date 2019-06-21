6299a8e12b4c05021932571d1f7cbf21934f76d7
Jaco Greeff <jacogr@gmail.com>
2019-06-21 14:46:15 +0200

    Comment 2.x/1.x detection

--- packages/api-contract/src/RxBase.ts
+++ packages/api-contract/src/RxBase.ts
@@ -20,6 +20,8 @@ export default abstract class RxBase implements IContractBase<'rxjs'> {
       ? abi
       : new Abi(abi);
     this.api = api;
+
+    // cater for substrate 2.x & 1.x (in order)
     this.apiContracts = api.tx.contracts || api.tx.contract;
 
     assert(this.apiContracts && this.apiContracts.putCode, `You need to connect to a node with the contracts module, the metadata does not enable api.tx.contracts on this instance`);
