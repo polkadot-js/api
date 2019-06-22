17bf549eed8e638e3be20e42c72215df5422fb6e
Jaco Greeff <jacogr@gmail.com>
2019-06-22 16:32:34 +0200

    Latest polkadot metadata

--- packages/types/src/Metadata/v5/Metadata.spec.ts
+++ packages/types/src/Metadata/v5/Metadata.spec.ts
@@ -2,14 +2,24 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import latestSubstrate from './latest.substrate.v5.json';
-import rpcData from './static';
+import polkadotJson from './latest.polkadot.v5.json';
+import substrateJson from './latest.substrate.v5.json';
+import polkadotData from './static.polkadot';
+import substrateData from './static';
 import { decodeLatestSubstrate, defaultValues, toV5 } from '../util/testUtil';
 
-describe('MetadataV5', () => {
-  decodeLatestSubstrate(5, rpcData, latestSubstrate);
+describe('MetadataV5 (substrate)', () => {
+  decodeLatestSubstrate(5, substrateData, substrateJson);
 
-  toV5(5, rpcData);
+  toV5(5, substrateData);
 
-  defaultValues(rpcData);
+  defaultValues(substrateData);
+});
+
+describe('MetadataV5 (polkadot)', () => {
+  decodeLatestSubstrate(5, polkadotData, polkadotJson);
+
+  toV5(5, polkadotData);
+
+  defaultValues(polkadotData);
 });
