f4e34741c24e834803196058bd522b2547cf9c19
YJ <yjkimjunior@gmail.com>
2019-06-27 14:22:56 +0200

    feat: metadata v6, minus static and test

---
+++ packages/types/src/Metadata/v6/Metadata.spec.ts
@@ -0,0 +1,25 @@
+// Copyright 2017-2019 @polkadot/types authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+import polkadotJson from './latest.polkadot.v6.json';
+import substrateJson from './latest.substrate.v6.json';
+import polkadotData from './static.polkadot';
+import substrateData from './static';
+import { decodeLatestSubstrate, defaultValues, toV6 } from '../util/testUtil';
+
+describe('MetadataV6 (substrate)', () => {
+  decodeLatestSubstrate(6, substrateData, substrateJson);
+
+  toV6(6, substrateData);
+
+  defaultValues(substrateData);
+});
+
+describe('MetadataV6 (polkadot)', () => {
+  decodeLatestSubstrate(6, polkadotData, polkadotJson);
+
+  toV6(6, polkadotData);
+
+  defaultValues(polkadotData);
+});
