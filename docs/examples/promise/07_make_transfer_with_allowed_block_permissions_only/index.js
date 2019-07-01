d33a563bd64ab5708e71446d273d5b9396966304
Jesse Abramowitz <jesseabramowitz@hotmail.com>
2019-07-01 08:51:47 -0400

    spelling and formatting

--- docs/examples/promise/07_make_transfer_with_allowed_block_permissions_only/index.js
+++ docs/examples/promise/07_make_transfer_with_allowed_block_permissions_only/index.js
@@ -4,8 +4,6 @@ const { ApiPromise } = require('@polkadot/api');
 const { Keyring } = require('@polkadot/keyring');
 const { ExtrinsicEra } = require('@polkadot/types/type');
 
-
-
 const BOB = '5FHneW46xGXgs5mUiveU4sbTyGBzmstUspZC92UhjJM694ty';
 
 async function main () {
@@ -15,11 +13,11 @@ async function main () {
   // Constuct the keying after the API (crypto has an async init)
   const keyring = new Keyring({ type: 'sr25519' });
 
-  // Add alice to our keyring with a hard-deived path (empty phrase, so uses dev)
+  // Add alice to our keyring with a hard-derived path (empty phrase, so uses dev)
   const alice = keyring.addFromUri('//Alice');
 
   // Get nonce for account
-  const nonce = await api.query.system.accountNonce(alice.address());
+  const nonce = await api.query.system.accountNonce(alice.address);
 
   // get current block
   const signedBlock = await api.rpc.chain.getBlock();
@@ -39,7 +37,7 @@ async function main () {
   const transfer = api.tx.balances.transfer(BOB, 12345);
 
   // Sign and send the transaction using our account with a nonce and the length of blocks the transaction is valid for 
-  const hash = await transfer.signAndSend(alice, {blockHash: eraHash, era:exERA, nonce});
+  const hash = await transfer.signAndSend(alice, { blockHash: eraHash, era: exERA, nonce });
 
   console.log('Transfer sent with hash', hash.toHex());
 }
