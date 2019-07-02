1f9dff190e4062f9dfc265f88c4bf7cc885c8cef
YJ <yjkimjunior@gmail.com>
2019-07-02 23:32:12 +0200

    fix: encode as u8a

--- packages/types/src/type/Vote.ts
+++ packages/types/src/type/Vote.ts
@@ -2,7 +2,6 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import BN from 'bn.js';
 import { isBoolean, isNumber, isObject, u8aToBn, isU8a, bnToU8a, u8aConcat } from '@polkadot/util';
 
 import Conviction from './Conviction';
