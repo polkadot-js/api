2eee6cfea72e655c8d8a18af0f32cde3f2358a09
Jaco Greeff <jacogr@gmail.com>
2019-07-09 15:56:10 +0200

    Swap to Mortal transactions by default (#1081)
    * Start of mortal tx
    
    * Swap to Mortal transcations by default
    
    * Update SubmittableExtrinsic.ts
    
    * WIP (pre master merge)
    
    * Remove eraLength, allow era to specify
    
    * Oops, linting.... it happens in the best of families
    
    * WIP debugging immortals
    
    * Re-surrect with working meta, and e2e tests
    
    * Update docs/examples/promise/07_make_transfer_with_allowed_block_permissions_only/index.js
    
    Co-Authored-By: Amaury Martiny <amaury.martiny@protonmail.com>
    
    * Update docs/examples/promise/07_make_transfer_with_allowed_block_permissions_only/index.js
    
    Co-Authored-By: Amaury Martiny <amaury.martiny@protonmail.com>
    
    * Remove useless birth/death calls

--- packages/types/src/type/SignaturePayload.ts
+++ packages/types/src/type/SignaturePayload.ts
@@ -2,7 +2,7 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import { AnyNumber, AnyU8a, IKeyringPair } from '../types';
+import { AnyNumber, AnyU8a, IExtrinsicEra, IKeyringPair } from '../types';
 
 import { blake2AsU8a } from '@polkadot/util-crypto';
 
@@ -17,7 +17,7 @@ import Nonce from './NonceCompact';
 type SignaturePayloadValue = {
   nonce?: AnyNumber,
   method?: Method,
-  era?: AnyU8a | ExtrinsicEra
+  era?: AnyU8a | IExtrinsicEra
   blockHash?: AnyU8a
 };
 
