6609dc5c20e05fbf369b864cef5c9c1b6c49c06f
Jaco Greeff <jacogr@gmail.com>
2019-07-09 08:11:27 +0200

    Move api-metadata to actual api-metadata folder

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@ module.exports = Object.assign({}, config, {
   moduleNameMapper: {
     '@polkadot/api-contract(.*)$': '<rootDir>/packages/api-contract/src/$1',
     '@polkadot/api-derive(.*)$': '<rootDir>/packages/api-derive/src/$1',
-    '@polkadot/api-metadata(.*)$': '<rootDir>/packages/type-metadata/src/$1',
+    '@polkadot/api-metadata(.*)$': '<rootDir>/packages/api-metadata/src/$1',
     '@polkadot/api(.*)$': '<rootDir>/packages/api/src/$1',
     '@polkadot/rpc-(core|provider)(.*)$': '<rootDir>/packages/rpc-$1/src/$2',
     '@polkadot/jsonrpc(.*)$': '<rootDir>/packages/type-jsonrpc/src/$1',
@@ -14,9 +14,9 @@ module.exports = Object.assign({}, config, {
     '<rootDir>/packages/api/build',
     '<rootDir>/packages/api-derive/build',
     '<rootDir>/packages/api-contract/build',
+    '<rootDir>/packages/api-metadata/build',
     '<rootDir>/packages/rpc-core/build',
     '<rootDir>/packages/rpc-provider/build',
-    '<rootDir>/packages/type-metadata/build',
     '<rootDir>/packages/type-jsonrpc/build',
     '<rootDir>/packages/types/build'
   ]
