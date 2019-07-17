24756971a95047fa3a38484754746d7137461064
Jake Naviasky <jake@commonwealth.im>
2019-07-17 15:51:31 -0400

    Attempting linter fix.

--- packages/api-derive/src/elections/approvalsOf.ts
+++ packages/api-derive/src/elections/approvalsOf.ts
@@ -1,26 +1,28 @@
 import BN from 'bn.js';
-import { ApiInterfaceRx } from "@polkadot/api/types";
-import { Observable } from "rxjs";
-import { AccountId, Vector, SetIndex, ApprovalFlag } from "@polkadot/types";
-import { switchMap, map } from "rxjs/operators";
+import { ApiInterfaceRx } from '@polkadot/api/types';
+import { Observable } from 'rxjs';
+import { AccountId, Vector, SetIndex, ApprovalFlag } from '@polkadot/types';
+import { switchMap, map } from 'rxjs/operators';
 import { drr } from '../util/drr';
 
-export function approvalFlagToBool(flags: Vector<ApprovalFlag>): boolean[] {
+export function approvalFlagToBool (flags: Vector<ApprovalFlag>): boolean[] {
   const bools: boolean[] = [];
-  flags.forEach((flag: ApprovalFlag) =>
-    Array(flag.bitLength()).forEach((bit) =>
-      // for each bit in flag, do: flag & (1 << bit) !== 0
-      bools.push(!flag.toBn().uand((new BN(1)).shln(bit)).isZero())))
+  for (const flag of flags) {
+    for (const bit of Array(flag.bitLength()).map((_, i) => i)) {
+      bools.push(!flag.toBn().uand((new BN(1)).shln(bit)).isZero());
+    }
+  }
   return bools;
 }
 
 export function approvalsOf (api: ApiInterfaceRx): (who: AccountId) => Observable<Vector<ApprovalFlag>> {
   return (who: AccountId): Observable<Vector<ApprovalFlag>> =>
     api.query.elections.nextVoterSet().pipe(
-      switchMap((nextVoterSet: SetIndex) => 
-        api.query.elections.approvalsOf.multi([...Array(+nextVoterSet + 1)].map((_, i) => [who, i]))
+      switchMap((nextVoterSet: SetIndex): Observable<Vector<ApprovalFlag>[]> =>
+        api.query.elections.approvalsOf.multi([...Array(+nextVoterSet + 1)].map((_, i): number => [who, i]))
       ),
-      map((votes: Vector<ApprovalFlag>[]) => votes.find((v) => v.length > 0) || new Vector(ApprovalFlag, [])),
+      map((votes: Vector<ApprovalFlag>[]): Vector<ApprovalFlag> =>
+        votes.find((v) => v.length > 0) || new Vector(ApprovalFlag, [])),
       drr()
-    )
+    );
 }
--- packages/api-derive/src/elections/voters.ts
+++ packages/api-derive/src/elections/voters.ts
@@ -1,16 +1,17 @@
-import { ApiInterfaceRx } from "@polkadot/api/types";
-import { Observable } from "rxjs";
-import { AccountId, Vector, Option, SetIndex } from "@polkadot/types";
-import { switchMap, map } from "rxjs/operators";
+import { ApiInterfaceRx } from '@polkadot/api/types';
+import { Observable } from 'rxjs';
+import { AccountId, Vector, Option, SetIndex } from '@polkadot/types';
+import { switchMap, map } from 'rxjs/operators';
 import { drr } from '../util/drr';
 
-export function voters (api: ApiInterfaceRx): () => Observable<Array<Option<AccountId>>> {
-  return (): Observable<Array<Option<AccountId>>> =>
+export function voters (api: ApiInterfaceRx): () => Observable<Option<AccountId>[]> {
+  return (): Observable<Option<AccountId>[]> =>
     api.query.elections.nextVoterSet().pipe(
-      switchMap((nextVoterSet: SetIndex) => 
-        api.query.elections.voters.multi([...Array(+nextVoterSet + 1)].map((_, i) => i))
+      switchMap((nextVoterSet: SetIndex): Observable<Vector<Option<AccountId>>[]> =>
+        api.query.elections.voters.multi([...Array(+nextVoterSet + 1)].map((_, i): number => i))
       ),
-      map((voters: Vector<Option<AccountId>>[]) => Array.prototype.concat.apply([], voters)),
+      map((voters: Vector<Option<AccountId>>[]): Option<AccountId>[] =>
+        Array.prototype.concat.apply([], voters)),
       drr()
-    )
+    );
 }
