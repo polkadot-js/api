d0e7189c3552b55bf6e200a888bc31b90a5161eb
Keith Ingram <kwingram25@gmail.com>
2019-07-19 00:41:05 +0100

    lint

--- packages/api-derive/src/elections/voters.ts
+++ packages/api-derive/src/elections/voters.ts
@@ -27,7 +27,7 @@ export function voters (api: ApiInterfaceRx): () => Observable<{[key: string]: S
         api.query.elections.voters.multi([...Array(+nextVoterSet + 1).keys()].map((_, i) => [i])) as any as Observable<Vector<Option<AccountId>>[]>
       ),
       map((voters: Vector<Option<AccountId>>[]): {[key: string]: SetIndex} =>
-        voters.reduce((result, vec, setIndex): {[key: string]: SetIndex} => {
+        voters.reduce((result: {[key: string]: SetIndex}, vec, setIndex): {[key: string]: SetIndex} => {
           vec.forEach((e): void => {
             // re-create the index based on position 0 is [0][0] and likewise
             // 64 (0..63 in first) is [1][0] (the first index value in set 2)
@@ -39,7 +39,8 @@ export function voters (api: ApiInterfaceRx): () => Observable<{[key: string]: S
           });
 
           return result;
-        }, {} as {[key: string]: SetIndex})),
+        }, {}),
       drr()
     )
+  );
 }
