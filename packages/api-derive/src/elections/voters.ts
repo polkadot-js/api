5504962e9adf25e7d4ec02d579e2b393adf80867
Keith Ingram <kwingram25@gmail.com>
2019-07-19 01:04:35 +0100

    lint

--- packages/api-derive/src/elections/voters.ts
+++ packages/api-derive/src/elections/voters.ts
@@ -39,7 +39,7 @@ export function voters (api: ApiInterfaceRx): () => Observable<{[key: string]: S
           });
 
           return result;
-        }, {}),
+        }, {})
       ),
       drr()
     );
