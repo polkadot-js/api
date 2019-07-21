c2cf69cbfc126ac6c1653aac445a570bde3caefc
Jaco Greeff <jacogr@gmail.com>
2019-07-21 10:27:52 +0200

    Remove v2 comments referring to v1 data

--- packages/types/src/primitive/Extrinsic/v2/SignaturePayload.ts
+++ packages/types/src/primitive/Extrinsic/v2/SignaturePayload.ts
@@ -31,11 +31,6 @@ const basePayload = {
  * @description
  * A signing payload for an [[Extrinsic]]. For the final encoding, it is variable length based
  * on the contents included
- *
- *   1-8 bytes: The Transaction Compact<Index/Nonce> as provided in the transaction itself.
- *   2+ bytes: The Function Descriptor as provided in the transaction itself.
- *   1/2 bytes: The Transaction Era as provided in the transaction itself.
- *   32 bytes: The hash of the authoring block implied by the Transaction Era and the current block.
  */
 export default class SignaturePayloadV2 extends Struct {
   public constructor (value?: SignaturePayloadValueV2 | Uint8Array | string) {
