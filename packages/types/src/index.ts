edc8af90d092946de533915fc1e8506d49fd6784
Jaco Greeff <jacogr@gmail.com>
2019-07-23 21:35:39 +0200

    typo

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -2,7 +2,7 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-// NOTE When adding any types here, we need to update the coumentation links as
+// NOTE When adding any types here, we need to update the documentation links as
 // well - <root>/docs/SUMMARY.md as well as ../README.md
 
 import { assertSingletonPackage } from '@polkadot/util';
