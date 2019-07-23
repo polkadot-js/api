61bda59dde6dce13c816c68603e71622b7accdbf
Amaury Martiny <amaury.martiny@protonmail.com>
2019-07-23 15:53:01 +0200

    use u8aToU8a

--- packages/api-metadata/src/storage/fromMetadata/getHasher.ts
+++ packages/api-metadata/src/storage/fromMetadata/getHasher.ts
@@ -3,7 +3,7 @@
 // of the Apache-2.0 license. See the LICENSE file for details.
 
 import { StorageHasher } from '@polkadot/types/primitive';
-import { bufferToU8a, isString, stringToU8a, u8aConcat } from '@polkadot/util';
+import { u8aConcat, u8aToU8a } from '@polkadot/util';
 import { blake2AsU8a, xxhashAsU8a } from '@polkadot/util-crypto';
 
 type HasherInput = string | Buffer | Uint8Array;
@@ -33,18 +33,7 @@ export default function getHasher (hasher?: StorageHasher): HasherFunction {
   }
 
   if (hasher.isTwox64Concat) {
-    return (data: HasherInput): Uint8Array => {
-      let dataAsU8a: Uint8Array;
-      if (isString(data)) {
-        dataAsU8a = stringToU8a(data);
-      } else if (data instanceof Buffer) {
-        dataAsU8a = bufferToU8a(data);
-      } else {
-        dataAsU8a = data;
-      }
-
-      return u8aConcat(xxhashAsU8a(data, 64), dataAsU8a);
-    };
+    return (data: HasherInput): Uint8Array => u8aConcat(xxhashAsU8a(data, 64), u8aToU8a(data));
   }
 
   // All cases should be handled above, but if not, return Twox128 for
