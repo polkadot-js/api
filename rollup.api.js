e23883b2536eabdec151cc100e716f19c74a9f72
retotrinkler <reto@trinkler.software>
2019-08-03 12:52:35 +0200

    1) Add rollup-plugin-{commonjs, node-resolve} dependencies 2) Update rollup.api.js configs to fix global variables and unresolved dependecies warning in rollup compile step

--- rollup.api.js
+++ rollup.api.js
@@ -1,10 +1,19 @@
 import typescript from 'rollup-plugin-typescript2';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 
 export default {
   external: ['@polkadot/util', '@polkadot/util-crypto'],
   input: './packages/api/src/index.ts',
   plugins: [
-    typescript()
+    typescript(),
+    // If you have external dependencies installed from
+    // npm, you'll most likely need these plugins. In
+    // some cases you'll need additional configuration —
+    // consult the documentation for details:
+    // https://github.com/rollup/rollup-plugin-commonjs
+    resolve(),
+    commonjs()
   ],
   output: {
     file: './build/rollup/api.js',
