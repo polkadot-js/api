626c9f86d8c5df8f7aaae9cb1be94b5204cc6047
Jaco Greeff <jacogr@gmail.com>
2019-08-17 10:10:41 +0200

    Optional recentlyOffline

--- packages/api-derive/src/staking/recentlyOffline.ts
+++ packages/api-derive/src/staking/recentlyOffline.ts
@@ -6,38 +6,45 @@ import { AccountId, BlockNumber } from '@polkadot/types/interfaces';
 import { Codec } from '@polkadot/types/types';
 
 import BN from 'bn.js';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { ApiInterfaceRx } from '@polkadot/api/types';
 import { DerivedRecentlyOffline } from '../types';
 
 import { drr } from '../util/drr';
 
+type OfflineResult = [AccountId, BlockNumber, BN][] & Codec;
+
+function expandDerived (recentlyOffline: OfflineResult): DerivedRecentlyOffline {
+  return recentlyOffline.reduce((result: DerivedRecentlyOffline, [accountId, blockNumber, count]): DerivedRecentlyOffline => {
+    const key = accountId.toString();
+
+    if (!result[key]) {
+      result[key] = [];
+    }
+
+    result[key].push({
+      blockNumber,
+      count
+    });
+
+    return result;
+  }, {});
+}
+
 /**
  * @description Retrieve a keyed record of accounts recently reported to be offline
  */
 export function recentlyOffline (api: ApiInterfaceRx): () => Observable<DerivedRecentlyOffline> {
   return (): Observable<DerivedRecentlyOffline> =>
-    api.query.staking
-      .recentlyOffline<[AccountId, BlockNumber, BN][] & Codec>()
-      .pipe(
-        map((recentlyOffline): DerivedRecentlyOffline =>
-          recentlyOffline.reduce((result: DerivedRecentlyOffline, [accountId, blockNumber, count]): DerivedRecentlyOffline => {
-            const key = accountId.toString();
-
-            if (!result[key]) {
-              result[key] = [];
-            }
-
-            result[key].push({
-              blockNumber,
-              count
-            });
-
-            return result;
-          },
-          {})
-        ),
-        drr()
-      );
+    (
+      // TODO recentlyOffline  has been dropped for 2.x and replaced, figure out the
+      // replacement as actually use and implement it
+      api.query.staking.recentlyOffline
+        ? api.query.staking.recentlyOffline<OfflineResult>()
+        : of([] as unknown as OfflineResult)
+    ).pipe(
+      map(expandDerived),
+      drr()
+    );
 }
