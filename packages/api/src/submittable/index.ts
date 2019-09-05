1c63799887ce047af77131218c4f50f383ec2664
Jaco Greeff <jacogr@gmail.com>
2019-09-05 10:18:55 +0200

    Aletrnative lazy Submittable

--- packages/api/src/submittable/index.ts
+++ packages/api/src/submittable/index.ts
@@ -2,5 +2,5 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-export { default as createSubmittable } from './Extrinsic';
+export { default as createSubmittable } from './createSubmittable';
 export { default as SubmittableResult } from './Result';
