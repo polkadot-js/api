27d193006ac034a586e34a49679cb6c2cd908241
Jaco Greeff <jacogr@gmail.com>
2019-09-05 10:06:29 +0200

    Only create Submittable on demand

--- packages/api/src/submittable/Extrinsic.ts
+++ packages/api/src/submittable/Extrinsic.ts
@@ -32,230 +32,241 @@ const BLOCKTIME = 6;
 const ONE_MINUTE = 60 / BLOCKTIME;
 const DEFAULT_MORTAL_LENGTH = 5 * ONE_MINUTE;
 
-const _Extrinsic: Constructor<Extrinsic> = ClassOf('Extrinsic');
+// creates a submittable class - we are wrapping this whole thing into one monster function
+// so we can actually do this on-demand, i.e. we only create right at the point of need
+function createSubmittableClass<ApiType> (): Constructor<SubmittableExtrinsic<ApiType>> {
+  const _Extrinsic: Constructor<Extrinsic> = ClassOf('Extrinsic');
 
-class Submittable<ApiType> extends _Extrinsic implements SubmittableExtrinsic<ApiType> {
-  private readonly _api: ApiInterfaceRx;
+  return  class Submittable<ApiType> extends _Extrinsic implements SubmittableExtrinsic<ApiType> {
+    private readonly _api: ApiInterfaceRx;
 
-  private readonly _decorateMethod: ApiBase<ApiType>['decorateMethod'];
+    private readonly _decorateMethod: ApiBase<ApiType>['decorateMethod'];
 
-  private readonly _ignoreStatusCb: boolean;
+    private readonly _ignoreStatusCb: boolean;
 
-  public constructor (extrinsic: Call | Uint8Array | string, { api, decorateMethod, type }: SubmittableOptions<ApiType>) {
-    super(extrinsic, { version: api.extrinsicType });
+    public constructor (extrinsic: Call | Uint8Array | string, { api, decorateMethod, type }: SubmittableOptions<ApiType>) {
+      super(extrinsic, { version: api.extrinsicType });
 
-    this._api = api;
-    this._decorateMethod = decorateMethod;
-    this._ignoreStatusCb = type === 'rxjs';
-  }
-
-  // sign a transaction, returning the this to allow cianing, i.e. .sign(...).send()
-  public sign (account: IKeyringPair, optionsOrNonce: Partial<SignerOptions>): this {
-    // NOTE here we actually override nonce if it was specified (backwards compat for
-    // the previous signature - don't let userspace break, but allow then time to upgrade)
-    const options: Partial<SignerOptions> = isBn(optionsOrNonce) || isNumber(optionsOrNonce)
-      ? { nonce: optionsOrNonce }
-      : optionsOrNonce;
-
-    super.sign(account, this._makeSignOptions(options, {}));
-
-    return this;
-  }
-
-  // signAndSend with an immediate Hash result
-  public signAndSend(account: IKeyringPair | string | AccountId | Address, options?: Partial<SignerOptions>): SubmitableResultResult<ApiType>;
-
-  // signAndSend with a subscription, i.e. callback provided
-  public signAndSend(account: IKeyringPair | string | AccountId | Address, statusCb: Callback<SubmittableResultImpl>): SubmitableResultSubscription<ApiType>;
-
-  // signAndSend with options and a callback
-  public signAndSend(account: IKeyringPair | string | AccountId | Address, options: Partial<SignerOptions>, statusCb?: Callback<SubmittableResultImpl>): SubmitableResultSubscription<ApiType>;
-
-  // signAndSend implementation for all 3 cases above
-  public signAndSend (account: IKeyringPair | string | AccountId | Address, optionsOrStatus?: Partial<SignerOptions> | Callback<SubmittableResultImpl>, optionalStatusCb?: Callback<SubmittableResultImpl>): SubmitableResultResult<ApiType> | SubmitableResultSubscription<ApiType> {
-    const [options, statusCb] = this._makeSignAndSendOptions(optionsOrStatus, optionalStatusCb);
-    const isSubscription = this._ignoreStatusCb || !!statusCb;
-    const address = isKeyringPair(account) ? account.address : account.toString();
-    let updateId: number | undefined;
-
-    return this._decorateMethod(
-      (): Observable<Codec> => (
-        this._getPrelimState(address, options).pipe(
-          first(),
-          mergeMap(async ([nonce, header]): Promise<void> => {
-            const eraOptions = this._makeEraOptions(options, { header, nonce });
-
-            if (isKeyringPair(account)) {
-              this.sign(account, eraOptions);
-            } else {
-              updateId = await this._signViaSigner(address, eraOptions, header);
-            }
-          }),
-          switchMap((): Observable<SubmittableResultImpl> | Observable<Hash> => {
-            return isSubscription
-              ? this._subscribeObservable(updateId)
-              : this._sendObservable(updateId);
-          })
-        ) as Observable<Codec>) // FIXME This is wrong, SubmittableResult is _not_ a codec
-    )(statusCb) as SubmitableResultResult<ApiType> | SubmitableResultSubscription<ApiType>;
-  }
-
-  // send with an immediate Hash result
-  public send (): SubmitableResultResult<ApiType>;
-
-  // send with a status callback
-  public send (statusCb: Callback<SubmittableResultImpl>): SubmitableResultSubscription<ApiType>;
-
-  // send implementation for both immediate Hash and statusCb variants
-  public send (statusCb?: Callback<SubmittableResultImpl>): SubmitableResultResult<ApiType> | SubmitableResultSubscription<ApiType> {
-    const isSubscription = this._ignoreStatusCb || !!statusCb;
-
-    return this._decorateMethod(
-      isSubscription
-        ? this._subscribeObservable
-        : this._sendObservable
-    )(statusCb);
-  }
-
-  private _makeSignAndSendOptions (optionsOrStatus?: Partial<SignerOptions> | Callback<SubmittableResultImpl>, statusCb?: Callback<SubmittableResultImpl>): [Partial<SignerOptions>, Callback<SubmittableResultImpl>?] {
-    let options: Partial<SignerOptions> = {};
-
-    if (isFunction(optionsOrStatus)) {
-      statusCb = optionsOrStatus;
-    } else {
-      options = { ...optionsOrStatus };
+      this._api = api;
+      this._decorateMethod = decorateMethod;
+      this._ignoreStatusCb = type === 'rxjs';
     }
 
-    return [options, statusCb];
-  }
+    // sign a transaction, returning the this to allow cianing, i.e. .sign(...).send()
+    public sign (account: IKeyringPair, optionsOrNonce: Partial<SignerOptions>): this {
+      // NOTE here we actually override nonce if it was specified (backwards compat for
+      // the previous signature - don't let userspace break, but allow then time to upgrade)
+      const options: Partial<SignerOptions> = isBn(optionsOrNonce) || isNumber(optionsOrNonce)
+        ? { nonce: optionsOrNonce }
+        : optionsOrNonce;
 
-  private async _signViaSigner (address: string, optionsWithEra: SignatureOptions, header: Header | null): Promise<number> {
-    if (!this._api.signer) {
-      throw new Error('no signer attached');
+      super.sign(account, this._makeSignOptions(options, {}));
+
+      return this;
     }
 
-    const payload = createType('SignerPayload', {
-      ...optionsWithEra,
-      address,
-      method: this.method,
-      blockNumber: header ? header.number : 0
-    });
-    let result: SignerResult;
+    // signAndSend with an immediate Hash result
+    public signAndSend(account: IKeyringPair | string | AccountId | Address, options?: Partial<SignerOptions>): SubmitableResultResult<ApiType>;
 
-    if (this._api.signer.signPayload) {
-      result = await this._api.signer.signPayload(payload.toPayload());
-    } else if (this._api.signer.signRaw) {
-      result = await this._api.signer.signRaw(payload.toRaw());
-    } else {
-      throw new Error('Invalid signer interface, it should implement either signPayload or signRaw (or both)');
+    // signAndSend with a subscription, i.e. callback provided
+    public signAndSend(account: IKeyringPair | string | AccountId | Address, statusCb: Callback<SubmittableResultImpl>): SubmitableResultSubscription<ApiType>;
+
+    // signAndSend with options and a callback
+    public signAndSend(account: IKeyringPair | string | AccountId | Address, options: Partial<SignerOptions>, statusCb?: Callback<SubmittableResultImpl>): SubmitableResultSubscription<ApiType>;
+
+    // signAndSend implementation for all 3 cases above
+    public signAndSend (account: IKeyringPair | string | AccountId | Address, optionsOrStatus?: Partial<SignerOptions> | Callback<SubmittableResultImpl>, optionalStatusCb?: Callback<SubmittableResultImpl>): SubmitableResultResult<ApiType> | SubmitableResultSubscription<ApiType> {
+      const [options, statusCb] = this._makeSignAndSendOptions(optionsOrStatus, optionalStatusCb);
+      const isSubscription = this._ignoreStatusCb || !!statusCb;
+      const address = isKeyringPair(account) ? account.address : account.toString();
+      let updateId: number | undefined;
+
+      return this._decorateMethod(
+        (): Observable<Codec> => (
+          this._getPrelimState(address, options).pipe(
+            first(),
+            mergeMap(async ([nonce, header]): Promise<void> => {
+              const eraOptions = this._makeEraOptions(options, { header, nonce });
+
+              if (isKeyringPair(account)) {
+                this.sign(account, eraOptions);
+              } else {
+                updateId = await this._signViaSigner(address, eraOptions, header);
+              }
+            }),
+            switchMap((): Observable<SubmittableResultImpl> | Observable<Hash> => {
+              return isSubscription
+                ? this._subscribeObservable(updateId)
+                : this._sendObservable(updateId);
+            })
+          ) as Observable<Codec>) // FIXME This is wrong, SubmittableResult is _not_ a codec
+      )(statusCb) as SubmitableResultResult<ApiType> | SubmitableResultSubscription<ApiType>;
     }
 
-    // Here we explicitly call `toPayload()` again instead of working with an object
-    // (reference) as passed to the signer. This means that we are sure that the
-    // payload data is not modified from our inputs, but the signer
-    super.addSignature(address, result.signature, payload.toPayload());
+    // send with an immediate Hash result
+    public send (): SubmitableResultResult<ApiType>;
 
-    return result.id;
-  }
+    // send with a status callback
+    public send (statusCb: Callback<SubmittableResultImpl>): SubmitableResultSubscription<ApiType>;
 
-  private _makeSignOptions (options: Partial<SignerOptions>, extras: { blockHash?: Hash; era?: ExtrinsicEra; nonce?: Index }): SignatureOptions {
-    return {
-      blockHash: this._api.genesisHash,
-      ...options,
-      ...extras,
-      genesisHash: this._api.genesisHash,
-      runtimeVersion: this._api.runtimeVersion,
-      version: this._api.extrinsicType
-    } as unknown as SignatureOptions;
-  }
+    // send implementation for both immediate Hash and statusCb variants
+    public send (statusCb?: Callback<SubmittableResultImpl>): SubmitableResultResult<ApiType> | SubmitableResultSubscription<ApiType> {
+      const isSubscription = this._ignoreStatusCb || !!statusCb;
 
-  private _makeEraOptions (options: Partial<SignerOptions>, { header, nonce }: { header: Header | null; nonce: Index }): SignatureOptions {
-    if (!header) {
-      if (isNumber(options.era)) {
-        // since we have no header, it is immortal, remove any option overrides
-        // so we only supply the genesisHash and no era to the construction
-        delete options.era;
-        delete options.blockHash;
+      return this._decorateMethod(
+        isSubscription
+          ? this._subscribeObservable
+          : this._sendObservable
+      )(statusCb);
+    }
+
+    private _makeSignAndSendOptions (optionsOrStatus?: Partial<SignerOptions> | Callback<SubmittableResultImpl>, statusCb?: Callback<SubmittableResultImpl>): [Partial<SignerOptions>, Callback<SubmittableResultImpl>?] {
+      let options: Partial<SignerOptions> = {};
+
+      if (isFunction(optionsOrStatus)) {
+        statusCb = optionsOrStatus;
+      } else {
+        options = { ...optionsOrStatus };
       }
 
-      return this._makeSignOptions(options, { nonce });
+      return [options, statusCb];
     }
 
-    return this._makeSignOptions(options, {
-      blockHash: header.hash,
-      era: createType('ExtrinsicEra', {
-        current: header.number,
-        period: options.era || DEFAULT_MORTAL_LENGTH
-      }),
-      nonce
-    });
-  }
+    private async _signViaSigner (address: string, optionsWithEra: SignatureOptions, header: Header | null): Promise<number> {
+      if (!this._api.signer) {
+        throw new Error('no signer attached');
+      }
 
-  private _getPrelimState (address: string, options: Partial<SignerOptions>): Observable<[Index, Header | null]> {
-    return combineLatest([
-      // if we have a nonce already, don't retrieve the latest, use what is there
-      isUndefined(options.nonce)
-        ? this._api.query.system.accountNonce<Index>(address)
-        : of(createType('Index', options.nonce)),
-      // if we have an era provided already or eraLength is <= 0 (immortal)
-      // don't get the latest block, just pass null, handle in mergeMap
-      (isUndefined(options.era) || (isNumber(options.era) && options.era > 0))
-        ? this._api.rpc.chain.getHeader()
-        : of(null)
-    ]);
-  }
+      const payload = createType('SignerPayload', {
+        ...optionsWithEra,
+        address,
+        method: this.method,
+        blockNumber: header ? header.number : 0
+      });
+      let result: SignerResult;
 
-  private _updateSigner (updateId: number, status: Hash | SubmittableResultImpl): void {
-    if ((updateId !== -1) && this._api.signer && this._api.signer.update) {
-      this._api.signer.update(updateId, status);
-    }
-  }
+      if (this._api.signer.signPayload) {
+        result = await this._api.signer.signPayload(payload.toPayload());
+      } else if (this._api.signer.signRaw) {
+        result = await this._api.signer.signRaw(payload.toRaw());
+      } else {
+        throw new Error('Invalid signer interface, it should implement either signPayload or signRaw (or both)');
+      }
 
-  private _statusObservable (status: ExtrinsicStatus): Observable<SubmittableResultImpl> {
-    if (!status.isFinalized) {
-      return of(new SubmittableResult({ status }));
+      // Here we explicitly call `toPayload()` again instead of working with an object
+      // (reference) as passed to the signer. This means that we are sure that the
+      // payload data is not modified from our inputs, but the signer
+      super.addSignature(address, result.signature, payload.toPayload());
+
+      return result.id;
     }
 
-    const blockHash = status.asFinalized;
+    private _makeSignOptions (options: Partial<SignerOptions>, extras: { blockHash?: Hash; era?: ExtrinsicEra; nonce?: Index }): SignatureOptions {
+      return {
+        blockHash: this._api.genesisHash,
+        ...options,
+        ...extras,
+        genesisHash: this._api.genesisHash,
+        runtimeVersion: this._api.runtimeVersion,
+        version: this._api.extrinsicType
+      } as unknown as SignatureOptions;
+    }
 
-    return combineLatest([
-      this._api.rpc.chain.getBlock(blockHash),
-      this._api.query.system.events.at(blockHash) as Observable<Vec<EventRecord>>
-    ]).pipe(
-      map(([signedBlock, allEvents]): SubmittableResultImpl =>
-        new SubmittableResult({
-          events: filterEvents(this.hash, signedBlock, allEvents),
-          status
-        })
-      )
-    );
-  }
+    private _makeEraOptions (options: Partial<SignerOptions>, { header, nonce }: { header: Header | null; nonce: Index }): SignatureOptions {
+      if (!header) {
+        if (isNumber(options.era)) {
+          // since we have no header, it is immortal, remove any option overrides
+          // so we only supply the genesisHash and no era to the construction
+          delete options.era;
+          delete options.blockHash;
+        }
 
-  private _sendObservable = (updateId = -1): Observable<Hash> => {
-    return this._api.rpc.author
-      .submitExtrinsic(this)
-      .pipe(
-        tap((hash): void => {
-          this._updateSigner(updateId, hash);
-        })
+        return this._makeSignOptions(options, { nonce });
+      }
+
+      return this._makeSignOptions(options, {
+        blockHash: header.hash,
+        era: createType('ExtrinsicEra', {
+          current: header.number,
+          period: options.era || DEFAULT_MORTAL_LENGTH
+        }),
+        nonce
+      });
+    }
+
+    private _getPrelimState (address: string, options: Partial<SignerOptions>): Observable<[Index, Header | null]> {
+      return combineLatest([
+        // if we have a nonce already, don't retrieve the latest, use what is there
+        isUndefined(options.nonce)
+          ? this._api.query.system.accountNonce<Index>(address)
+          : of(createType('Index', options.nonce)),
+        // if we have an era provided already or eraLength is <= 0 (immortal)
+        // don't get the latest block, just pass null, handle in mergeMap
+        (isUndefined(options.era) || (isNumber(options.era) && options.era > 0))
+          ? this._api.rpc.chain.getHeader()
+          : of(null)
+      ]);
+    }
+
+    private _updateSigner (updateId: number, status: Hash | SubmittableResultImpl): void {
+      if ((updateId !== -1) && this._api.signer && this._api.signer.update) {
+        this._api.signer.update(updateId, status);
+      }
+    }
+
+    private _statusObservable (status: ExtrinsicStatus): Observable<SubmittableResultImpl> {
+      if (!status.isFinalized) {
+        return of(new SubmittableResult({ status }));
+      }
+
+      const blockHash = status.asFinalized;
+
+      return combineLatest([
+        this._api.rpc.chain.getBlock(blockHash),
+        this._api.query.system.events.at(blockHash) as Observable<Vec<EventRecord>>
+      ]).pipe(
+        map(([signedBlock, allEvents]): SubmittableResultImpl =>
+          new SubmittableResult({
+            events: filterEvents(this.hash, signedBlock, allEvents),
+            status
+          })
+        )
       );
-  }
+    }
 
-  private _subscribeObservable = (updateId = -1): Observable<SubmittableResultImpl> => {
-    return this._api.rpc.author
-      .submitAndWatchExtrinsic(this)
-      .pipe(
-        switchMap((status): Observable<SubmittableResultImpl> =>
-          this._statusObservable(status)
-        ),
-        tap((status): void => {
-          this._updateSigner(updateId, status);
-        })
-      );
+    private _sendObservable = (updateId = -1): Observable<Hash> => {
+      return this._api.rpc.author
+        .submitExtrinsic(this)
+        .pipe(
+          tap((hash): void => {
+            this._updateSigner(updateId, hash);
+          })
+        );
+    }
+
+    private _subscribeObservable = (updateId = -1): Observable<SubmittableResultImpl> => {
+      return this._api.rpc.author
+        .submitAndWatchExtrinsic(this)
+        .pipe(
+          switchMap((status): Observable<SubmittableResultImpl> =>
+            this._statusObservable(status)
+          ),
+          tap((status): void => {
+            this._updateSigner(updateId, status);
+          })
+        );
+    }
   }
 }
 
+let SubmittableClass: Constructor<SubmittableExtrinsic<any>>;
+
 export default function createSubmittableExtrinsic<ApiType> (type: ApiTypes, api: ApiInterfaceRx, decorateMethod: ApiBase<ApiType>['decorateMethod']): Creator<ApiType> {
-  return (extrinsic: Call | Uint8Array | string): SubmittableExtrinsic<ApiType> =>
-    new Submittable(extrinsic, { api, decorateMethod, type });
+  return (extrinsic: Call | Uint8Array | string): SubmittableExtrinsic<ApiType> => {
+    if (!SubmittableClass) {
+      SubmittableClass = createSubmittableClass();
+    }
+
+    return new SubmittableClass(extrinsic, { api, decorateMethod, type });
+  }
 }
