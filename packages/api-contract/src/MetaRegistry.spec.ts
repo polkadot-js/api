50eedba83cf93dcac9026d7571a85db1a5ffacf3
Keith Ingram <kwingram25@gmail.com>
2019-09-30 13:01:18 +0200

    rv union test

--- packages/api-contract/src/MetaRegistry.spec.ts
+++ packages/api-contract/src/MetaRegistry.spec.ts
@@ -8,20 +8,10 @@ import path from 'path';
 import MetaRegistry from './MetaRegistry';
 
 import erc20Abi from '../test/contracts/Erc20.json';
-import v2Abi from '../test/contracts/V2.json';
 import sharedVecAbi from '../test/contracts/SharedVecV2.json';
 
 describe('MetaRegistry', (): void => {
   describe('construction', (): void => {
-    it('initializes from a contract ABI (V2 Structures)', (): void => {
-      const metaRegistry = new MetaRegistry(v2Abi);
-      fs.writeFile(path.join(__dirname, 'v2.test.json'), JSON.stringify(metaRegistry.typeDefs, null, 2), function (err): void {
-        if (err) throw err;
-      });
-
-      expect(true).toBe(true);
-    });
-
     it('initializes from a contract ABI (ERC20)', (): void => {
       const metaRegistry = new MetaRegistry(erc20Abi);
       fs.writeFile(path.join(__dirname, 'erc20.test.json'), JSON.stringify(metaRegistry.typeDefs, null, 2), function (err): void {
