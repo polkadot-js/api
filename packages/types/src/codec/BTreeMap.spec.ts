352ea486b1303105ba151b3f4b84f2ed3fcd1afe
Paul M Fox <iam@yourheropaul.com>
2019-10-10 14:53:08 +0100

    BTreeMap codec type

---
+++ packages/types/src/codec/BTreeMap.spec.ts
@@ -0,0 +1,76 @@
+import '@polkadot/types/injector';
+import BTreeMap from './BTreeMap';
+import Text from '../primitive/Text';
+import U32 from '../primitive/U32';
+import { CodecTo } from '../types';
+import Struct from './Struct';
+
+const expectedMap = new Map<Text, U32>();
+expectedMap.set(new Text('bazzing'), new U32(69));
+
+describe('BTreeMap', (): void => {
+  describe('decoding', (): void => {
+    const testDecode = (type: string, input: any): void =>
+      it(`can decode from ${type}`, (): void => {
+        const s = new BTreeMap(Text, U32, input);
+
+        expect(s.toString()).toBe('{"bazzing":69}');
+      });
+
+    testDecode('map', expectedMap);
+    testDecode('hex', '0x041c62617a7a696e6745000000');
+    testDecode('Uint8Array', Uint8Array.from([4, 28, 98, 97, 122, 122, 105, 110, 103, 69, 0, 0, 0]));
+  });
+
+  describe('encoding', (): void => {
+    const testEncode = (to: CodecTo, expected: any): void =>
+      it(`can encode ${to}`, (): void => {
+        const s = new BTreeMap(Text, U32, expectedMap);
+        expect(s[to]()).toEqual(expected);
+      });
+
+    testEncode('toHex', '0x041c62617a7a696e6745000000');
+    testEncode('toJSON', { bazzing: 69 });
+    testEncode('toU8a', Uint8Array.from([4, 28, 98, 97, 122, 122, 105, 110, 103, 69, 0, 0, 0]));
+    testEncode('toString', '{"bazzing":69}');
+  });
+
+  it('decodes null', (): void => {
+    expect(
+      new (
+        BTreeMap.with(Text, U32)
+      )(null).toString()
+    ).toEqual('{}');
+  });
+
+  it('decodes within more complicated types', (): void => {
+    const s = new Struct({
+      placeholder: U32,
+      value: BTreeMap.with(Text, U32)
+    });
+    s.set('value', new BTreeMap(Text, U32, expectedMap));
+    expect(s.toString()).toBe('{"placeholder":0,"value":{"bazzing":69}}');
+  });
+
+  it('throws when it cannot decode', (): void => {
+    expect(
+      (): BTreeMap<Text, U32> => new (
+        BTreeMap.with(Text, U32)
+      )('ABC')
+    ).toThrowError(/BTreeMap: cannot decode type/);
+  });
+
+  it('correctly encodes length', (): void => {
+    expect(
+      new (
+        BTreeMap.with(Text, U32))(expectedMap).encodedLength
+    ).toEqual(13);
+  });
+
+  it('generates sane toRawTypes', (): void => {
+    expect(new (BTreeMap.with(Text, U32))().toRawType()).toBe('BTreeMap<Text,u32>');
+    expect(new (BTreeMap.with(Text, Text))().toRawType()).toBe('BTreeMap<Text,Text>');
+    expect(new (BTreeMap.with(Text, Struct.with({ a: U32, b: Text })))().toRawType())
+      .toBe('BTreeMap<Text,{"a":"u32","b":"Text"}>');
+  });
+});
---
+++ packages/types/src/codec/BTreeMap.ts
@@ -0,0 +1,183 @@
+import { Compact, U8a } from '.';
+import { compareMap, decodeU8a } from './utils';
+import { AnyJson, Constructor, Codec, IHash } from '../types';
+import { isHex, hexToU8a, isU8a, u8aConcat, u8aToHex, u8aToU8a } from '@polkadot/util';
+import { blake2AsU8a } from '@polkadot/util-crypto';
+
+export default class BTreeMap<K extends Codec = Codec, V extends Codec = Codec> extends Map<K, V> implements Codec {
+  protected _key: Constructor<K>
+  protected _val: Constructor<V>
+
+  constructor (keyT: Constructor<K>, valueT: Constructor<V>, rawValue: any) {
+    super(BTreeMap.decodeBTreeMap(keyT, valueT, rawValue));
+    this._key = keyT;
+    this._val = valueT;
+  }
+
+  /**
+   * Decode input to pass into constructor.
+   *
+   * @param keyConstructor - Type of the map key
+   * @param valueConstructor - Type of the map value
+   * @param value - Value to decode, one of:
+   * - null
+   * - undefined
+   * - hex
+   * - Uint8Array
+   * - Map<any, any>, where both key and value types are either
+   *   constructors or decodeable values for their types.
+   * @param jsonMap
+   */
+  public static decodeBTreeMap< K extends Codec = Codec, V extends Codec = Codec> (
+    KeyConstructor: Constructor<K>,
+    ValueConstructor: Constructor<V>,
+    value: Uint8Array | string | Map<any, any>): Map<K, V> {
+    if (!value) {
+      return new Map<K, V>();
+    }
+
+    // Convert hex values to UA8 and re-run the function
+    if (isHex(value)) {
+      return BTreeMap.decodeBTreeMap(
+        KeyConstructor,
+        ValueConstructor,
+        hexToU8a(value)
+      );
+    }
+
+    const output = new Map<K, V>();
+
+    // Values already in U8A can be processed
+    if (isU8a(value)) {
+      const u8a = u8aToU8a(value);
+      const [offset, length] = Compact.decodeU8a(u8a);
+
+      const types = [];
+      for (let i = 0; i < length.toNumber(); i++) {
+        types.push(KeyConstructor, ValueConstructor);
+      }
+
+      const values = decodeU8a(u8a.subarray(offset), types);
+      for (let i = 0; i < values.length; i += 2) {
+        output.set(values[i] as K, values[i + 1] as V);
+      }
+
+      return output;
+    }
+
+    // Try to handle map types
+    if (value instanceof Map) {
+      value.forEach((v: any, k: any) => {
+        let key, val;
+        try {
+          key = (k instanceof KeyConstructor) ? k : new KeyConstructor(k);
+        } catch (error) {
+          console.error('Failed to decode BTreeMap key:', error.message);
+          throw error;
+        }
+
+        try {
+          val = (v instanceof ValueConstructor) ? v : new ValueConstructor(v);
+        } catch (error) {
+          console.error('Failed to decode BTreeMap value:', error.message);
+          throw error;
+        }
+
+        output.set(key, val);
+      });
+      return output;
+    }
+
+    throw new Error('BTreeMap: cannot decode type');
+  }
+
+  public static with<K extends Codec, V extends Codec> (k: Constructor<K>, v: Constructor<V>): Constructor<BTreeMap<K, V>> {
+    return class extends BTreeMap<K, V> {
+      constructor (value?: any) {
+        super(k, v, value);
+      }
+    };
+  }
+
+  /**
+   * @description The length of the value when encoded as a Uint8Array
+   */
+  public get encodedLength (): number {
+    let len = Compact.encodeU8a(this.size).length;
+    this.forEach((v: V, k: K) => {
+      len += v.encodedLength + k.encodedLength;
+    });
+    return len;
+  }
+
+  /**
+   * @description Returns a hash of the value
+   */
+  public get hash (): IHash {
+    return new U8a(blake2AsU8a(this.toU8a(), 256));
+  }
+
+  /**
+   * @description Checks if the value is an empty value
+   */
+  public get isEmpty (): boolean {
+    return this.size === 0;
+  }
+
+  /**
+   * @description Compares the value of the input to see if there is a match
+   */
+  public eq (other?: any): boolean {
+    return compareMap(this, other);
+  }
+
+  /**
+   * @description Returns a hex string representation of the value. isLe returns a LE (number-only) representation
+   */
+  public toHex (): string {
+    return u8aToHex(this.toU8a());
+  }
+
+  /**
+   * @description Converts the Object to JSON, typically used for RPC transfers
+   */
+  public toJSON (): AnyJson {
+    const json: any = {};
+    this.forEach((v: V, k: K) => {
+      json[k.toString()] = v.toJSON();
+    });
+    return json;
+  }
+
+  /**
+   * @description Returns the base runtime type name for this instance
+   */
+  public toRawType (): string {
+    return `BTreeMap<${new this._key().toRawType()},${new this._val().toRawType()}>`;
+  }
+
+  /**
+   * @description Returns the string representation of the value
+   */
+  public toString (): string {
+    return JSON.stringify(this.toJSON());
+  }
+
+  /**
+   * @description Encodes the value as a Uint8Array as per the SCALE specifications
+   * @param isBare true when the value has none of the type-specific prefixes (internal)
+   */
+  public toU8a (isBare?: boolean): Uint8Array {
+    const encoded = new Array<Uint8Array>();
+
+    if (!isBare) {
+      encoded.push(Compact.encodeU8a(this.size));
+    }
+
+    this.forEach((v: V, k: K) => {
+      encoded.push(k.toU8a(isBare), v.toU8a(isBare));
+    });
+
+    return u8aConcat(...encoded);
+  }
+}
