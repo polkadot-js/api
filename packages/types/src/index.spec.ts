2d7bcac68cc024af3424a11d6ee04c5956312286
Amaury Martiny <amaury.martiny@protonmail.com>
2019-10-11 14:21:47 +0200

    Fix forgotten import

--- packages/types/src/index.spec.ts
+++ packages/types/src/index.spec.ts
@@ -4,7 +4,7 @@
 
 import './injector';
 
-import Metadata from '@polkadot/api-metadata';
+import Metadata from '@polkadot/api-metadata/Metadata';
 import metadataStatic from '@polkadot/api-metadata/Metadata/static';
 
 import { createTypeUnsafe } from './codec/create';
