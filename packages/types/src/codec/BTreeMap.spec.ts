c0be92721461d95a3031993a2a79057b76dce815
Paul M Fox <iam@yourheropaul.com>
2019-10-11 13:31:32 +0100

    better unit tests
    multiple value examples taken from scale codec repo

--- packages/types/src/codec/BTreeMap.spec.ts
+++ packages/types/src/codec/BTreeMap.spec.ts
@@ -10,34 +10,65 @@ import { CodecTo } from '../types';
 
 import BTreeMap from './BTreeMap';
 
-const expectedMap = new Map<Text, U32>();
-expectedMap.set(new Text('bazzing'), new U32(69));
+const mockU32TextMap = new Map<Text, U32>();
+mockU32TextMap.set(new Text('bazzing'), new U32(69));
+const mockU32TextMapString = '{"bazzing":69}';
+const mockU32TextMapObject = { bazzing: 69 };
+const mockU32TextMapHexString = '0x041c62617a7a696e6745000000';
+const mockU32TextMapUint8Array = Uint8Array.from([4, 28, 98, 97, 122, 122, 105, 110, 103, 69, 0, 0, 0]);
+
+const mockU32U32Map = new Map<U32, U32>();
+mockU32U32Map.set(new U32(1), new U32(2));
+mockU32U32Map.set(new U32(23), new U32(24));
+mockU32U32Map.set(new U32(28), new U32(30));
+mockU32U32Map.set(new U32(45), new U32(80));
+const mockU32U32MapString = '{"1":2,"23":24,"28":30,"45":80}';
+const mockU32U32MapObject = { 1: 2, 23: 24, 28: 30, 45: 80 };
+const mockU32U32MapHexString = '0x10043102000000083233180000000832381e00000008343550000000';
+const mockU32U32MapUint8Array = Uint8Array.from([16, 4, 49, 2, 0, 0, 0, 8, 50, 51, 24, 0, 0, 0, 8, 50, 56, 30, 0, 0, 0, 8, 52, 53, 80, 0, 0, 0]);
 
 describe('BTreeMap', (): void => {
   describe('decoding', (): void => {
-    const testDecode = (type: string, input: any): void =>
+    const testDecode = (type: string, input: any, output: string): void =>
       it(`can decode from ${type}`, (): void => {
         const s = new BTreeMap(Text, U32, input);
 
-        expect(s.toString()).toBe('{"bazzing":69}');
+        expect(s.toString()).toBe(output);
       });
 
-    testDecode('map', expectedMap);
-    testDecode('hex', '0x041c62617a7a696e6745000000');
-    testDecode('Uint8Array', Uint8Array.from([4, 28, 98, 97, 122, 122, 105, 110, 103, 69, 0, 0, 0]));
+    testDecode('map', mockU32TextMap, mockU32TextMapString);
+    testDecode('hex', mockU32TextMapHexString, mockU32TextMapString);
+    testDecode('Uint8Array', mockU32TextMapUint8Array, mockU32TextMapString);
+
+    testDecode('map', mockU32U32Map, mockU32U32MapString);
+    testDecode('hex', mockU32U32MapHexString, mockU32U32MapString);
+    testDecode('Uint8Array', mockU32U32MapUint8Array,mockU32U32MapString);
   });
 
   describe('encoding', (): void => {
     const testEncode = (to: CodecTo, expected: any): void =>
       it(`can encode ${to}`, (): void => {
-        const s = new BTreeMap(Text, U32, expectedMap);
+        const s = new BTreeMap(Text, U32, mockU32TextMap);
         expect(s[to]()).toEqual(expected);
       });
 
-    testEncode('toHex', '0x041c62617a7a696e6745000000');
-    testEncode('toJSON', { bazzing: 69 });
-    testEncode('toU8a', Uint8Array.from([4, 28, 98, 97, 122, 122, 105, 110, 103, 69, 0, 0, 0]));
-    testEncode('toString', '{"bazzing":69}');
+    testEncode('toHex', mockU32TextMapHexString);
+    testEncode('toJSON', mockU32TextMapObject);
+    testEncode('toU8a', mockU32TextMapUint8Array);
+    testEncode('toString', mockU32TextMapString);
+  });
+
+  describe('encoding muple values', (): void => {
+    const testEncode = (to: CodecTo, expected: any): void =>
+      it(`can encode ${to}`, (): void => {
+        const s = new BTreeMap(Text, U32, mockU32U32Map);
+        expect(s[to]()).toEqual(expected);
+      });
+
+    testEncode('toHex', mockU32U32MapHexString);
+    testEncode('toJSON', mockU32U32MapObject);
+    testEncode('toU8a', mockU32U32MapUint8Array);
+    testEncode('toString', mockU32U32MapString);
   });
 
   it('decodes null', (): void => {
@@ -53,7 +84,7 @@ describe('BTreeMap', (): void => {
       placeholder: U32,
       value: BTreeMap.with(Text, U32)
     });
-    s.set('value', new BTreeMap(Text, U32, expectedMap));
+    s.set('value', new BTreeMap(Text, U32, mockU32TextMap));
     expect(s.toString()).toBe('{"placeholder":0,"value":{"bazzing":69}}');
   });
 
@@ -68,7 +99,7 @@ describe('BTreeMap', (): void => {
   it('correctly encodes length', (): void => {
     expect(
       new (
-        BTreeMap.with(Text, U32))(expectedMap).encodedLength
+        BTreeMap.with(Text, U32))(mockU32TextMap).encodedLength
     ).toEqual(13);
   });
 
