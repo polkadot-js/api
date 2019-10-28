5d794324b98ca541dc1c7dc20028b0f0b3643839
Jaco Greeff <jacogr@gmail.com>
2019-10-28 11:24:38 +0100

    Re-enable account_nextIndex of submittables

--- packages/api/src/submittable/Submittable.ts
+++ packages/api/src/submittable/Submittable.ts
@@ -193,12 +193,9 @@ export default class Submittable<ApiType> extends _Extrinsic implements Submitta
     return combineLatest([
       // if we have a nonce already, don't retrieve the latest, use what is there
       isUndefined(options.nonce)
-        // FIXME This apparently is having issues on latest Kusama for nonce retrieval,
-        // hence we are using the accountNonce only
-        // ? this._api.rpc.account.nextIndex
-        //   ? this._api.rpc.account.nextIndex(address)
-        //   : this._api.query.system.accountNonce(address)
-        ? this._api.query.system.accountNonce(address)
+        ? this._api.rpc.account && this._api.rpc.account.nextIndex
+          ? this._api.rpc.account.nextIndex(address)
+          : this._api.query.system.accountNonce(address)
         : of(createType('Index', options.nonce)),
       // if we have an era provided already or eraLength is <= 0 (immortal)
       // don't get the latest block, just pass null, handle in mergeMap
