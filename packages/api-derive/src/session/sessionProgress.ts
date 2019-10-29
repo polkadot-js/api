41cf32c8089a6f3b56cd4a416384c235581c98f2
Jaco Greeff <jacogr@gmail.com>
2019-10-29 17:49:42 +0100

    Flatten more combineLatest into queryMulti (#1528)

--- packages/api-derive/src/session/sessionProgress.ts
+++ packages/api-derive/src/session/sessionProgress.ts
@@ -2,7 +2,8 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import BN from 'bn.js';
+import { BlockNumber } from '@polkadot/types/interfaces';
+
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { ApiInterfaceRx } from '@polkadot/api/types';
@@ -10,10 +11,10 @@ import { ApiInterfaceRx } from '@polkadot/api/types';
 import { drr } from '../util/drr';
 import { info } from './info';
 
-export function sessionProgress (api: ApiInterfaceRx): () => Observable<BN> {
-  return (): Observable<BN> =>
+export function sessionProgress (api: ApiInterfaceRx): () => Observable<BlockNumber> {
+  return (): Observable<BlockNumber> =>
     info(api)().pipe(
-      map(({ sessionProgress }): BN => sessionProgress),
+      map(({ sessionProgress }): BlockNumber => sessionProgress),
       drr()
     );
 }
