e34ea8c2ab1e2e17587b6f1fcf68db7fcbfac4e6
Jaco Greeff <jacogr@gmail.com>
2019-11-13 08:54:09 +0100

    Display elections runnersUp in reverse order

--- packages/api-derive/src/staking/controllers.ts
+++ packages/api-derive/src/staking/controllers.ts
@@ -3,22 +3,24 @@
 // of the Apache-2.0 license. See the LICENSE file for details.
 
 import { ApiInterfaceRx } from '@polkadot/api/types';
-import { AccountId } from '@polkadot/types/interfaces';
-import { Codec } from '@polkadot/types/types';
+import { AccountId, ValidatorPrefs } from '@polkadot/types/interfaces';
+import { ITuple } from '@polkadot/types/types';
 
 import { Observable, combineLatest, of } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
-import { Option } from '@polkadot/types';
+import { Option, Vec } from '@polkadot/types';
 
 import { memo } from '../util';
+
+type DeriveControllers = [AccountId[], Option<AccountId>[]];
 
 /**
  * @description From the list of stash accounts, retrieve the list of controllers
  */
-export function controllers (api: ApiInterfaceRx): () => Observable<[AccountId[], Option<AccountId>[]]> {
+export function controllers (api: ApiInterfaceRx): () => Observable<DeriveControllers> {
   return memo((): Observable<[AccountId[], Option<AccountId>[]]> =>
-    api.query.staking.validators<[AccountId[]] & Codec>().pipe(
-      switchMap(([stashIds]): Observable<[AccountId[], Option<AccountId>[]]> =>
+    api.query.staking.validators<ITuple<[Vec<AccountId>, Vec<ValidatorPrefs>]>>().pipe(
+      switchMap(([stashIds]): Observable<DeriveControllers> =>
         combineLatest([
           of(stashIds),
           // for V2, don't return all the controllers, we call bonded at a later point
