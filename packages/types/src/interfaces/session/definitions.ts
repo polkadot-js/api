3707af4ff426b4ff281e3314c6da2eefbda3f4ad
Jaco Greeff <jacogr@gmail.com>
2019-11-14 14:35:10 +0100

    Update session keys (with AuthorityDiscovery)

--- packages/types/src/interfaces/session/definitions.ts
+++ packages/types/src/interfaces/session/definitions.ts
@@ -2,21 +2,32 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
+// The runtime definition of SessionKeys are passes as a Trait to session
+// Defined in `node/runtime/src/lib.rs` as follow
+//   impl_opaque_keys! {
+//     pub struct SessionKeys {
+// Here we revert to tuples to keep the interfaces "opaque", as per the use
+const keyTypes = {
+  // default to Substrate
+  Keys: 'SessionKeysSubstrate',
+
+  // shortcuts for 3, 4 & 5 key tuples
+  SessionKeys3: '(AccountId, AccountId, AccountId)',
+  SessionKeys4: '(AccountId, AccountId, AccountId, AccountId)',
+  SessionKeys5: '(AccountId, AccountId, AccountId, AccountId, AccountId)',
+
+  // For substrate: Grandpa, Babe, ImOnline, AuthorityDiscovery
+  SessionKeysSubstrate: 'SessionKeys4',
+
+  // For polkadot: Grandpa, Babe, ImOnline, Parachains
+  SessionKeysPolkadot: 'SessionKeys4'
+};
+
 export default {
   types: {
+    ...keyTypes,
     FullIdentification: 'Exposure',
     IdentificationTuple: '(ValidatorId, FullIdentification)',
-    SessionIndex: 'u32',
-
-    // The runtime definition of SessionKeys are passes as a Trait to session
-    // Defined in `node/runtime/src/lib.rs` as follow
-    //   impl_opaque_keys! {
-    //     pub struct SessionKeys {
-    // Here we revert to tuples to keep the interfaces "opaque", as per the use
-    Keys: 'SessionKeysSubstrate',
-    // For substrate: Grandpa, Babe, ImOnline
-    SessionKeysSubstrate: '(AccountId, AccountId, AccountId)',
-    // For polkadot: Grandpa, Babe, ImOnline, Parachains
-    SessionKeysPolkadot: '(AccountId, AccountId, AccountId, AccountId)'
+    SessionIndex: 'u32'
   }
 };
