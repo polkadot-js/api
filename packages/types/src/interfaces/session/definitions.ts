046e087795419ae861f169db77f416770095de72
Jaco Greeff <jacogr@gmail.com>
2019-11-27 21:38:58 +0100

    SessionKey5 based on CC3 specVersion

--- packages/types/src/interfaces/session/definitions.ts
+++ packages/types/src/interfaces/session/definitions.ts
@@ -8,19 +8,19 @@
 //     pub struct SessionKeys {
 // Here we revert to tuples to keep the interfaces "opaque", as per the use
 const keyTypes = {
-  // default to Substrate
-  Keys: 'SessionKeysSubstrate',
+  // default to Substrate master defaults, 4 keys (polkadot master, 5 keys)
+  Keys: 'SessionKeys4',
 
-  // shortcuts for 3, 4 & 5 key tuples
+  // shortcuts for 1, 2, 3, 4, 5 & 6 key tuples
+  SessionKeys1: '(AccountId)',
+  SessionKeys2: '(AccountId, AccountId)',
+  // older substrate master
   SessionKeys3: '(AccountId, AccountId, AccountId)',
+  // CC2, Substrate master
   SessionKeys4: '(AccountId, AccountId, AccountId, AccountId)',
+  // CC3
   SessionKeys5: '(AccountId, AccountId, AccountId, AccountId, AccountId)',
-
-  // For substrate: Grandpa, Babe, ImOnline, AuthorityDiscovery
-  SessionKeysSubstrate: 'SessionKeys4',
-
-  // For polkadot: Grandpa, Babe, ImOnline, Parachains, AuthorityDiscovery
-  SessionKeysPolkadot: 'SessionKeys5'
+  SessionKeys6: '(AccountId, AccountId, AccountId, AccountId, AccountId, AccountId)'
 };
 
 export default {
