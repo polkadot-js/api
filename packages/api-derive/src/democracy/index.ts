23aece7c865c0dcaadebdeb5f642a2e4ab44d3c4
Jaco Greeff <jacogr@gmail.com>
2019-12-01 21:08:52 +0100

    Add derive.democracy.proposals

--- packages/api-derive/src/democracy/index.ts
+++ packages/api-derive/src/democracy/index.ts
@@ -2,6 +2,7 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
+export * from './proposals';
 export * from './referendumInfos';
 export * from './referendums';
 export * from './referendumVotesFor';
