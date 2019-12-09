df0b2160a04401491f1324a5677fc8348a02096b
Keith Ingram <kwingram25@gmail.com>
2019-12-09 15:34:30 -0500

    Combine elections.info derive

--- packages/api-derive/src/elections/index.ts
+++ packages/api-derive/src/elections/index.ts
@@ -2,8 +2,4 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-export * from './approvalsOf';
-export * from './approvalsOfAt';
 export * from './info';
-export * from './voters';
-export * from './voterPositions';
