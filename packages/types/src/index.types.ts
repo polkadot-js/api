a22e5935ae9d05f38a864ce5694b5e804e40312e
Jaco Greeff <jacogr@gmail.com>
2019-12-09 14:52:40 +0100

    Explicitly add U8a to the registry

--- packages/types/src/index.types.ts
+++ packages/types/src/index.types.ts
@@ -2,7 +2,8 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
+/**
+ * @summary Type definitions that are used in the system
+ */
 export { default as Metadata } from '@polkadot/metadata/Metadata';
-
-export { default as U8a } from './codec/U8a';
 export * from './primitive';
