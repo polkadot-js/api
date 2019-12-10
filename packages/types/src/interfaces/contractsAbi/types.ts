0d2c2b4ddc1fcc763160c4edfad8fa0d9a0fecb3
Jaco Greeff <jacogr@gmail.com>
2019-12-10 23:15:00 +0100

    Run type generation

--- packages/types/src/interfaces/contractsAbi/types.ts
+++ packages/types/src/interfaces/contractsAbi/types.ts
@@ -1,136 +1,81 @@
 // Auto-generated via `yarn build:interfaces`, do not edit
 /* eslint-disable @typescript-eslint/no-empty-interface */
 
-import { Codec } from '@polkadot/types/types';
-import { Compact, Enum, Option, Raw, Struct } from '@polkadot/types/codec';
-import { Bytes, Null, bool, u32, u64, u8 } from '@polkadot/types/primitive';
-import { AccountId, Balance, BlockNumber, Hash } from '@polkadot/types/interfaces/runtime';
+import { Option, Raw, Struct, Vec } from '@polkadot/types/codec';
+import { Text, bool, u32 } from '@polkadot/types/primitive';
 
 /** Struct */
-export interface AliveContractInfo extends Struct {
-  /** TrieId */
-  readonly trieId: TrieId;
+export interface AbiArg extends Struct {
   /** u32 */
-  readonly storageSize: u32;
-  /** CodeHash */
-  readonly codeHash: CodeHash;
-  /** Balance */
-  readonly rentAllowance: Balance;
-  /** BlockNumber */
-  readonly deductBlock: BlockNumber;
-  /** Option<BlockNumber> */
-  readonly lastWrite: Option<BlockNumber>;
-}
-
-/** Hash */
-export interface CodeHash extends Hash {}
-
-/** Struct */
-export interface ContractCallRequest extends Struct {
-  /** AccountId */
-  readonly origin: AccountId;
-  /** AccountId */
-  readonly dest: AccountId;
-  /** Balance */
-  readonly value: Balance;
-  /** u64 */
-  readonly gasLimit: u64;
-  /** Bytes */
-  readonly inputData: Bytes;
-}
-
-/** Enum */
-export interface ContractExecResult extends Enum {
-  /** 0:: Success(ContractExecResultSuccess) */
-  readonly isSuccess: boolean;
-  /** ContractExecResultSuccess */
-  readonly asSuccess: ContractExecResultSuccess;
-  /** 1:: Error */
-  readonly isError: boolean;
+  readonly name: u32;
 }
 
 /** Struct */
-export interface ContractExecResultSuccess extends Struct {
-  /** u8 */
-  readonly status: u8;
+export interface AbiConstructor extends Struct {
+  /** u32 */
+  readonly name: u32;
   /** Raw */
-  readonly data: Raw;
+  readonly selector: Raw;
+  /** Vec<AbiArg> */
+  readonly args: Vec<AbiArg>;
+  /** Vec<Text> */
+  readonly docs: Vec<Text>;
 }
-
-/** Enum */
-export interface ContractInfo extends Enum {
-  /** 0:: Alive(AliveContractInfo) */
-  readonly isAlive: boolean;
-  /** AliveContractInfo */
-  readonly asAlive: AliveContractInfo;
-  /** 1:: Tombstone(TombstoneContractInfo) */
-  readonly isTombstone: boolean;
-  /** TombstoneContractInfo */
-  readonly asTombstone: TombstoneContractInfo;
-}
-
-/** Uint8Array, Codec */
-export interface ContractStorageKey extends Uint8Array, Codec {}
-
-/** u64 */
-export interface Gas extends u64 {}
 
 /** Struct */
-export interface PrefabWasmModule extends Struct {
-  /** Compact<u32> */
-  readonly scheduleVersion: Compact<u32>;
-  /** Compact<u32> */
-  readonly initial: Compact<u32>;
-  /** Compact<u32> */
-  readonly maximum: Compact<u32>;
-  /** PrefabWasmModuleReserved */
-  readonly _reserved: PrefabWasmModuleReserved;
-  /** Bytes */
-  readonly code: Bytes;
+export interface AbiContract extends Struct {
+  /** u32 */
+  readonly name: u32;
+  /** Vec<AbiConstructor> */
+  readonly constructors: Vec<AbiConstructor>;
+  /** Vec<AbiMessage> */
+  readonly messages: Vec<AbiMessage>;
+  /** Vec<AbiEvent> */
+  readonly events: Vec<AbiEvent>;
+  /** Vec<Text> */
+  readonly docs: Vec<Text>;
 }
 
-/** Option<Null> */
-export interface PrefabWasmModuleReserved extends Option<Null> {}
+/** Struct */
+export interface AbiDef extends Struct {
+  /** AbiRegistry */
+  readonly registry: AbiRegistry;
+  /** AbiTypes */
+  readonly types: AbiTypes;
+  /** AbiStorage */
+  readonly storage: AbiStorage;
+  /** AbiContract */
+  readonly contracts: AbiContract;
+}
 
 /** Struct */
-export interface Schedule extends Struct {
+export interface AbiEvent extends Struct {}
+
+/** Struct */
+export interface AbiMessage extends Struct {
   /** u32 */
-  readonly version: u32;
-  /** Gas */
-  readonly putCodePerByteCost: Gas;
-  /** Gas */
-  readonly growMemCost: Gas;
-  /** Gas */
-  readonly regularOpCost: Gas;
-  /** Gas */
-  readonly returnDataPerByteCost: Gas;
-  /** Gas */
-  readonly eventDataPerByteCost: Gas;
-  /** Gas */
-  readonly eventPerTopicCost: Gas;
-  /** Gas */
-  readonly eventBaseCost: Gas;
-  /** Gas */
-  readonly sandboxDataReadCost: Gas;
-  /** Gas */
-  readonly sandboxDataWriteCost: Gas;
-  /** u32 */
-  readonly maxEventTopics: u32;
-  /** u32 */
-  readonly maxStackHeight: u32;
-  /** u32 */
-  readonly maxMemoryPages: u32;
+  readonly name: u32;
+  /** Raw */
+  readonly selector: Raw;
   /** bool */
-  readonly enablePrintln: bool;
-  /** u32 */
-  readonly maxSubjectLen: u32;
+  readonly mutates: bool;
+  /** Vec<AbiArg> */
+  readonly args: Vec<AbiArg>;
+  /** Option<AbiReturn> */
+  readonly returnType: Option<AbiReturn>;
 }
 
-/** Hash */
-export interface SeedOf extends Hash {}
+/** Struct */
+export interface AbiRegistry extends Struct {
+  /** Vec<Text> */
+  readonly strings: Vec<Text>;
+}
 
-/** Hash */
-export interface TombstoneContractInfo extends Hash {}
+/** Struct */
+export interface AbiReturn extends Struct {}
 
-/** Bytes */
-export interface TrieId extends Bytes {}
+/** Struct */
+export interface AbiStorage extends Struct {}
+
+/** Struct */
+export interface AbiTypes extends Struct {}
