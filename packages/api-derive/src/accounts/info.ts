8762d3159a76f12e554237742adce6c5052c9a67
Jaco Greeff <jacogr@gmail.com>
2019-12-15 11:54:38 +0100

    Add identity names to derive.accounts.info

--- packages/api-derive/src/accounts/info.ts
+++ packages/api-derive/src/accounts/info.ts
@@ -3,9 +3,9 @@
 // of the Apache-2.0 license. See the LICENSE file for details.
 
 import { ApiInterfaceRx } from '@polkadot/api/types';
-import { AccountId, AccountIndex, Address, Balance } from '@polkadot/types/interfaces';
+import { AccountId, AccountIndex, Address, Balance, Registration } from '@polkadot/types/interfaces';
 import { ITuple } from '@polkadot/types/types';
-import { DeriveAccountInfo } from '../types';
+import { DeriveAccountInfo, DeriveAccountRegistration } from '../types';
 
 import { Observable, combineLatest, of } from 'rxjs';
 import { map, switchMap } from 'rxjs/operators';
@@ -16,7 +16,7 @@ import { memo } from '../util';
 
 function retrieveNick (api: ApiInterfaceRx, accountId?: AccountId): Observable<string | undefined> {
   return ((
-    accountId && api.query.nicks
+    accountId && api.query.nicks?.nameOf
       ? api.query.nicks.nameOf<Option<ITuple<[Bytes, Balance]>>>(accountId)
       : of(undefined)
   ) as Observable<Option<ITuple<[Bytes, Balance]>> | undefined>).pipe(
@@ -28,6 +28,29 @@ function retrieveNick (api: ApiInterfaceRx, accountId?: AccountId): Observable<s
   );
 }
 
+function retrieveIdentity (api: ApiInterfaceRx, accountId?: AccountId): Observable<DeriveAccountRegistration> {
+  return ((
+    accountId && api.query.identity?.identityOf
+      ? api.query.identity.identityOf<Option<Registration>>(accountId)
+      : of(undefined)
+  ) as Observable<Option<Registration> | undefined>).pipe(
+    map((identityOfOpt): DeriveAccountRegistration => {
+      if (!identityOfOpt?.isSome) {
+        return { judgements: [] };
+      }
+
+      const { info, judgements } = identityOfOpt.unwrap();
+
+      return {
+        displayName: info.display.isRaw
+          ? u8aToString(info.display.asRaw.toU8a(true))
+          : undefined,
+        judgements
+      };
+    })
+  );
+}
+
 /**
  * @name info
  * @description Returns aux. info with regards to an account, current that includes the accountId, accountIndex and nickname
@@ -35,14 +58,15 @@ function retrieveNick (api: ApiInterfaceRx, accountId?: AccountId): Observable<s
 export function info (api: ApiInterfaceRx): (address?: AccountIndex | AccountId | Address | string | null) => Observable<DeriveAccountInfo> {
   return memo((address?: AccountIndex | AccountId | Address | string | null): Observable<DeriveAccountInfo> =>
     api.derive.accounts.idAndIndex(address).pipe(
-      switchMap(([accountId, accountIndex]): Observable<[DeriveAccountInfo, string?]> =>
+      switchMap(([accountId, accountIndex]): Observable<[Partial<DeriveAccountInfo>, DeriveAccountRegistration, string?]> =>
         combineLatest([
           of({ accountId, accountIndex }),
+          retrieveIdentity(api, accountId),
           retrieveNick(api, accountId)
         ])
       ),
-      map(([{ accountId, accountIndex }, nickname]): DeriveAccountInfo => ({
-        accountId, accountIndex, nickname
+      map(([{ accountId, accountIndex }, identity, nickname]): DeriveAccountInfo => ({
+        accountId, accountIndex, identity, nickname
       }))
     ));
 }
