e4ae5b020471d666029e6296209fd4ed4d8fc1b3
satellitex <s.a.t.e.3.ths@gmail.com>
2019-12-16 17:28:05 +0900

    fix compareSet

--- packages/types/src/codec/utils/compareSet.ts
+++ packages/types/src/codec/utils/compareSet.ts
@@ -3,18 +3,13 @@
 // of the Apache-2.0 license. See the LICENSE file for details.
 
 import { isObject } from '@polkadot/util';
-function notEntry (value: any): boolean {
-  return !Array.isArray(value) || value.length !== 2;
-}
 
 // NOTE These are used internally and when comparing objects, expects that
 // when the second is an Set<string, Codec> that the first has to be as well
 export default function compareSet (a: Set<any>, b?: any): boolean {
   if (Array.isArray(b)) {
     // equal number of entries and each entry in the array should match
-    return (a.size === b.length) && !b.some((entry): boolean =>
-      notEntry(entry) || a.has(entry)
-    );
+    return (a.size === b.length) && !b.some((entry): boolean => !a.has(entry));
   } else if (b instanceof Set) {
     return compareSet(a, [...b.entries()]);
   } else if (isObject(b)) {
