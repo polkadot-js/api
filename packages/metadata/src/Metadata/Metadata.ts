a1d77592dee70560207a00357ce6bb3f3cc4cc29
Amaury Martiny <amaury.martiny@protonmail.com>
2020-01-02 17:32:46 +0100

    Add HACK

--- packages/metadata/src/Metadata/Metadata.ts
+++ packages/metadata/src/Metadata/Metadata.ts
@@ -23,6 +23,17 @@ export default class Metadata extends MetadataVersioned {
   // we adjust with the magic number and a manual version and re-try. As soon as
   // we remove support for V0, we will just do a new here
   private static decodeMetadata (registry: Registry, _value: Uint8Array | string = new Uint8Array()): MetadataVersioned {
+    /**
+     * HACK 01.02.2020
+     * There was a fk-up in the metadata upgrade process: Kusama's metadata got
+     * updated to a non-backward-compatible way, but the metadata version
+     * stayed at v9. Then v10 was merged in Substrate but not Kusama. Here,
+     * we're just saying that if it's a v9 metadata, decode as v10.
+     */
+    if (isHex(_value) && _value.startsWith('0x6d65746109')) {
+      return Metadata.decodeMetadata(registry, _value.replace('0x6d65746109', '0x6d6574610a'));
+    }
+
     const value = isHex(_value)
       ? hexToU8a(_value)
       : _value;
