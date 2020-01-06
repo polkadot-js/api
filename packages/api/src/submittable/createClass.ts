b7eeb992cd1f05d77bb5371a1ffa3894f968f042
Jaco Greeff <jacogr@gmail.com>
2020-01-06 14:12:49 +0100

    Bump deps (#1699)

--- packages/api/src/submittable/createClass.ts
+++ packages/api/src/submittable/createClass.ts
@@ -1,5 +1,5 @@
 /* eslint-disable no-dupe-class-members */
-// Copyright 2017-2019 @polkadot/api authors & contributors
+// Copyright 2017-2020 @polkadot/api authors & contributors
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
