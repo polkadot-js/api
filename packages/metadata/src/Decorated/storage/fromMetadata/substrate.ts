82cca4895e04ceb8d13042c9fddf112c1016fe38
Jaco Greeff <jacogr@gmail.com>
2020-01-13 15:32:32 +0100

    Remove old known keys

--- packages/metadata/src/Decorated/storage/fromMetadata/substrate.ts
+++ packages/metadata/src/Decorated/storage/fromMetadata/substrate.ts
@@ -31,18 +31,6 @@ function createRuntimeFunction (method: string, key: string, { documentation, ty
     }, { key, metaVersion, skipHashing: true });
 }
 
-// @deprecated: The ':auth:' (authorityPrefix) and ':auth:len' (authorityCount) storage keys
-// have been removed in https://github.com/paritytech/substrate/pull/2802
-export const authorityCount = createRuntimeFunction('authorityCount', ':auth:len', {
-  documentation: 'Number of authorities.',
-  type: 'u32'
-});
-
-export const authorityPrefix = createRuntimeFunction('authorityPrefix', ':auth:', {
-  documentation: 'Prefix under which authorities are stored.',
-  type: 'u32'
-});
-
 export const code = createRuntimeFunction('code', ':code', {
   documentation: 'Wasm code of the runtime.',
   type: 'Bytes'
