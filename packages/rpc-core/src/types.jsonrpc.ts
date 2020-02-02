5e0c0ba91a1ae2536ec02a779c5fc79a40a776aa
Jaco Greeff <jacogr@gmail.com>
2020-02-02 20:16:41 +0100

    Align jsonrpc augmentation with others (#1796)

---
+++ packages/rpc-core/src/types.jsonrpc.ts
@@ -0,0 +1,6 @@
+// Copyright 2017-2020 @polkadot/rpc-core authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface RpcInterface {}
