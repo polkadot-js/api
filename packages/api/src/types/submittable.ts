20c7e534abe712877d66589bb302cea3a93dab46
Jaco Greeff <jacogr@gmail.com>
2020-02-04 11:47:39 +0100

    Adjust submittable augmentation

--- packages/api/src/types/submittable.ts
+++ packages/api/src/types/submittable.ts
@@ -2,7 +2,7 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import { AnyFunction, CallFunction } from '@polkadot/types/types';
+import { AnyFunction, CallBase } from '@polkadot/types/types';
 
 import { SubmittableExtrinsic } from '../submittable/types';
 import { ApiTypes } from './base';
@@ -10,9 +10,9 @@ import { ApiTypes } from './base';
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface AugmentedSubmittables<ApiType extends ApiTypes> { }
 
-export type AugmentedSubmittable<T extends AnyFunction> = T & CallFunction;
+export type AugmentedSubmittable<T extends AnyFunction> = T & CallBase;
 
-export interface SubmittableExtrinsicFunction<ApiType extends ApiTypes> extends CallFunction {
+export interface SubmittableExtrinsicFunction<ApiType extends ApiTypes> extends CallBase {
   (...params: any[]): SubmittableExtrinsic<ApiType>;
 }
 
