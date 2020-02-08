b6534d2958f383262476d1e7f91d05028bb9964b
Jaco Greeff <jacogr@gmail.com>
2020-02-08 13:11:33 +0100

    Polkadot V11 metadata (#1821)

--- packages/metadata/src/Metadata/v11/Metadata.spec.ts
+++ packages/metadata/src/Metadata/v11/Metadata.spec.ts
@@ -4,11 +4,13 @@
 
 import { TypeRegistry } from '@polkadot/types';
 
+import polkadotJson from './static-polkadot.json';
+import polkadotData from './static.polkadot';
 import substrateJson from './static-substrate.json';
 import substrateData from './static';
 import { decodeLatestSubstrate, defaultValues, toLatest } from '../util/testUtil';
 
-describe('MetadataV10 (substrate)', (): void => {
+describe('MetadataV11 (substrate)', (): void => {
   const registry = new TypeRegistry();
 
   decodeLatestSubstrate(registry, 11, substrateData, substrateJson);
@@ -16,4 +18,14 @@ describe('MetadataV10 (substrate)', (): void => {
   toLatest(registry, 11, substrateData);
 
   defaultValues(registry, substrateData);
+});
+
+describe('MetadataV11 (polkadot)', (): void => {
+  const registry = new TypeRegistry();
+
+  decodeLatestSubstrate(registry, 11, polkadotData, polkadotJson);
+
+  toLatest(registry, 11, polkadotData);
+
+  defaultValues(registry, polkadotData);
 });
