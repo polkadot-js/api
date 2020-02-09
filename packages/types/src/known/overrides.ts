7d49fe5505a57661aeac6d1277331f7cabab7f68
Jaco Greeff <jacogr@gmail.com>
2020-02-09 15:53:07 +0100

    Adjust overrides

--- packages/types/src/known/overrides.ts
+++ packages/types/src/known/overrides.ts
@@ -32,6 +32,8 @@ const TYPES_POLKADOT_VERSIONED: OverrideVersionedType[] = [
   {
     minmax: [1000, undefined], // from launch
     types: {
+      // Indices optinal, not in transaction
+      Address: 'AccountId',
       Keys: 'SessionKeys5'
     }
   }
@@ -68,6 +70,7 @@ const TYPES_KUSAMA_VERSIONED: OverrideVersionedType[] = [
   {
     minmax: [1046, undefined],
     types: {
+      // Indices optinal, not in transaction
       Address: 'AccountId',
       Keys: 'SessionKeys5'
     }
