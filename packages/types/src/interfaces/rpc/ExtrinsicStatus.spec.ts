ee55811b99aadf53a44768bc9ec9c86f62483a76
Jaco Greeff <jacogr@gmail.com>
2020-02-14 15:10:22 +0100

    Add test for isInBlock

--- packages/types/src/interfaces/rpc/ExtrinsicStatus.spec.ts
+++ packages/types/src/interfaces/rpc/ExtrinsicStatus.spec.ts
@@ -2,28 +2,28 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import { ExtrinsicStatus } from './types';
-
 import { createType, TypeRegistry } from '../../codec/create';
 import rpc from '../../json/ExtrinsicStatus.001.json';
 
 describe('ExtrinsicStatus', (): void => {
   const registry = new TypeRegistry();
-  let status: ExtrinsicStatus;
-
-  beforeEach((): void => {
-    status = createType(registry, 'ExtrinsicStatus', rpc.params.result);
-  });
 
   it('has the correct type', (): void => {
     expect(
-      status.type
+      createType(registry, 'ExtrinsicStatus', rpc.params.result).type
     ).toEqual('Finalized');
+  });
+
+  it('has an alias to isInBlock', (): void => {
+    const status = createType(registry, 'ExtrinsicStatus', rpc.params.result);
+
+    expect(status.isFinalized).toEqual(true);
+    expect(status.isInBlock).toEqual(true);
   });
 
   it('has the correct hash', (): void => {
     expect(
-      status.value.toString()
+      createType(registry, 'ExtrinsicStatus', rpc.params.result).value.toString()
     ).toEqual('0xc465b92a72b1d20918d64cd4effa70c2bb58b53a3f8c24c3ac8fd8f465f059b4');
   });
 });
