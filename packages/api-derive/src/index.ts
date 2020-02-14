ad5647e5559fdad078ac167c208ac598ac71153c
Keith Ingram <kwingram25@gmail.com>
2020-02-14 12:18:45 -0500

    api.derive.registrar.parachains

--- packages/api-derive/src/index.ts
+++ packages/api-derive/src/index.ts
@@ -15,6 +15,7 @@ import * as council from './council';
 import * as democracy from './democracy';
 import * as elections from './elections';
 import * as imOnline from './imOnline';
+import * as registrar from './registrar';
 import * as session from './session';
 import * as society from './society';
 import * as staking from './staking';
@@ -23,7 +24,7 @@ import * as treasury from './treasury';
 
 export * from './type';
 
-export const derive = { accounts, balances, chain, contracts, council, democracy, elections, imOnline, session, society, staking, technicalCommittee, treasury };
+export const derive = { accounts, balances, chain, contracts, council, democracy, elections, imOnline, registrar, session, society, staking, technicalCommittee, treasury };
 
 type DeriveSection<Section> = {
   [Method in keyof Section]: Section[Method] extends AnyFunction
---
+++ packages/api-derive/src/registrar/parachains.ts
@@ -0,0 +1,116 @@
+// Copyright 2017-2020 @polkadot/api-derive authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+import { BlockNumber, CollatorId, ParaId, ParaInfo, Retriable, UpwardMessage } from '@polkadot/types/interfaces';
+import { ITuple } from '@polkadot/types/types';
+import { DeriveParachain, DeriveParachainActive } from '../types';
+
+import { Observable, combineLatest, of } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
+import { ApiInterfaceRx } from '@polkadot/api/types';
+import { Bytes, Option, Vec } from '@polkadot/types';
+
+import { memo } from '../util';
+
+type ParaIds = ParaId[];
+type ParaInfoResult = Option<ParaInfo>;
+type PendingSwap = Option<ParaId>;
+type Active = ITuple<[ParaId, Option<ITuple<[CollatorId, Retriable]>>]>[];
+type RetryQueue = ITuple<[ParaId, CollatorId]>[][]
+type SelectedThreads = ITuple<[ParaId, CollatorId]>[][];
+type Heads = Bytes;
+type RelayDispatchQueue = Vec<UpwardMessage>
+type Watermarks = Option<BlockNumber>;
+type DidUpdate = Option<Vec<ParaId>>;
+
+type Result = [
+  ParaId[],
+  ParaInfoResult[],
+  PendingSwap[],
+  Active,
+  RetryQueue,
+  SelectedThreads,
+  Heads[],
+  RelayDispatchQueue[],
+  Watermarks[],
+  DidUpdate
+];
+
+function parseActive (id: ParaId, active: Active): DeriveParachainActive | null {
+  const found = active.find(([paraId]): boolean => paraId === id);
+
+  if (found && found[1].isSome) {
+    const [collatorId, retriable] = found[1].unwrap();
+
+    return {
+      collatorId,
+      ...(
+        retriable.isWithRetries
+          ? {
+            isRetriable: true,
+            retries: retriable.asWithRetries.toNumber()
+          }
+          : {
+            isRetriable: false,
+            retries: 0
+          }
+      )
+    };
+  }
+  return null;
+}
+
+function parseCollators (id: ParaId, collatorQueue: SelectedThreads | RetryQueue): (CollatorId | null)[] {
+  return collatorQueue
+    .map((queue): CollatorId | null => {
+      const found = queue.find(([paraId]): boolean => paraId === id);
+
+      return found ? found[1] : null;
+    });
+}
+
+function parse (api: ApiInterfaceRx, [ids, infos, pendingSwaps, active, retryQueue, selectedThreads, heads, relayDispatchQueues, watermarks, didUpdate]: Result): DeriveParachain[] {
+  return ids.map((id, index): DeriveParachain => {
+    return {
+      active: parseActive(id, active),
+      didUpdate: didUpdate.isSome
+        ? !!didUpdate.unwrap().find((paraId): boolean => paraId === id)
+        : false,
+      heads: heads[index],
+      id,
+      info: infos[index].unwrapOr(null),
+      pendingSwapId: pendingSwaps[index].unwrapOr(null),
+      relayDispatchQueue: relayDispatchQueues[index],
+      retryCollators: parseCollators(id, retryQueue),
+      selectedCollators: parseCollators(id, selectedThreads),
+      watermarks: watermarks[index].unwrapOr(null)
+    };
+  });
+}
+
+export function parachains (api: ApiInterfaceRx): () => Observable<DeriveParachain[]> {
+  return memo((): Observable<DeriveParachain[]> =>
+    api.query.registrar?.parachains && api.query.parachains
+      ? api.query.registrar.parachains<ParaIds>().pipe(
+        switchMap((paraIds) =>
+          combineLatest([
+            of(paraIds),
+            api.query.registrar.paras.multi<ParaInfo>(paraIds),
+            api.query.registrar.pendingSwap.multi<PendingSwap>(paraIds),
+            api.query.registrar.active<Active>(),
+            api.query.registrar.retryQueue<RetryQueue>(),
+            api.query.registrar.selectedThreads<SelectedThreads>(),
+            api.query.parachains.heads.multi<Heads>(paraIds),
+            api.query.parachains.relayDispatchQueue.multi<RelayDispatchQueue>(paraIds),
+            api.query.parachains.watermarks.multi<Watermarks>(paraIds),
+            api.query.parachains.didUpdate<DidUpdate>()
+          ]) as Observable<Result>
+        ),
+        map((result: Result): DeriveParachain[] =>
+          parse(api, result)
+        )
+      )
+      : of([] as DeriveParachain[])
+  );
+}
