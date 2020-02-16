c4afb2fdec6571f1ce5ecaa58b2f44118e8e9a03
Jaco Greeff <jacogr@gmail.com>
2020-02-16 17:35:23 +0100

    Update links

--- packages/types/src/scripts/interfacesTsWrapper.js
+++ packages/types/src/scripts/interfacesTsWrapper.js
@@ -9,6 +9,7 @@ require('@babel/register')({
   plugins: [
     ['module-resolver', {
       alias: {
+        '^@polkadot/jsonrpc(.*)': './packages/jsonrpc/src\\1',
         '^@polkadot/metadata(.*)': './packages/metadata/src\\1',
         '^@polkadot/types(.*)': './packages/types/src\\1'
       }
