009a6fdaa7ee64f791e07d5d1353152b6ba7b723
Jaco Greeff <jacogr@gmail.com>
2020-02-16 18:02:05 +0100

    Remove moved files

--- packages/types/src/scripts/util/index.ts
+++
@@ -1,9 +0,0 @@
-// Copyright 2017-2020 @polkadot/types authors & contributors
-// This software may be modified and distributed under the terms
-// of the Apache-2.0 license. See the LICENSE file for details.
-
-export * from './derived';
-export * from './docs';
-export * from './file';
-export * from './formatting';
-export * from './imports';
