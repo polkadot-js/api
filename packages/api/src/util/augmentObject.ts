a03b0da20225f2bceef16857e6434f26f3480738
Jaco Greeff <jacogr@gmail.com>
2020-02-18 20:40:11 +0100

    Add injectMetadata to api (with decoration)

---
+++ packages/api/src/util/augmentObject.ts
@@ -0,0 +1,64 @@
+// Copyright 2017-2020 @polkadot/api authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+// log all the stuff that has been removed/
+export function findRemoved (prefix: string, src: Record<string, Record<string, any>>, dst: Record<string, Record<string, any>>): void {
+  const srcSections = Object.keys(src);
+  const dstSections = Object.keys(dst);
+  const rmSections = dstSections
+    .filter((section): boolean => !srcSections.includes(section))
+    .sort();
+
+  if (rmSections.length) {
+    console.warn(`api.${prefix}: Found ${rmSections.length} removed modules: ${rmSections.join(', ')}`);
+  }
+
+  const rmMethods = dstSections
+    .filter((section): boolean => srcSections.includes(section))
+    .reduce((rmMethods: string[], section): string[] => {
+      const srcMethods = Object.keys(src[section]);
+      const dstMethods = Object.keys(dst[section]);
+
+      return rmMethods.concat(
+        ...dstMethods
+          .filter((method): boolean => !srcMethods.includes(method))
+          .map((method): string => `${section}.${method}`)
+      );
+    }, [])
+    .sort();
+
+  if (rmMethods.length) {
+    console.warn(`api.${prefix}: Found ${rmMethods.length} removed calls: ${rmMethods.join(', ')}`);
+  }
+}
+
+/**
+ * Takes a decorated api section (e.g. api.tx) and augment it with the details. It does not override what is
+ * already available, but rather just adds new missing ites into the result object.
+ * @internal
+ */
+export default function augmentObject (prefix: string, src: Record<string, Record<string, any>>, dst: Record<string, Record<string, any>>): Record<string, Record<string, any>> {
+  if (prefix && Object.keys(dst).length) {
+    findRemoved(prefix, src, dst);
+  }
+
+  return Object
+    .keys(src)
+    .reduce((newSection, sectionName): Record<string, Record<string, any>> => {
+      const section = src[sectionName];
+
+      newSection[sectionName] = Object
+        .keys(section)
+        .reduce((result, methodName): Record<string, any> => {
+          // TODO When it does match, check the actual details and warn when there are differences
+          if (!result[methodName]) {
+            result[methodName] = section[methodName];
+          }
+
+          return result;
+        }, dst[sectionName] || {});
+
+      return newSection;
+    }, dst);
+}
