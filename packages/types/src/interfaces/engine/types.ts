5f9a9ee065c2689c840f76a8c78b3e60e38c485c
Axel Chalon <xaxel@protonmail.com>
2020-02-19 00:12:08 +0000

    yarn build:interfaces

---
+++ packages/types/src/interfaces/engine/types.ts
@@ -0,0 +1,22 @@
+// Auto-generated via `yarn polkadot-types-from-defs`, do not edit
+/* eslint-disable @typescript-eslint/no-empty-interface */
+
+import { Struct } from '@polkadot/types/codec';
+import { bool } from '@polkadot/types/primitive';
+import { BlockHash } from '@polkadot/types/interfaces/rpc';
+
+/** @name CreatedBlock */
+export interface CreatedBlock extends Struct {
+  readonly hash: BlockHash;
+  readonly aux: ImportedAux;
+}
+
+/** @name ImportedAux */
+export interface ImportedAux extends Struct {
+  readonly headerOnly: bool;
+  readonly clearJustificationRequests: bool;
+  readonly needsJustification: bool;
+  readonly badJustification: bool;
+  readonly needsFinalityProof: bool;
+  readonly isNewBest: bool;
+}
