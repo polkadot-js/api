bc599be94cdf274316b59859810d3181afa10618
Jaco Greeff <jacogr@gmail.com>
2020-02-27 14:11:31 +0100

    Adjust Metadata tests

--- packages/types/src/index.spec.ts
+++ packages/types/src/index.spec.ts
@@ -18,6 +18,7 @@ const UNCONSTRUCTABLE = [
   'ExtrinsicPayloadUnknown', 'GenericExtrinsicPayloadUnknown',
   'ExtrinsicUnknown', 'GenericExtrinsicUnknown',
   'GenericOrigin', 'Origin',
+  'Metadata',
   'Unconstructable',
   'usize'
 ].map((v): string => v.toLowerCase());
