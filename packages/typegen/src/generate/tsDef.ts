70a5ce1970aa76af6ec11605b385a7398baca172
Jeremias Diaz <monitz87@gmail.com>
2020-03-01 01:05:15 -0500

    fix: support tuples as enum members in typegen

--- packages/typegen/src/generate/tsDef.ts
+++ packages/typegen/src/generate/tsDef.ts
@@ -51,7 +51,7 @@ function tsEnum (definitions: object, { name: enumName, sub }: TypeDef, imports:
     const getter = stringUpperFirst(stringCamelCase(name.replace(' ', '_')));
     const asGetter = type === 'Null'
       ? ''
-      : createGetter(definitions, `as${getter}`, type, imports);
+      : createGetter(definitions, `as${getter}`, info === TypeDefInfo.Tuple ? formatType(definitions, type, imports) : type, imports);
     const isGetter = createGetter(definitions, `is${getter}`, 'boolean', imports);
 
     switch (info) {
