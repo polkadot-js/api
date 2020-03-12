f3c2c5c084560e958a04d1d4d364b866daaf8c25
Jaco Greeff <jacogr@gmail.com>
2020-03-12 21:58:57 +0100

    Re-enable argument checks (dropped in 1.6.2) (#2035)
    * Re-enable argument checks (dropped in 1.6.2)
    
    * typo
    
    * Bump to preminor

--- packages/api/src/util/validate.spec.ts
+++ packages/api/src/util/validate.spec.ts
@@ -11,7 +11,7 @@ import { TypeRegistry } from '@polkadot/types';
 
 import { extractStorageArgs } from './validate';
 
-describe.skip('extractStorageArgs', (): void => {
+describe('extractStorageArgs', (): void => {
   const registry = new TypeRegistry();
   let storage: Storage;
 
@@ -24,6 +24,12 @@ describe.skip('extractStorageArgs', (): void => {
   it('validates no-arg plain', (): void => {
     expect(
       extractStorageArgs(storage.timestamp.now, [])
+    ).toEqual([storage.timestamp.now]);
+  });
+
+  it('validates no-arg plain (with undefined, undefined)', (): void => {
+    expect(
+      extractStorageArgs(storage.timestamp.now, [undefined, undefined])
     ).toEqual([storage.timestamp.now]);
   });
 
--- packages/api/src/util/validate.ts
+++ packages/api/src/util/validate.ts
@@ -3,53 +3,48 @@
 // of the Apache-2.0 license. See the LICENSE file for details.
 
 import { StorageEntry } from '@polkadot/types/primitive/StorageKey';
+import { Type } from '@polkadot/types';
+import { assert, isUndefined } from '@polkadot/util';
 
-// FIXME This breaks with .at(hash, ...)
+function sig ({ method, section }: StorageEntry, ...args: Type[]): string {
+  return `${section}.${method}(${args.join(', ')})`;
+}
 
-// import { Type } from '@polkadot/types';
-// import { assert } from '@polkadot/util';
+function doDoubleMap (creator: StorageEntry, args: any[]): [StorageEntry, [any, any]] {
+  const { key1, key2 } = creator.meta.type.asDoubleMap;
 
-// function sig ({ method, section }: StorageEntry, ...args: Type[]): string {
-//   return `${section}.${method}(${args.join(', ')})`;
-// }
+  assert(args.length === 2, `${sig(creator, key1, key2)} is a doublemap, requiring 2 arguments, ${args.length} found`);
 
-// function doDoubleMap (creator: StorageEntry, args: any[]): [StorageEntry, [any, any]] {
-//   const { key1, key2 } = creator.meta.type.asDoubleMap;
+  // pass as tuple
+  return [creator, args as [any, any]];
+}
 
-//   assert(args.length === 2, `${sig(creator, key1, key2)} is a doublemap, requiring 2 arguments, ${args.length} found`);
+function doMap (creator: StorageEntry, args: any[]): [StorageEntry] | [StorageEntry, any] {
+  const { key, linked } = creator.meta.type.asMap;
 
-//   // pass as tuple
-//   return [creator, args as [any, any]];
-// }
+  linked.isTrue
+    ? assert(args.length <= 1, `${sig(creator, key)} is a linked map, requiring either 0 arguments (retrieving all) or 1 argument, ${args.length} found`)
+    : assert(args.length === 1, `${sig(creator, key)} is a map, requiring 1 argument, ${args.length} found`);
 
-// function doMap (creator: StorageEntry, args: any[]): [StorageEntry] | [StorageEntry, any] {
-//   const { key, linked } = creator.meta.type.asMap;
-
-//   linked.isTrue
-//     ? assert(args.length <= 1, `${sig(creator, key)} is a linked map, requiring either 0 arguments (retrieving all) or 1 argument, ${args.length} found`)
-//     : assert(args.length === 1, `${sig(creator, key)} is a map, requiring 1 argument, ${args.length} found`);
-
-//   // expand
-//   return args.length
-//     ? [creator, args[0]]
-//     : [creator];
-// }
+  // expand
+  return args.length
+    ? [creator, args[0]]
+    : [creator];
+}
 
 // sets up the arguments in the form of [creator, args] ready to be used in a storage
 // call. Additionally, it verifies that the correct number of arguments have been passed
-export function extractStorageArgs (creator: StorageEntry, args: any[]): any[] {
-  return creator.meta.type.isDoubleMap
-    ? [creator, args]
-    : [creator, ...args];
+export function extractStorageArgs (creator: StorageEntry, _args: any[]): any[] {
+  const args = _args.filter((arg) => !isUndefined(arg));
 
-  // if (creator.meta.type.isDoubleMap) {
-  //   return doDoubleMap(creator, args);
-  // } else if (creator.meta.type.isMap) {
-  //   return doMap(creator, args);
-  // }
+  if (creator.meta.type.isDoubleMap) {
+    return doDoubleMap(creator, args);
+  } else if (creator.meta.type.isMap) {
+    return doMap(creator, args);
+  }
 
-  // assert(args.length === 0, `${sig(creator)} does not take any arguments, ${args.length} found`);
+  assert(args.length === 0, `${sig(creator)} does not take any arguments, ${args.length} found`);
 
-  // // no args
-  // return [creator, ...args];
+  // no args
+  return [creator];
 }
