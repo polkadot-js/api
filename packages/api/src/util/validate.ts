00dcfe5d69ffaa2b46989781dd63798c1363d5c2
Jaco Greeff <jacogr@gmail.com>
2020-03-12 07:03:20 +0100

    Split map/doublemap

--- packages/api/src/util/validate.ts
+++ packages/api/src/util/validate.ts
@@ -2,8 +2,30 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
+import { DoubleMapTypeLatest, MapTypeLatest } from '@polkadot/types/interfaces';
+
 import { StorageEntry, unwrapStorageType } from '@polkadot/types/primitive/StorageKey';
 import { assert } from '@polkadot/util';
+
+function doDoubleMap (creator: StorageEntry, { key1, key2 }: DoubleMapTypeLatest, type: string, args: any[]): [StorageEntry, [any, any]] {
+  assert(args.length === 2, `${creator.section}.${creator.method}(${key1}, ${key2}): ${type} is a doublemap, requiring 2 arguments, ${args.length} found`);
+
+  // pass as tuple
+  return [creator, args as [any, any]];
+}
+
+function doMap (creator: StorageEntry, { key, linked }: MapTypeLatest, type: string, args: any[]): [StorageEntry] | [StorageEntry, any] {
+  const sig = `${creator.section}.${creator.method}(${key}): ${type}`;
+
+  linked.isTrue
+    ? assert(args.length <= 1, `${sig} is a linked map, requiring either 0 arguments (retrieving all) or 1 argument, ${args.length} found`)
+    : assert(args.length === 1, `${sig} is a map, requiring 1 argument, ${args.length} found`);
+
+  // expand
+  return args.length
+    ? [creator, args[0]]
+    : [creator];
+}
 
 // sets up the arguments in the form of [creator, args] ready to be used in a storage
 // call. Additionally, it verifies that the correct number of arguments have been passed
@@ -12,24 +34,9 @@ export function extractStorageArgs (creator: StorageEntry, args: any[]): any[] {
   const type = unwrapStorageType(meta.type);
 
   if (meta.type.isDoubleMap) {
-    const dmType = meta.type.asDoubleMap;
-
-    assert(args.length === 2, `${section}.${method}(${dmType.key1}, ${dmType.key2}): ${type} is a doublemap, requiring 2 arguments, ${args.length} found`);
-
-    // pass as tuple
-    return [creator, args];
+    return doDoubleMap(creator, meta.type.asDoubleMap, type, args);
   } else if (meta.type.isMap) {
-    const mType = meta.type.asMap;
-    const sig = `${section}.${method}(${mType.key}): ${type}`;
-
-    mType.linked.isTrue
-      ? assert(args.length <= 1, `${sig} is a linked map, requiring either 0 arguments (retrieving all) or 1 argument, ${args.length} found`)
-      : assert(args.length === 1, `${sig} is a map, requiring 1 argument, ${args.length} found`);
-
-    // expand
-    return args.length
-      ? [creator, args[0]]
-      : [creator];
+    return doMap(creator, meta.type.asMap, type, args);
   }
 
   assert(args.length === 0, `${section}.${method}(): ${type} does not take any arguments, ${args.length} found`);
