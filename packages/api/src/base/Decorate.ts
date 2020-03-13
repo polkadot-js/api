bcf01709c0f1a03e926ec6200280b2ee8290a955
Jaco Greeff <jacogr@gmail.com>
2020-03-13 18:45:13 +0100

    Fix concatMap index

--- packages/api/src/base/Decorate.ts
+++ packages/api/src/base/Decorate.ts
@@ -482,8 +482,8 @@ export default abstract class Decorate<ApiType extends ApiTypes> extends Events 
         combineLatest([
           of(keys),
           from(Array(Math.ceil(keys.length / PAGE_SIZE_VALS)).fill(0)).pipe(
-            // FIXME Nw RPC to take care of this in the works...
-            concatMap((index): Observable<Option<Raw>[]> =>
+            // FIXME New RPC to take care of this in the works...
+            concatMap((_, index): Observable<Option<Raw>[]> =>
               this._rpcCore.state.subscribeStorage<Option<Raw>[]>(
                 keys.slice(index * PAGE_SIZE_VALS, (index * PAGE_SIZE_VALS) + PAGE_SIZE_VALS)
               ).pipe(take(1))
