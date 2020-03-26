bb8b15e5adeb3cc52ce468bcb1bf9f0b8fa83b7d
Jaco Greeff <jacogr@gmail.com>
2020-03-26 15:57:44 +0100

    Trim down metadata toCallsOnly

--- packages/metadata/src/Metadata/util/toCallsOnly.ts
+++ packages/metadata/src/Metadata/util/toCallsOnly.ts
@@ -2,16 +2,32 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import { MetadataLatest } from '@polkadot/types/interfaces/metadata';
+import { FunctionMetadataLatest, MetadataLatest } from '@polkadot/types/interfaces/metadata';
 import { AnyJson, Registry } from '@polkadot/types/types';
+
+import { Option, Text, Vec } from '@polkadot/types';
+
+interface ModuleMetadataCallsOnly {
+  name: Text;
+  calls: FunctionMetadataLatest[];
+}
+
+function mapCalls (registry: Registry, calls: Option<Vec<FunctionMetadataLatest>>): FunctionMetadataLatest[] {
+  return calls.unwrapOr([] as FunctionMetadataLatest[]).map(({ args, documentation, name }) =>
+    registry.createType('FunctionMetadataLatest', {
+      args,
+      name,
+      documentation: documentation.map((doc) => doc.toString().trim())
+    })
+  );
+}
 
 /** @internal */
 export default function toCallsOnly (registry: Registry, { extrinsic, modules }: MetadataLatest): AnyJson {
   return registry.createType('MetadataLatest', {
-    // FIXME, this needs typing, not any
-    modules: modules.map(({ calls, name }): any => ({
+    modules: modules.map(({ calls, name }): ModuleMetadataCallsOnly => ({
       name,
-      calls
+      calls: mapCalls(registry, calls)
     })),
     extrinsic
   }).toJSON();
