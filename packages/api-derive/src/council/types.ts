d338fcb58cdebc7ce05abd9b34474e139eb7d236
Jaco Greeff <jacogr@gmail.com>
2020-03-30 13:48:14 +0200

    Add derive for council voting info (#2115)
    * Add derive for council voting info
    
    * Update packages/api-derive/src/council/votes.ts

---
+++ packages/api-derive/src/council/types.ts
@@ -0,0 +1,12 @@
+// Copyright 2017-2020 @polkadot/api-derive authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+import { AccountId, Balance } from '@polkadot/types/interfaces';
+
+export interface DeriveCouncilVote {
+  stake: Balance;
+  votes: AccountId[];
+}
+
+export type DeriveCouncilVotes = [AccountId, DeriveCouncilVote][];
