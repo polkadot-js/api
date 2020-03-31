56412cf8cd09b3ff9c4923e2103cfeb3f8896349
Jaco Greeff <jacogr@gmail.com>
2020-03-31 13:25:56 +0200

    script aliasses

--- packages/typegen/scripts/polkadot-types-from-defs.js
+++ packages/typegen/scripts/polkadot-types-from-defs.js
@@ -16,6 +16,7 @@ try {
         alias: {
           '^@polkadot/metadata(.*)': './packages/metadata/src\\1',
           '^@polkadot/typegen(.*)': './packages/typegen/src\\1',
+          '^@polkadot/types-known(.*)': './packages/types-known/src\\1',
           '^@polkadot/types(.*)': './packages/types/src\\1'
         }
       }]
