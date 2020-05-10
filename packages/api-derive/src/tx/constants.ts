68a6c049cf8ffcf0a89aad7e5633508f120e5d9d
Jaco Greeff <jacogr@gmail.com>
2020-05-10 21:37:51 +0200

    Additional moratl era tests

--- packages/api-derive/src/tx/constants.ts
+++ packages/api-derive/src/tx/constants.ts
@@ -4,4 +4,11 @@
 
 import BN from 'bn.js';
 
+export const FALLBACK_MAX_HASH_COUNT = 250;
+
+// default here to 5 min eras, adjusted based on the actual blocktime
+export const FALLBACK_PERIOD = new BN(6 * 1000);
+
 export const MAX_FINALITY_LAG = new BN(5);
+
+export const MORTAL_PERIOD = new BN(5 * 60 * 1000);
