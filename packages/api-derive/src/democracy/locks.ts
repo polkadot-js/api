00ce18c11ba1426e2cf31af117cb2f7b95453dee
Jaco Greeff <jacogr@gmail.com>
2020-05-12 09:46:17 +0200

    Cater for old referendums

--- packages/api-derive/src/democracy/locks.ts
+++ packages/api-derive/src/democracy/locks.ts
@@ -2,16 +2,17 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import { AccountId, ReferendumInfo, VotingDirect, VotingDirectVote } from '@polkadot/types/interfaces';
+import { AccountId, ReferendumInfo, ReferendumInfoTo239, VotingDirect, VotingDirectVote } from '@polkadot/types/interfaces';
 import { ApiInterfaceRx } from '@polkadot/api/types';
 import { DeriveDemocracyLock } from '../types';
 
+import BN from 'bn.js';
 import { Observable, of } from 'rxjs';
 import { map, switchMap } from 'rxjs/operators';
 import { Option } from '@polkadot/types';
+import { isUndefined } from '@polkadot/util';
 
 import { memo } from '../util';
-import BN from 'bn.js';
 
 const LOCKUPS = [0, 1, 2, 4, 8, 16, 32];
 const ZERO = new BN(0);
@@ -21,11 +22,15 @@ function directLocks (api: ApiInterfaceRx, { votes }: VotingDirect): Observable<
     return of([]);
   }
 
-  return api.query.democracy.referendumInfoOf.multi<Option<ReferendumInfo>>(votes.map(([referendumId]) => referendumId)).pipe(
+  return api.query.democracy.referendumInfoOf.multi<Option<ReferendumInfo | ReferendumInfoTo239>>(votes.map(([referendumId]) => referendumId)).pipe(
     map((referendums) =>
       votes
-        .map((vote, index): [VotingDirectVote, ReferendumInfo | null] => [vote, referendums[index].unwrapOr(null)])
-        .filter((item): item is [VotingDirectVote, ReferendumInfo] => !!item[1] && item[0][1].isStandard)
+        .map((vote, index): [VotingDirectVote, ReferendumInfo | ReferendumInfoTo239 | null] =>
+          [vote, referendums[index].unwrapOr(null)]
+        )
+        .filter((item): item is [VotingDirectVote, ReferendumInfo] =>
+          !!item[1] && isUndefined((item[1] as ReferendumInfoTo239).end) && item[0][1].isStandard
+        )
         .map(([[referendumId, directVote], referendum]): DeriveDemocracyLock => {
           const { balance, vote } = directVote.asStandard;
           let unlockAt = ZERO;
