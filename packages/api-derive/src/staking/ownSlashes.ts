0187f1ff199a66ae0354780fbba3c95b80b56b54
Jaco Greeff <jacogr@gmail.com>
2020-05-28 12:07:02 +0200

    Extend deriveCache around ownExposure/ownSlash (#2315)

--- packages/api-derive/src/staking/ownSlashes.ts
+++ packages/api-derive/src/staking/ownSlashes.ts
@@ -2,35 +2,67 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
+import { EraIndex } from '@polkadot/types/interfaces';
 import { ApiInterfaceRx } from '@polkadot/api/types';
 import { DeriveStakerSlashes } from '../types';
 
 import { Observable, combineLatest, of } from 'rxjs';
 import { map, switchMap } from 'rxjs/operators';
 
-import { memo } from '../util';
+import { deriveCache, memo } from '../util';
+
+const CACHE_KEY = 'ownSlash';
+
+export function _ownSlash (api: ApiInterfaceRx): (accountId: Uint8Array | string, era: EraIndex, withActive: boolean) => Observable<DeriveStakerSlashes> {
+  return memo((accountId: Uint8Array | string, era: EraIndex, withActive: boolean): Observable<DeriveStakerSlashes> => {
+    const cacheKey = `${CACHE_KEY}-${era.toString()}`;
+    const cached = withActive
+      ? undefined
+      : deriveCache.get<DeriveStakerSlashes>(cacheKey);
+
+    return cached
+      ? of(cached)
+      : combineLatest([
+        api.query.staking.nominatorSlashInEra(era, accountId),
+        api.query.staking.validatorSlashInEra(era, accountId)
+      ]).pipe(
+        map(([optNom, optVal]): DeriveStakerSlashes => {
+          const value = {
+            era,
+            total: optVal.isSome
+              ? optVal.unwrap()[1]
+              : optNom.unwrapOrDefault()
+          };
+
+          !withActive && deriveCache.set(cacheKey, value);
+
+          return value;
+        })
+      );
+  });
+}
+
+export function ownSlash (api: ApiInterfaceRx): (accountId: Uint8Array | string, era: EraIndex) => Observable<DeriveStakerSlashes> {
+  return memo((accountId: Uint8Array | string, era: EraIndex): Observable<DeriveStakerSlashes> =>
+    api.derive.staking._ownSlash(accountId, era, true)
+  );
+}
+
+export function _ownSlashes (api: ApiInterfaceRx): (accountId: Uint8Array | string, eras: EraIndex[], withActive: boolean) => Observable<DeriveStakerSlashes[]> {
+  return memo((accountId: Uint8Array | string, eras: EraIndex[], withActive: boolean): Observable<DeriveStakerSlashes[]> =>
+    eras.length
+      ? combineLatest(
+        eras.map((era) => api.derive.staking._ownSlash(accountId, era, withActive))
+      )
+      : of([])
+  );
+}
 
 export function ownSlashes (api: ApiInterfaceRx): (accountId: Uint8Array | string, withActive?: boolean) => Observable<DeriveStakerSlashes[]> {
   return memo((accountId: Uint8Array | string, withActive = false): Observable<DeriveStakerSlashes[]> => {
     return api.derive.staking.erasHistoric(withActive).pipe(
       switchMap((eras) =>
-        eras.length
-          ? combineLatest(
-            eras.map((era) => combineLatest([
-              of(era),
-              api.query.staking.nominatorSlashInEra(era, accountId),
-              api.query.staking.validatorSlashInEra(era, accountId)
-            ]))
-          )
-          : of([])
-      ),
-      map((result): DeriveStakerSlashes[] =>
-        result.map(([era, optNom, optVal]): DeriveStakerSlashes => ({
-          era,
-          total: optVal.isSome
-            ? optVal.unwrap()[1]
-            : optNom.unwrapOrDefault()
-        }))
+        api.derive.staking._ownSlashes(accountId, eras, withActive)
       )
     );
   });
