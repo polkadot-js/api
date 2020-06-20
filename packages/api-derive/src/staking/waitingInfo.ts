c3fd8d3086bc16a65ef88872aa7fcca360083e08
Jaco Greeff <jacogr@gmail.com>
2020-06-20 14:11:28 +0200

    Small optimizations

--- packages/api-derive/src/staking/waitingInfo.ts
+++ packages/api-derive/src/staking/waitingInfo.ts
@@ -3,10 +3,9 @@
 // of the Apache-2.0 license. See the LICENSE file for details.
 
 import { ApiInterfaceRx } from '@polkadot/api/types';
-import { AccountId } from '@polkadot/types/interfaces';
-import { DeriveStakingQuery, DeriveStakingWaiting } from '../types';
+import { DeriveStakingWaiting } from '../types';
 
-import { Observable, combineLatest, of } from 'rxjs';
+import { Observable, combineLatest } from 'rxjs';
 import { map, switchMap } from 'rxjs/operators';
 
 import { memo } from '../util';
@@ -14,21 +13,19 @@ import { memo } from '../util';
 export function waitingInfo (api: ApiInterfaceRx): () => Observable<DeriveStakingWaiting> {
   return memo((): Observable<DeriveStakingWaiting> =>
     api.derive.staking.validators().pipe(
-      switchMap(({ nextElected, validators }): Observable<[AccountId[], DeriveStakingQuery[]]> => {
+      switchMap(({ nextElected, validators }): Observable<DeriveStakingWaiting> => {
         const elected = nextElected.map((a) => a.toString());
         const waiting = validators.filter((v) => !elected.includes(v.toString()));
 
-        return combineLatest([
-          of(waiting),
-          combineLatest(
-            waiting.map((accountId) => api.derive.staking.query(accountId))
-          )
-        ]);
-      }),
-      map(([waiting, info]): DeriveStakingWaiting => ({
-        info,
-        waiting
-      }))
+        return combineLatest(
+          waiting.map((accountId) => api.derive.staking.query(accountId))
+        ).pipe(
+          map((info): DeriveStakingWaiting => ({
+            info,
+            waiting
+          }))
+        );
+      })
     )
   );
 }
