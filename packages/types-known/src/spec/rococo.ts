3ccd6812ee714d6f2273d38871113b23e2636106
Jaco Greeff <jacogr@gmail.com>
2020-07-29 09:42:31 +0200

    Known types for Rococo

---
+++ packages/types-known/src/spec/rococo.ts
@@ -0,0 +1,25 @@
+// Copyright 2017-2020 @polkadot/types-known authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+import { OverrideVersionedType } from '@polkadot/types/types';
+
+const sharedTypes = {
+  Address: 'AccountId',
+  Keys: 'SessionKeys5',
+  LookupSource: 'AccountId',
+  ProxyType: {
+    _enum: ['Any', 'NonTransfer', 'Staking']
+  }
+};
+
+const versioned: OverrideVersionedType[] = [
+  {
+    minmax: [0, undefined],
+    types: {
+      ...sharedTypes
+    }
+  }
+];
+
+export default versioned;
