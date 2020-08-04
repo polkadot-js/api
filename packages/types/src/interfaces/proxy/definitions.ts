0a42456017bd26aa2299ce248b65632cc7c7eb2d
Jaco Greeff <jacogr@gmail.com>
2020-08-04 21:51:04 +0200

    Types for time-delay proxies

---
+++ packages/types/src/interfaces/proxy/definitions.ts
@@ -0,0 +1,27 @@
+// Copyright 2017-2020 @polkadot/types authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+// order important in structs... :)
+/* eslint-disable sort-keys */
+
+import { Definitions } from '../../types';
+
+export default {
+  rpc: {},
+  types: {
+    ProxyDefinition: {
+      delegate: 'AccountId',
+      proxyType: 'ProxyType',
+      delay: 'BlockNumber'
+    },
+    ProxyType: {
+      _enum: ['Any', 'NonTransfer', 'Governance', 'Staking']
+    },
+    ProxyAnnouncement: {
+      real: 'AccountId',
+      callHash: 'Hash',
+      height: 'BlockNumber'
+    }
+  }
+} as Definitions;
