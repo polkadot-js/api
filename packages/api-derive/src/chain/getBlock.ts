716e5fa4209b3714404e859479ba1b1e90e45b4c
Jaco Greeff <jacogr@gmail.com>
2020-08-31 20:44:33 +0200

    api.derive.chain.getBlock<hash>) with author getter

---
+++ packages/api-derive/src/chain/getBlock.ts
@@ -0,0 +1,44 @@
+// Copyright 2017-2020 @polkadot/api-derive authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+import { ApiInterfaceRx } from '@polkadot/api/types';
+
+import { Observable, combineLatest, of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+
+import { SignedBlockExtended } from '../type';
+import { memo } from '../util';
+
+/**
+ * @name getBlock
+ * @param {( Uint8Array | string )} hash - A block hash as U8 array or string.
+ * @description Get a specific block (e.g. rpc.chain.getBlock) and extend it with the author
+ * @example
+ * <BR>
+ *
+ * ```javascript
+ * const { author, number } = await api.derive.chain.getHeader('0x123...456');
+ *
+ * console.log(`block #${number} was authored by ${author}`);
+ * ```
+ */
+export function getHeader (api: ApiInterfaceRx): (hash: Uint8Array | string) => Observable<SignedBlockExtended | undefined> {
+  return memo((hash: Uint8Array | string): Observable<SignedBlockExtended | undefined> =>
+    combineLatest([
+      api.rpc.chain.getBlock(hash),
+      api.query.session
+        ? api.query.session.validators.at(hash)
+        : of([])
+    ]).pipe(
+      map(([signedBlock, validators]): SignedBlockExtended =>
+        new SignedBlockExtended(api.registry, signedBlock, validators)
+      ),
+      catchError((): Observable<undefined> =>
+        // where rpc.chain.getHeader throws, we will land here - it can happen that
+        // we supplied an invalid hash. (Due to defaults, storeage will have an
+        // empty value, so only the RPC is affected). So return undefined
+        of()
+      )
+    ));
+}
