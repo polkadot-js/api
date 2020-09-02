ecd43bdc920d55fb294369ec4b30792387b32c50
Jaco Greeff <jacogr@gmail.com>
2020-09-02 08:49:48 +0200

    Known Kusama update (slight upgrades rework)

--- packages/types-known/src/upgrades/polkadot.ts
+++ packages/types-known/src/upgrades/polkadot.ts
@@ -2,22 +2,16 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import { ChainUpgrades } from '@polkadot/types/types';
+import { ChainUpgradesRaw } from './types';
 
-import BN from 'bn.js';
-import { hexToU8a } from '@polkadot/util';
-
-const upgrades: ChainUpgrades = {
-  genesisHash: hexToU8a('0x91b171bb158e2d3848fa23a9f1c25182fb8e20313b2c1eb49219da7a70ce90c3'),
+const upgrades: ChainUpgradesRaw = {
+  genesisHash: '0x91b171bb158e2d3848fa23a9f1c25182fb8e20313b2c1eb49219da7a70ce90c3',
   versions: [
     [0, 0], [29231, 1], [188836, 5], [199405, 6], [214264, 7],
     [244358, 8], [303079, 9], [314201, 10], [342400, 11], [443963, 12],
     [528470, 13], [687751, 14], [746085, 15], [787923, 16], [799302, 17],
     [1205128, 18]
-  ].map(([blockNumber, specVersion]) => ({
-    blockNumber: new BN(blockNumber),
-    specVersion: new BN(specVersion)
-  }))
+  ]
 };
 
 export default upgrades;
