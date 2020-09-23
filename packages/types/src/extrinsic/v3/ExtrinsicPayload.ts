abe038b6f89a99ab9bf5e5af0178367c9c2753f4
Jaco Greeff <jacogr@gmail.com>
2020-09-23 11:19:42 +0200

    Revert "V3 payload extends v4 payload"
    This reverts commit b1e546aa08fe1027aa77a235fd3b98441c426812.

--- packages/types/src/extrinsic/v3/ExtrinsicPayload.ts
+++ packages/types/src/extrinsic/v3/ExtrinsicPayload.ts
@@ -1,10 +1,15 @@
 // Copyright 2017-2020 @polkadot/types authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { IKeyringPair } from '../../types';
+import { ExtrinsicEra } from '../../interfaces/extrinsics';
+import { Balance, Hash, Index } from '../../interfaces/runtime';
+import { ExtrinsicPayloadValue, IKeyringPair, Registry } from '../../types';
 
+import Compact from '../../codec/Compact';
+import Struct from '../../codec/Struct';
+import Bytes from '../../primitive/Bytes';
+import u32 from '../../primitive/U32';
 import { sign } from '../util';
-import ExtrinsicPayloadV4 from '../v4/ExtrinsicPayload';
 
 /**
  * @name GenericExtrinsicPayloadV3
@@ -12,7 +17,72 @@ import ExtrinsicPayloadV4 from '../v4/ExtrinsicPayload';
  * A signing payload for an [[Extrinsic]]. For the final encoding, it is variable length based
  * on the contents included
  */
-export default class ExtrinsicPayloadV3 extends ExtrinsicPayloadV4 {
+export default class ExtrinsicPayloadV3 extends Struct {
+  constructor (registry: Registry, value?: ExtrinsicPayloadValue | Uint8Array | string) {
+    super(registry, {
+      method: 'Bytes',
+      // eslint-disable-next-line sort-keys
+      era: 'ExtrinsicEra',
+      nonce: 'Compact<Index>',
+      tip: 'Compact<Balance>',
+      // eslint-disable-next-line sort-keys
+      specVersion: 'u32',
+      // eslint-disable-next-line sort-keys
+      genesisHash: 'Hash',
+      // eslint-disable-next-line sort-keys
+      blockHash: 'Hash'
+    }, value);
+  }
+
+  /**
+   * @description The block [[Hash]] the signature applies to (mortal/immortal)
+   */
+  public get blockHash (): Hash {
+    return this.get('blockHash') as Hash;
+  }
+
+  /**
+   * @description The [[ExtrinsicEra]]
+   */
+  public get era (): ExtrinsicEra {
+    return this.get('era') as ExtrinsicEra;
+  }
+
+  /**
+   * @description The genesis [[Hash]] the signature applies to (mortal/immortal)
+   */
+  public get genesisHash (): Hash {
+    return this.get('genesisHash') as Hash;
+  }
+
+  /**
+   * @description The [[Bytes]] contained in the payload
+   */
+  public get method (): Bytes {
+    return this.get('method') as Bytes;
+  }
+
+  /**
+   * @description The [[Index]]
+   */
+  public get nonce (): Compact<Index> {
+    return this.get('nonce') as Compact<Index>;
+  }
+
+  /**
+   * @description The specVersion for this signature
+   */
+  public get specVersion (): u32 {
+    return this.get('specVersion') as u32;
+  }
+
+  /**
+   * @description The tip [[Balance]]
+   */
+  public get tip (): Compact<Balance> {
+    return this.get('tip') as Compact<Balance>;
+  }
+
   /**
    * @description Sign the payload with the keypair
    */
