2650ea8bd1147ea6605b55b7f1e9725a8c32a809
Jaco Greeff <jacogr@gmail.com>
2020-09-27 10:33:10 +0200

    Update tests

--- packages/types/src/interfaces/staking/Exposure.spec.ts
+++ packages/types/src/interfaces/staking/Exposure.spec.ts
@@ -11,11 +11,11 @@ describe('Exposure', (): void => {
       registry.createType('Exposure', '0x0fd2c5f6bc904d990f94373671da716d04fe65717dad0447d715f660a0a58411de509b42e6efb8375f562f58a554d5860e0f3e8ec04bb6db2b').toHuman()
     ).toEqual({
       others: [{
-        value: '12.344k Unit',
+        value: '12.3449 kUnit',
         who: '5HpG9w8EBLe5XCrbczpwq5TSXvedjrBGCwqxK1iQ7qUsSWFc'
       }],
-      own: '30.805k Unit',
-      total: '43.150k Unit'
+      own: '30.8059 kUnit',
+      total: '43.1509 kUnit'
     });
   });
 });
