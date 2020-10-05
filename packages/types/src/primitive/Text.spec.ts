f8ea344243562d9c71744a68685852b9d62aa25c
Jaco Greeff <jacogr@gmail.com>
2020-10-05 12:43:24 +0200

    Add to decode section

--- packages/types/src/primitive/Text.spec.ts
+++ packages/types/src/primitive/Text.spec.ts
@@ -9,12 +9,8 @@ import Text from './Text';
 describe('Text', (): void => {
   const registry = new TypeRegistry();
 
-  it('can be created from null', (): void => {
-    expect(new Text(registry, null).toString()).toEqual('');
-  });
-
   describe('decode', (): void => {
-    const testDecode = (type: string, input: string | Uint8Array | { toString: () => string }, expected: string, toFn: 'toString' | 'toHex' = 'toString'): void =>
+    const testDecode = (type: string, input: null | string | Uint8Array | { toString: () => string }, expected: string, toFn: 'toString' | 'toHex' = 'toString'): void =>
       it(`can decode from ${type}`, (): void => {
         expect(new Text(registry, input)[toFn]()).toBe(expected);
       });
@@ -25,6 +21,7 @@ describe('Text', (): void => {
     testDecode('Raw', new Raw(registry, Uint8Array.from([102, 111, 111])), 'foo'); // no length
     testDecode('object with `toString()`', { toString (): string { return 'foo'; } }, 'foo');
     testDecode('hex input value', new Text(registry, '0x12345678'), '0x12345678', 'toHex');
+    testDecode('null', null, '');
   });
 
   describe('encode', (): void => {
