094e80cb73df9c54a2ec583519868bc87614293f
Jaco Greeff <jacogr@gmail.com>
2020-10-11 09:12:48 +0200

    Fix c-like enums type encoding

--- packages/api-contract/src/ContractRegistry.ts
+++ packages/api-contract/src/ContractRegistry.ts
@@ -53,7 +53,16 @@ export default class ContractRegistry {
   }
 
   public setTypeDef (id: MtLookupTypeId): void {
-    this.typeDefs[getRegistryOffset(id)] = this.extractType(this.getAbiType(id), id) as TypeDef;
+    const typeDef = this.extractType(this.getAbiType(id), id) as TypeDef;
+
+    this.typeDefs[getRegistryOffset(id)] = typeDef;
+
+    // we have a displayName for non-primitives and non-results
+    if (typeDef.displayName) {
+      assert(!typeDef.type.startsWith('['), JSON.stringify(typeDef));
+
+      this.registry.register({ [typeDef.displayName]: typeDef.type });
+    }
   }
 
   private extractType (inkType: MtType, id: MtLookupTypeId): Pick<TypeDef, never> {
