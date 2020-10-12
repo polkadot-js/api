ef05f060685e69f28ab60e478c137fbb614d152c
Jaco Greeff <jacogr@gmail.com>
2020-10-12 10:19:33 +0200

    Rename contract helpers

--- packages/api-contract/src/promise/index.ts
+++ packages/api-contract/src/promise/index.ts
@@ -1,6 +1,6 @@
 // Copyright 2017-2020 @polkadot/api-contract authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-export { default as PromiseBlueprint } from './PromiseBlueprint';
-export { default as PromiseCode } from './PromiseCode';
-export { default as PromiseContract } from './PromiseContract';
+export { default as BlueprintPromise } from './PromiseBlueprint';
+export { default as CodePromise } from './PromiseCode';
+export { default as ContractPromise } from './PromiseContract';
