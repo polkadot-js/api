7f0a45b008b7688afbf9cfc94a8094821d761937
Jaco Greeff <jacogr@gmail.com>
2020-10-12 14:25:22 +0200

    Expose result classes

--- packages/api-contract/src/index.ts
+++ packages/api-contract/src/index.ts
@@ -1,6 +1,10 @@
 // Copyright 2017-2020 @polkadot/api-contract authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+export { CodeSubmittableResult } from './base/Code';
+export { BlueprintSubmittableResult } from './base/Blueprint';
+
 export { default as Abi } from './Abi';
+
 export * from './rx';
 export * from './promise';
