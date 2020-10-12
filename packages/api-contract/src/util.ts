8a8b6f7ac8285e5bf0c8505c96106b5db489c81d
Jaco Greeff <jacogr@gmail.com>
2020-10-12 14:36:47 +0200

    s/contract/contracts/ on applyOnEvent (#2712)
    * s/contract/contracts/ on applyOnEvent
    
    * Expose result classes

--- packages/api-contract/src/util.ts
+++ packages/api-contract/src/util.ts
@@ -16,7 +16,7 @@ export function formatData (registry: Registry, data: Raw, { type }: TypeDef): C
 
 export function applyOnEvent <T> (result: SubmittableResult, type: 'CodeStored' | 'Instantiated', fn: (record: EventRecord) => T): T | undefined {
   if (result.isInBlock || result.isFinalized) {
-    const record = result.findRecord('contract', type);
+    const record = result.findRecord('contracts', type);
 
     if (record) {
       return fn(record);
