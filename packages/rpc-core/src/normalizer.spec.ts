11804eb6cf8c565e87f9cf52a40e59d73398eccd
Jaco Greeff <jacogr@gmail.com>
2020-10-15 09:10:54 +0200

    More expansive

--- packages/rpc-core/src/normalizer.spec.ts
+++ packages/rpc-core/src/normalizer.spec.ts
@@ -4,9 +4,21 @@
 import normalizer from './normalizer';
 
 describe('normalizer', (): void => {
-  it('works when BigInt is in the data', (): void => {
+  it('works when BigInt is in the data (object)', (): void => {
     expect(
       normalizer('instance')({ a: 'bar', b: 1234567890987654321n, c: 123 })
     ).toEqual('instance{"a":"bar","b":"1234567890987654321","c":123}');
+  });
+
+  it('works when BigInt is in the data (array)', (): void => {
+    expect(
+      normalizer('instance')(['bar', 1234567890987654321n, 123])
+    ).toEqual('instance["bar","1234567890987654321",123]');
+  });
+
+  it('works when BigInt is in the data (single)', (): void => {
+    expect(
+      normalizer('instance')(1234567890987654321n)
+    ).toEqual('instance"1234567890987654321"');
   });
 });
