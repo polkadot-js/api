69acbfe805fad4dc02d4257f0b310b583d49cf0b
Sean Young <sean@mess.org>
2020-10-20 14:38:26 +0100

    Solang requires u256 and i256 types
    Tested with latest solang (current master).
    
    Signed-off-by: Sean Young <sean@mess.org>

--- packages/types/src/interfaces/scaleInfo/definitions.ts
+++ packages/types/src/interfaces/scaleInfo/definitions.ts
@@ -41,7 +41,7 @@ export default {
       variants: 'Vec<SiVariant>'
     },
     SiTypeDefPrimitive: {
-      _enum: ['Bool', 'Char', 'Str', 'U8', 'U16', 'U32', 'U64', 'U128', 'I8', 'I16', 'I32', 'I64', 'I128']
+      _enum: ['Bool', 'Char', 'Str', 'U8', 'U16', 'U32', 'U64', 'U128', 'U256', 'I8', 'I16', 'I32', 'I64', 'I128', 'I256']
     },
     SiTypeDefSequence: {
       type: 'SiLookupTypeId'
