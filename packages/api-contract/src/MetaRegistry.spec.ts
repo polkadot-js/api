9b225142a51ea47a0c38f996c549296f3dc1c6d7
Jaco Greeff <jacogr@gmail.com>
2020-10-22 11:29:11 +0200

    Add solang tests, re-organize sample layouts

--- packages/api-contract/src/MetaRegistry.spec.ts
+++ packages/api-contract/src/MetaRegistry.spec.ts
@@ -7,13 +7,11 @@ import path from 'path';
 import Abi from './Abi';
 
 import * as testContracts from '../test/contracts';
-import * as userContracts from '../test/contracts/user';
 
 const abis: Record<string, any> = { ...testContracts };
-const userAbis: Record<string, any> = { ...userContracts };
 
 function compareTypes (name: string): void {
-  const abi = new Abi(abis[name] || userAbis[name]);
+  const abi = new Abi(abis[name]);
 
   try {
     const cmpPath = path.join(__dirname, `../test/compare/${name}.test.json`);
@@ -32,35 +30,9 @@ function compareTypes (name: string): void {
 }
 
 describe('MetaRegistry', (): void => {
-  describe('types', (): void => {
-    it('initializes from a contract ABI (flipper)', (): void => {
-      compareTypes('flipper');
-    });
-
-    it('initializes from a contract ABI (incrementer)', (): void => {
-      compareTypes('incrementer');
-    });
-
-    it('initializes from a contract ABI (erc20)', (): void => {
-      compareTypes('erc20');
-    });
-
-    it('initializes from a contract ABI (dns)', (): void => {
-      compareTypes('dns');
-    });
-
-    it('initializes from a contract ABI (erc721)', (): void => {
-      compareTypes('erc721');
-    });
-
-    it('initializes from a contract ABI (multisig_plain)', (): void => {
-      compareTypes('multisigPlain');
-    });
-  });
-
-  describe('user ABIs', (): void => {
-    it('initializes from a contract ABI (withString)', (): void => {
-      compareTypes('withString');
+  Object.keys(abis).forEach((abiName) => {
+    it(`initializes from a contract ABI (${abiName})`, (): void => {
+      compareTypes(abiName);
     });
   });
 
