e1ac0d9ffaef8cc27445ac237ec7ac5b60972d15
Jaco Greeff <jacogr@gmail.com>
2020-10-23 12:47:58 +0200

    Fix linting

--- packages/typegen/src/util/file.ts
+++ packages/typegen/src/util/file.ts
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import fs from 'fs';
+import path from 'path';
 
 export function writeFile (dest: string, generator: () => string, noLog?: boolean): void {
   !noLog && console.log(`${dest}\n\tGenerating`);
@@ -19,6 +20,6 @@ export function writeFile (dest: string, generator: () => string, noLog?: boolea
   !noLog && console.log('');
 }
 
-export function readTemplate (path: string): string {
-  return fs.readFileSync(`${__dirname}/../templates/${path}.hbs`).toString();
+export function readTemplate (template: string): string {
+  return fs.readFileSync(path.join(__dirname, `../templates/${template}.hbs`)).toString();
 }
