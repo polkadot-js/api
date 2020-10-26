9689bcf7c03dd92e604deb14c6c4d937c60f45e2
Jaco Greeff <jacogr@gmail.com>
2020-10-26 11:38:53 +0100

    Bump GH actions, root cleanups

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,15 @@ const base = require('@polkadot/dev/config/eslint');
 
 module.exports = {
   ...base,
+  ignorePatterns: [
+    '.eslintrc.js',
+    '.github/**',
+    '.vscode/**',
+    '.yarn/**',
+    '**/build/*',
+    '**/coverage/*',
+    '**/node_modules/*'
+  ],
   parserOptions: {
     ...base.parserOptions,
     project: [
