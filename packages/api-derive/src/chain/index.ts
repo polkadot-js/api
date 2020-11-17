dd2dde5701bc44e63458ee58e7109df38b93504e
Lovesh Harchandani <lovesh@users.noreply.github.com>
2020-11-17 20:01:01 +0530

    Fix name of "getBlock" in package "api-derive" and export it in "index.ts" (#2857)
    Signed-off-by: lovesh <lovesh.bond@gmail.com>

--- packages/api-derive/src/chain/index.ts
+++ packages/api-derive/src/chain/index.ts
@@ -5,5 +5,6 @@ export * from './bestNumber';
 export * from './bestNumberFinalized';
 export * from './bestNumberLag';
 export * from './getHeader';
+export * from './getBlock';
 export * from './subscribeNewBlocks';
 export * from './subscribeNewHeads';
