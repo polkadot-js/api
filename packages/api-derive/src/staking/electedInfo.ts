f6af2570517a713c14ba2d821e79a713b666a54a
Jaco Greeff <jacogr@gmail.com>
2020-11-25 13:08:17 +0100

    derive.staking.electedInfo also contains current (#2874)

--- packages/api-derive/src/staking/electedInfo.ts
+++ packages/api-derive/src/staking/electedInfo.ts
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { ApiInterfaceRx } from '@polkadot/api/types';
+import { AccountId } from '@polkadot/types/interfaces';
 import { DeriveStakingElected } from '../types';
 
 import { Observable } from 'rxjs';
@@ -9,14 +10,19 @@ import { map, switchMap } from 'rxjs/operators';
 
 import { memo } from '../util';
 
+function combineAccounts (nextElected: AccountId[], validators: AccountId[]): AccountId[] {
+  return [...nextElected].concat(...validators.filter((v) => !nextElected.find((n) => n.eq(v))));
+}
+
 export function electedInfo (instanceId: string, api: ApiInterfaceRx): () => Observable<DeriveStakingElected> {
   return memo(instanceId, (): Observable<DeriveStakingElected> =>
     api.derive.staking.validators().pipe(
-      switchMap(({ nextElected }): Observable<DeriveStakingElected> =>
-        api.derive.staking.queryMulti(nextElected).pipe(
+      switchMap(({ nextElected, validators }): Observable<DeriveStakingElected> =>
+        api.derive.staking.queryMulti(combineAccounts(nextElected, validators)).pipe(
           map((info): DeriveStakingElected => ({
             info,
-            nextElected
+            nextElected,
+            validators
           }))
         )
       )
