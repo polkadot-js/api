bc129d67e65b4e457fc06c653d58418b512a4c82
Krzysztof Jelski <krzysztof@ethworks.io>
2020-12-08 10:05:58 +0100

    Attempt to remove duplication

--- packages/api-derive/src/treasury/bounties.ts
+++ packages/api-derive/src/treasury/bounties.ts
@@ -3,18 +3,12 @@
 
 import { DeriveBounties } from '@polkadot/api-derive/types';
 import { memo } from '@polkadot/api-derive/util';
-import { ApiInterfaceRx, RxResult } from '@polkadot/api/types';
+import { ApiInterfaceRx } from '@polkadot/api/types';
 import { Bytes, Option, StorageKey } from '@polkadot/types';
-import { Bounty, BountyIndex } from '@polkadot/types/interfaces';
+import { Bounty } from '@polkadot/types/interfaces';
 import { Codec } from '@polkadot/types/types';
 import { combineLatest, Observable } from 'rxjs';
 import { map, switchMap } from 'rxjs/operators';
-
-export type FetchBountiesInputs = {
-  keys: () => Observable<StorageKey[]>;
-  count: RxResult<() => Observable<BountyIndex>>;
-  bountiesQuery: (keys: Codec[]) => Observable<Option<Bounty>[]>
-};
 
 function parseResult ([maybeBounties, maybeDescriptions]: [Option<Bounty>[], Option<Bytes>[]]): DeriveBounties {
   const bounties: Bounty[] = [];
@@ -33,8 +27,12 @@ function parseResult ([maybeBounties, maybeDescriptions]: [Option<Bounty>[], Opt
   };
 }
 
-export function fetchBounties (api: ApiInterfaceRx): Observable<DeriveBounties> {
-  return api.query.treasury.bountyCount().pipe(
+function extractIds (keys: StorageKey[]): Codec[] {
+  return keys.map(({ args: [id] }: StorageKey) => id);
+}
+
+export function bounties (instanceId: string, api: ApiInterfaceRx): () => Observable<DeriveBounties> {
+  return memo(instanceId, (): Observable<DeriveBounties> => api.query.treasury.bountyCount().pipe(
     switchMap(() => api.query.treasury.bounties.keys()),
     switchMap((keys: StorageKey[]) => {
       return combineLatest([
@@ -42,13 +40,5 @@ export function fetchBounties (api: ApiInterfaceRx): Observable<DeriveBounties> 
         api.query.treasury.bountyDescriptions.multi<Option<Bytes>>(extractIds(keys))
       ]
       ).pipe(map(parseResult));
-    }));
-}
-
-export function bounties (instanceId: string, api: ApiInterfaceRx): () => Observable<DeriveBounties> {
-  return memo(instanceId, (): Observable<DeriveBounties> => fetchBounties(api));
-}
-
-function extractIds (keys: StorageKey[]): Codec[] {
-  return keys.map(({ args: [id] }: StorageKey) => id);
+    })));
 }
