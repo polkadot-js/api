b36dd6d36af93b98385eb305b6175e2545451ecc
Jaco Greeff <jacogr@gmail.com>
2020-12-16 09:19:30 +0100

    Simplify cjs

--- packages/x-rxjs/src/index.js
+++ packages/x-rxjs/src/index.js
@@ -1,11 +1,15 @@
 // Copyright 2017-2020 @polkadot/x-rxjs authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import rxjs from 'rxjs';
+const rxjs = require('rxjs');
 
-// This is quite horrible - this was lifted from rxjs/_esm5/index.js
-// The problem is that a simple `export * from 'rxjs'` has got named-export issues
-
-const { ArgumentOutOfRangeError, AsyncSubject, BehaviorSubject, ConnectableObservable, EMPTY, EmptyError, GroupedObservable, NEVER, Notification, NotificationKind, ObjectUnsubscribedError, Observable, ReplaySubject, Scheduler, Subject, Subscriber, Subscription, TimeoutError, UnsubscriptionError, VirtualAction, VirtualTimeScheduler, animationFrame, animationFrameScheduler, asap, asapScheduler, async, asyncScheduler, bindCallback, bindNodeCallback, combineLatest, concat, config, defer, empty, forkJoin, from, fromEvent, fromEventPattern, generate, identity, iif, interval, isObservable, merge, never, noop, observable, of, onErrorResumeNext, pairs, partition, pipe, queue, queueScheduler, race, range, scheduled, throwError, timer, using, zip } = rxjs;
-
-export { ArgumentOutOfRangeError, AsyncSubject, BehaviorSubject, ConnectableObservable, EMPTY, EmptyError, GroupedObservable, NEVER, Notification, NotificationKind, ObjectUnsubscribedError, Observable, ReplaySubject, Scheduler, Subject, Subscriber, Subscription, TimeoutError, UnsubscriptionError, VirtualAction, VirtualTimeScheduler, animationFrame, animationFrameScheduler, asap, asapScheduler, async, asyncScheduler, bindCallback, bindNodeCallback, combineLatest, concat, config, defer, empty, forkJoin, from, fromEvent, fromEventPattern, generate, identity, iif, interval, isObservable, merge, never, noop, observable, of, onErrorResumeNext, pairs, partition, pipe, queue, queueScheduler, race, range, scheduled, throwError, timer, using, zip };
+Object.keys(rxjs).forEach(function (key) {
+  if (key === 'default' || key === '__esModule') return;
+  if (key in exports && exports[key] === rxjs[key]) return;
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function () {
+      return rxjs[key];
+    }
+  });
+});
