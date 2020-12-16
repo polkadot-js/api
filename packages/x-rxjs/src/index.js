e5c42a5d66e819917f3f59ae4492e2b523b9fe68
Jaco Greeff <jacogr@gmail.com>
2020-12-16 14:44:58 +0100

    RxJs esm for non-node

--- packages/x-rxjs/src/index.js
+++ packages/x-rxjs/src/index.js
@@ -6,6 +6,9 @@
 //
 // Generated via s/export \{ (.*) \} from .*/exports.$1 = rxjs.$1;/ on rxjs/_esm
 
+// This is or our builder
+// @polkadot/dev: exports-node
+
 const rxjs = require('rxjs');
 
 exports.Observable = rxjs.Observable;
