a7f2abeef5489a00a53246b66cbca5bc7f62ea28
Jaco Greeff <jacogr@gmail.com>
2020-12-16 15:39:43 +0100

    header naming

--- packages/api/src/detectPackage.js
+++ packages/api/src/detectPackage.js
@@ -1,4 +1,4 @@
-// Copyright 2017-2020 @polkadot/metadata authors & contributors
+// Copyright 2017-2020 @polkadot/api authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
 const util = require('@polkadot/util');
