340cce6da2d1f09ccaffcf2488d0e5888aa126ba
Jaco Greeff <jacogr@gmail.com>
2020-12-16 12:11:58 +0100

    Comprehensive exports

--- packages/x-rxjs/src/index.d.ts
+++ packages/x-rxjs/src/index.d.ts
@@ -1,4 +1,4 @@
 // Copyright 2017-2020 @polkadot/x-rxjs authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-export { BehaviorSubject, ConnectableObservable, EMPTY, MonoTypeOperatorFunction, Observable, Observer, Subscription, TeardownLogic, asapScheduler, combineLatest, from, interval, of, timer } from 'rxjs';
+export * from 'rxjs';
