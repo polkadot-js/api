82cab2827a95a10bcbdef88d80cf22bd4f235964
Jaco Greeff <jacogr@gmail.com>
2021-01-15 12:23:19 +0100

    Add lottery types (#3059)

---
+++ packages/types/src/interfaces/lottery/types.ts
@@ -0,0 +1,20 @@
+// Auto-generated via `yarn polkadot-types-from-defs`, do not edit
+/* eslint-disable */
+
+import type { Struct, bool, u8 } from '@polkadot/types';
+import type { ITuple } from '@polkadot/types/types';
+import type { Balance, BlockNumber } from '@polkadot/types/interfaces/runtime';
+
+/** @name CallIndex */
+export interface CallIndex extends ITuple<[u8, u8]> {}
+
+/** @name LotteryConfig */
+export interface LotteryConfig extends Struct {
+  readonly price: Balance;
+  readonly start: BlockNumber;
+  readonly length: BlockNumber;
+  readonly delay: BlockNumber;
+  readonly repeat: bool;
+}
+
+export type PHANTOM_LOTTERY = 'lottery';
