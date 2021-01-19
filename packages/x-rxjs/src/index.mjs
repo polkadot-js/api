ec8c0d3e8bc383ca9c816aef3e1e8c1fb0dfc34e
Jaco Greeff <jacogr@gmail.com>
2021-01-19 22:08:03 +0100

    Move x-rxjs into common (#3078)
    * Moe x-rxjs into common
    
    * yarn
    
    * jest config
    
    * Pull in new pubslished x-rxjs

--- packages/x-rxjs/src/index.mjs
+++
@@ -1,4 +0,0 @@
-// Copyright 2017-2021 @polkadot/x-rxjs authors & contributors
-// SPDX-License-Identifier: Apache-2.0
-
-export { animationFrame, animationFrameScheduler, ArgumentOutOfRangeError, asap, asapScheduler, async, asyncScheduler, AsyncSubject, BehaviorSubject, bindCallback, bindNodeCallback, combineLatest, concat, config, ConnectableObservable, defer, EMPTY, empty, EmptyError, forkJoin, from, fromEvent, fromEventPattern, generate, GroupedObservable, identity, iif, interval, isObservable, merge, NEVER, never, noop, Notification, NotificationKind, ObjectUnsubscribedError, Observable, observable, of, onErrorResumeNext, pairs, partition, pipe, queue, queueScheduler, race, range, ReplaySubject, scheduled, Scheduler, Subject, Subscriber, Subscription, throwError, TimeoutError, timer, UnsubscriptionError, using, VirtualAction, VirtualTimeScheduler, zip } from 'rxjs/_esm5/index.js';
