dd9ba362b756ef2f794b25adb7b7fbff7f10b582
Jaco Greeff <jacogr@gmail.com>
2021-01-23 09:57:05 +0100

    u8 decimals/format

--- packages/types/src/generic/ChainProperties.ts
+++ packages/types/src/generic/ChainProperties.ts
@@ -9,13 +9,13 @@ import { Json } from '../codec/Json';
 import { Option } from '../codec/Option';
 import { Vec } from '../codec/Vec';
 import { Text } from '../primitive/Text';
-import { u32 } from '../primitive/U32';
+import { u8 } from '../primitive/U8';
 
 function createType (registry: Registry, type: string, value: unknown, asArray = true): Codec {
   // We detect codec here as well - when found, generally this is constructed from itself
   return isFunction((value as Codec).toHuman)
     ? value as Codec
-    : registry.createType(type as 'Option<u32>', asArray
+    : registry.createType(type as 'Option<u8>', asArray
       ? isNull(value)
         ? null
         : Array.isArray(value)
@@ -33,9 +33,9 @@ function decode (registry: Registry, value?: Map<string, unknown> | Record<strin
       : Object.entries(value || {})
   ).reduce((all: Record<string, unknown>, [key, value]) => {
     all[key] = key === 'ss58Format'
-      ? createType(registry, 'Option<u32>', value, false)
+      ? createType(registry, 'Option<u8>', value, false)
       : key === 'tokenDecimals'
-        ? createType(registry, 'Option<Vec<u32>>' as 'Vec<u32>', value)
+        ? createType(registry, 'Option<Vec<u8>>' as 'Vec<u8>', value)
         : key === 'tokenSymbol'
           ? createType(registry, 'Option<Vec<Text>>' as 'Vec<Text>', value)
           : value;
@@ -45,11 +45,11 @@ function decode (registry: Registry, value?: Map<string, unknown> | Record<strin
 
   // fill in the defaults
   if (isUndefined(mapped.ss58Format)) {
-    mapped.ss58Format = registry.createType('Option<u32>');
+    mapped.ss58Format = registry.createType('Option<u8>');
   }
 
   if (isUndefined(mapped.tokenDecimals)) {
-    mapped.tokenDecimals = registry.createType('Option<Vec<u32>>' as 'Vec<u32>');
+    mapped.tokenDecimals = registry.createType('Option<Vec<u8>>' as 'Vec<u8>');
   }
 
   if (isUndefined(mapped.tokenSymbol)) {
@@ -67,15 +67,15 @@ export class GenericChainProperties extends Json {
   /**
    * @description The chain ss58Format
    */
-  public get ss58Format (): Option<u32> {
-    return this.get('ss58Format') as Option<u32>;
+  public get ss58Format (): Option<u8> {
+    return this.get('ss58Format') as Option<u8>;
   }
 
   /**
    * @description The decimals for each of the tokens
    */
-  public get tokenDecimals (): Option<Vec<u32>> {
-    return this.get('tokenDecimals') as Option<Vec<u32>>;
+  public get tokenDecimals (): Option<Vec<u8>> {
+    return this.get('tokenDecimals') as Option<Vec<u8>>;
   }
 
   /**
