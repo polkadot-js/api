05472bdaa800363164501e5e788c2ebd4c3ba438
joelamouche <estienne.antoine@gmail.com>
2021-01-25 17:43:35 +0100

    refactor and lint

--- packages/types/src/generic/ConsensusEngineId.ts
+++ packages/types/src/generic/ConsensusEngineId.ts
@@ -88,6 +88,10 @@ export class GenericConsensusEngineId extends u32 {
     return this.registry.createType('AccountId', bytes);
   }
 
+  private _getH160Author (bytes: Bytes): AccountId {
+    return this.registry.createType('AccountId', bytes);
+  }
+
   /**
    * @description From the input bytes, decode into an author
    */
@@ -100,13 +104,13 @@ export class GenericConsensusEngineId extends u32 {
       }
     }
 
-    // Moonbeam is neither Aura nor Babe and uses h160 addresses
-    if (bytes.length===20){
-      return this.registry.createType('AccountId', bytes); //bytes.toString() as AccountId
-    }
-
     if (this.isPow) {
       return this._getPowAuthor(bytes);
+    }
+
+    // Moonbeam is neither Aura nor Babe nor Pow and uses h160 addresses
+    if (bytes.length === 20) {
+      return this._getH160Author(bytes);
     }
 
     return undefined;
