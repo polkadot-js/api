10ec4e6c3c93481404e0779e7a1b980e1adca043
Jaco Greeff <jacogr@gmail.com>
2021-02-26 17:28:33 +0100

    Bump deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,8 +24,6 @@ module.exports = {
     ...base.rules,
     // add override for any (a metric ton of them, initial conversion)
     '@typescript-eslint/no-explicit-any': 'off',
-    // these should be removed, there are 8 of them as errors
-    '@typescript-eslint/no-non-null-assertion': 'off',
     // this seems very broken atm, false positives
     '@typescript-eslint/unbound-method': 'off'
   }
