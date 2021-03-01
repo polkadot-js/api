2afff1b41fcd6c8ecc76d31122ee4d3155cf6973
Alex D <pr0fedt@gmail.com>
2021-03-01 10:42:07 +0300

    Use derive customAccount and customLocks for balance overrides (#3248)
    * modified api-derive balances to allow use of custom balance storages via derives; current implementation only works with standard call signatures
    
    * type predicate as filter value
    
    * renamed derive method name

--- packages/api-derive/src/balances/account.ts
+++ packages/api-derive/src/balances/account.ts
@@ -54,8 +54,18 @@ function queryBalancesFree (api: ApiInterfaceRx, accountId: AccountId): Observab
   );
 }
 
+type DeriveCustomAccount = ApiInterfaceRx['derive'] & { [custom: string]: {
+  customAccount?: ApiInterfaceRx['query']['balances']['account']
+} }
+
 function queryBalancesAccount (api: ApiInterfaceRx, accountId: AccountId, modules: string[] = ['balances']): Observable<Result> {
-  const balances = modules.map((m): QueryableStorageMultiArg<'rxjs'> => [api.query[m].account, accountId]);
+  const balances = modules.map(
+    (m): QueryableStorageMultiArg<'rxjs'> => [
+      (api.derive as DeriveCustomAccount)[m]?.customAccount ?? api.query[m].account,
+      accountId
+    ]
+  );
+
   const extract = (data: AccountData[]) =>
     data.map(({ feeFrozen, free, miscFrozen, reserved }): BalanceResult => [free, reserved, feeFrozen, miscFrozen]);
 
