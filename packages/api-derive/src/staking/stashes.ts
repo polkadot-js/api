36fad011ddda3f8da0dba5de5bbfffc145e99285
Jaco Greeff <jacogr@gmail.com>
2021-03-01 16:21:14 +0100

    Trigger validator shash retrievals on Bonded events

--- packages/api-derive/src/staking/stashes.ts
+++ packages/api-derive/src/staking/stashes.ts
@@ -5,16 +5,41 @@ import type { ApiInterfaceRx } from '@polkadot/api/types';
 import type { AccountId } from '@polkadot/types/interfaces';
 import type { Observable } from '@polkadot/x-rxjs';
 
-import { map } from '@polkadot/x-rxjs/operators';
+import { map, startWith, switchMap } from '@polkadot/x-rxjs/operators';
 
-import { memo } from '../util';
+import { drr, memo } from '../util';
+
+function onBondedEvent (api: ApiInterfaceRx): Observable<number> {
+  let current = Date.now();
+
+  return api.query.system.events().pipe(
+    map((events): number => {
+      current = events.filter(({ event, phase }): boolean => {
+        try {
+          return phase.isApplyExtrinsic &&
+            event.section === 'staking' &&
+            event.method === 'Bonded';
+        } catch {
+          return false;
+        }
+      })
+        ? Date.now()
+        : current;
+
+      return current;
+    }),
+    startWith(current),
+    drr({ skipTimeout: true })
+  );
+}
 
 /**
  * @description Retrieve the list of all validator stashes
  */
 export function stashes (instanceId: string, api: ApiInterfaceRx): () => Observable<AccountId[]> {
   return memo(instanceId, (): Observable<AccountId[]> =>
-    api.query.staking.validators.keys().pipe(
+    onBondedEvent(api).pipe(
+      switchMap(() => api.query.staking.validators.keys()),
       map((keys) => keys.map(({ args: [validatorId] }) => validatorId).filter((a) => a))
     )
   );
