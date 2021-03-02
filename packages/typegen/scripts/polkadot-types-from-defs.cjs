a5375a18bea6fbdab37127e9fc54c9dc809f666f
Jaco Greeff <jacogr@gmail.com>
2021-03-02 12:58:29 +0100

    Default build to type: module

--- packages/typegen/scripts/polkadot-types-from-defs.cjs
+++ packages/typegen/scripts/polkadot-types-from-defs.cjs
@@ -7,14 +7,17 @@
 let main;
 
 try {
-  main = require('../fromChain').main;
+  main = require('../fromDefs.cjs').main;
 } catch (error) {
+  process.env.JEST_WORKER_ID = '123';
+
   require('@babel/register')({
     extensions: ['.js', '.ts'],
     plugins: [
       ['module-resolver', {
         alias: {
           '^@polkadot/metadata(.*)': './packages/metadata/src\\1',
+          '^@polkadot/typegen(.*)': './packages/typegen/src\\1',
           '^@polkadot/types-known(.*)': './packages/types-known/src\\1',
           '^@polkadot/types(.*)': './packages/types/src\\1'
         }
@@ -22,7 +25,7 @@ try {
     ]
   });
 
-  main = require('../src/fromChain').main;
+  main = require('../src/fromDefs.ts').main;
 }
 
 main();
