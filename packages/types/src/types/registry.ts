ef2a894966113547e1cf56d27c36a43a268dc548
iiiiiiii <lllliliiii@outlook.com>
2021-03-05 20:39:36 +1300

    Update registry.ts

--- packages/types/src/types/registry.ts
+++ packages/types/src/types/registry.ts
@@ -98,6 +98,7 @@ export interface Registry {
   readonly chainSS58: number | undefined;
   readonly chainTokens: string[];
   readonly knownTypes: RegisteredTypes;
+  readonly unknownTypes: Map<string, boolean>;
   readonly signedExtensions: string[];
 
   findMetaCall (callIndex: Uint8Array): CallFunction;
