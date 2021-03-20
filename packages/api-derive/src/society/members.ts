23c38a248c2ab6a6fddcbea18ed4d0d10b9f51a1
Jaco Greeff <jacogr@gmail.com>
2021-03-20 18:07:04 +0100

    Optimize society multi-derive

--- packages/api-derive/src/society/members.ts
+++ packages/api-derive/src/society/members.ts
@@ -2,14 +2,42 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import type { ApiInterfaceRx } from '@polkadot/api/types';
-import type { AccountId } from '@polkadot/types/interfaces';
+import type { bool, Option, Vec } from '@polkadot/types';
+import type { AccountId, Balance, BlockNumber, SocietyVote, StrikeCount, VouchingStatus } from '@polkadot/types/interfaces';
+import type { ITuple } from '@polkadot/types/types';
 import type { Observable } from '@polkadot/x-rxjs';
 import type { DeriveSocietyMember } from '../types';
 
-import { combineLatest } from '@polkadot/x-rxjs';
-import { switchMap } from '@polkadot/x-rxjs/operators';
+import { combineLatest, of } from '@polkadot/x-rxjs';
+import { map, switchMap } from '@polkadot/x-rxjs/operators';
 
 import { memo } from '../util';
+
+type Result = [AccountId[], Vec<ITuple<[BlockNumber, Balance]>>[], StrikeCount[], Option<SocietyVote>[], bool[], Option<VouchingStatus>[]];
+
+export function _members (instanceId: string, api: ApiInterfaceRx): (accountIds: AccountId[]) => Observable<DeriveSocietyMember[]> {
+  return memo(instanceId, (accountIds: AccountId[]): Observable<DeriveSocietyMember[]> =>
+    combineLatest<Result>([
+      of(accountIds),
+      api.query.society.payouts.multi(accountIds),
+      api.query.society.strikes.multi(accountIds),
+      api.query.society.defenderVotes.multi(accountIds),
+      api.query.society.suspendedMembers.multi(accountIds),
+      api.query.society.vouching.multi(accountIds)
+    ]).pipe(
+      map(([accountIds, payouts, strikes, defenderVotes, suspended, vouching]) =>
+        accountIds.map((accountId, index) => ({
+          accountId,
+          isSuspended: suspended[index].isTrue,
+          payouts: payouts[index],
+          strikes: strikes[index],
+          vote: defenderVotes[index].unwrapOr(undefined),
+          vouching: vouching[index].unwrapOr(undefined)
+        }))
+      )
+    )
+  );
+}
 
 /**
  * @description Get the member info for a society
@@ -17,13 +45,7 @@ import { memo } from '../util';
 export function members (instanceId: string, api: ApiInterfaceRx): () => Observable<DeriveSocietyMember[]> {
   return memo(instanceId, (): Observable<DeriveSocietyMember[]> =>
     api.query.society.members<AccountId[]>().pipe(
-      switchMap((members): Observable<DeriveSocietyMember[]> =>
-        combineLatest(
-          members.map((accountId): Observable<DeriveSocietyMember> =>
-            api.derive.society.member(accountId)
-          )
-        )
-      )
+      switchMap((members) => api.derive.society._members(members))
     )
   );
 }
