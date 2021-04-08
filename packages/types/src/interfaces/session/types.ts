ed5f2673cebc30094c219f4d7b0ceb90d981392f
Jaco Greeff <jacogr@gmail.com>
2021-04-08 15:07:39 +0300

    Detect Keys, inject SessionKeys (#3380)

--- packages/types/src/interfaces/session/types.ts
+++ packages/types/src/interfaces/session/types.ts
@@ -1,10 +1,13 @@
 // Auto-generated via `yarn polkadot-types-from-defs`, do not edit
 /* eslint-disable */
 
-import type { Bytes, Struct, Vec, u32 } from '@polkadot/types';
+import type { Bytes, Struct, U8aFixed, Vec, u32 } from '@polkadot/types';
 import type { ITuple } from '@polkadot/types/types';
 import type { AccountId, ValidatorId } from '@polkadot/types/interfaces/runtime';
 import type { Exposure } from '@polkadot/types/interfaces/staking';
+
+/** @name BeefyKey */
+export interface BeefyKey extends U8aFixed {}
 
 /** @name FullIdentification */
 export interface FullIdentification extends Exposure {}
@@ -28,6 +31,12 @@ export interface SessionIndex extends u32 {}
 /** @name SessionKeys1 */
 export interface SessionKeys1 extends AccountId {}
 
+/** @name SessionKeys10 */
+export interface SessionKeys10 extends ITuple<[AccountId, AccountId, AccountId, AccountId, AccountId, AccountId, AccountId, AccountId, AccountId, AccountId]> {}
+
+/** @name SessionKeys10B */
+export interface SessionKeys10B extends ITuple<[AccountId, AccountId, AccountId, AccountId, AccountId, AccountId, AccountId, AccountId, AccountId, BeefyKey]> {}
+
 /** @name SessionKeys2 */
 export interface SessionKeys2 extends ITuple<[AccountId, AccountId]> {}
 
@@ -43,14 +52,26 @@ export interface SessionKeys5 extends ITuple<[AccountId, AccountId, AccountId, A
 /** @name SessionKeys6 */
 export interface SessionKeys6 extends ITuple<[AccountId, AccountId, AccountId, AccountId, AccountId, AccountId]> {}
 
+/** @name SessionKeys6B */
+export interface SessionKeys6B extends ITuple<[AccountId, AccountId, AccountId, AccountId, AccountId, BeefyKey]> {}
+
 /** @name SessionKeys7 */
 export interface SessionKeys7 extends ITuple<[AccountId, AccountId, AccountId, AccountId, AccountId, AccountId, AccountId]> {}
+
+/** @name SessionKeys7B */
+export interface SessionKeys7B extends ITuple<[AccountId, AccountId, AccountId, AccountId, AccountId, AccountId, BeefyKey]> {}
 
 /** @name SessionKeys8 */
 export interface SessionKeys8 extends ITuple<[AccountId, AccountId, AccountId, AccountId, AccountId, AccountId, AccountId, AccountId]> {}
 
+/** @name SessionKeys8B */
+export interface SessionKeys8B extends ITuple<[AccountId, AccountId, AccountId, AccountId, AccountId, AccountId, AccountId, BeefyKey]> {}
+
 /** @name SessionKeys9 */
 export interface SessionKeys9 extends ITuple<[AccountId, AccountId, AccountId, AccountId, AccountId, AccountId, AccountId, AccountId, AccountId]> {}
+
+/** @name SessionKeys9B */
+export interface SessionKeys9B extends ITuple<[AccountId, AccountId, AccountId, AccountId, AccountId, AccountId, AccountId, AccountId, BeefyKey]> {}
 
 /** @name ValidatorCount */
 export interface ValidatorCount extends u32 {}
