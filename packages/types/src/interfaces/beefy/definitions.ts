b6da84b8b2674eeaba7cbc0083753357ec18ee7a
Jaco <jacogr@gmail.com>
2021-04-13 11:59:23 +0300

    Beefy subscribeJustifications (#3393)
    * Beefy subscribeJustifications
    
    * Add payload info
    
    * Update RPC description

--- packages/types/src/interfaces/beefy/definitions.ts
+++ packages/types/src/interfaces/beefy/definitions.ts
@@ -7,13 +7,35 @@
 import type { Definitions } from '../../types';
 
 export default {
-  rpc: {},
+  rpc: {
+    subscribeJustifications: {
+      description: 'Returns the block most recently finalized by BEEFY, alongside side its justification.',
+      params: [],
+      pubsub: [
+        'justifications',
+        'subscribeJustifications',
+        'unsubscribeJustifications'
+      ],
+      type: 'BeefySignedCommitment'
+    }
+  },
   types: {
+    BeefyCommitment: {
+      payload: 'BeefyPayload',
+      blockNumber: 'BlockNumber',
+      validatorSetId: 'ValidatorSetId'
+    },
+    BeefySignedCommitment: {
+      commitment: 'BeefyCommitment',
+      signatures: 'Vec<Option<Signature>>'
+    },
     BeefyNextAuthoritySet: {
       id: 'u64',
       len: 'u32',
       root: 'H256'
     },
+    BeefyPayload: 'MmrRootHash',
+    MmrRootHash: 'H256',
     ValidatorSetId: 'u64'
   }
 } as Definitions;
