99bcd5416ebd3bfa96997469949d67f032109d86
Jaco Greeff <jacogr@gmail.com>
2021-05-07 11:25:47 +0300

    Add tests for bounded

--- packages/types/src/create/sanitize.spec.ts
+++ packages/types/src/create/sanitize.spec.ts
@@ -1,32 +1,32 @@
 // Copyright 2017-2021 @polkadot/types authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { alias, removeColons } from './sanitize';
+import { alias, removeBounded, removeColons } from './sanitize';
 
 describe('sanitize', (): void => {
   describe('alias', (): void => {
-    const exec = alias('String', 'Text');
+    const fn = alias('String', 'Text');
 
     it('replaces all occurrences for types', (): void => {
-      expect(exec('(String,Address,MasterString,String)')).toEqual(
+      expect(fn('(String,Address,MasterString,String)')).toEqual(
         '(Text,Address,MasterString,Text)'
       );
     });
 
     it('replaces actual types, but leaves struct names', (): void => {
-      expect(exec('{"system":"String","versionString":"String"}')).toEqual(
+      expect(fn('{"system":"String","versionString":"String"}')).toEqual(
         '{"system":"Text","versionString":"Text"}'
       );
     });
 
     it('handles the preceding correctly', (): void => {
       // NOTE This type doesn't make sense
-      expect(exec('String String (String,[String;32],String)"String<String>')).toEqual(
+      expect(fn('String String (String,[String;32],String)"String<String>')).toEqual(
         'Text Text (Text,[Text;32],Text)"Text<Text>'
       );
     });
 
-    it('handles emdedded Vec/Tuples', (): void => {
+    it('handles embedded Vec/Tuples', (): void => {
       const ann = alias('Announcement', 'ProxyAnnouncement');
 
       expect(ann('(Vec<Announcement>,BalanceOf)')).toEqual(
@@ -36,24 +36,38 @@ describe('sanitize', (): void => {
   });
 
   describe('removeColons', (): void => {
+    const fn = removeColons();
+
     it('removes preceding ::Text -> Text', (): void => {
-      expect(removeColons()('::Text')).toEqual('Text');
+      expect(fn('::Text')).toEqual('Text');
     });
 
     it('removes middle voting::TallyType -> TallyType', (): void => {
-      expect(removeColons()('voting::TallyType')).toEqual('TallyType');
+      expect(fn('voting::TallyType')).toEqual('TallyType');
     });
 
     it('removes on embedded values (one)', (): void => {
-      expect(removeColons()('(T::AccountId, SpanIndex)')).toEqual('(AccountId, SpanIndex)');
+      expect(fn('(T::AccountId, SpanIndex)')).toEqual('(AccountId, SpanIndex)');
     });
 
     it('removes on embedded values (all)', (): void => {
-      expect(removeColons()('(T::AccountId, slashing::SpanIndex)')).toEqual('(AccountId, SpanIndex)');
+      expect(fn('(T::AccountId, slashing::SpanIndex)')).toEqual('(AccountId, SpanIndex)');
     });
 
     it('keeps with allowNamespaces', (): void => {
-      expect(removeColons()('::slashing::SpanIndex', { allowNamespaces: true })).toEqual('slashing::SpanIndex');
+      expect(fn('::slashing::SpanIndex', { allowNamespaces: true })).toEqual('slashing::SpanIndex');
+    });
+  });
+
+  describe('bounded', (): void => {
+    const fn = removeBounded();
+
+    it('correctly cleans up bounded values', (): void => {
+      expect(fn('BoundedVec<u32>')).toEqual('Vec<u32>');
+    });
+
+    it('correctly cleans up nested bounded values', (): void => {
+      expect(fn('BoundedBTreeMap<BoundedVec<BoundedVec<u32>>, BTreeSet<u32, BoundedVec<u64>>>')).toEqual('BTreeMap<Vec<Vec<u32>>, BTreeSet<u32, Vec<u64>>>');
     });
   });
 });
