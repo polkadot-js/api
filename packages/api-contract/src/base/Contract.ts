2e84198f157f0e0487fa53444094a488322314c2
Jaco Greeff <jacogr@gmail.com>
2021-05-09 10:33:24 +0300

    Raw directly

--- packages/api-contract/src/base/Contract.ts
+++ packages/api-contract/src/base/Contract.ts
@@ -146,7 +146,7 @@ export class Contract<ApiType extends ApiTypes> extends Base<ApiType> {
               debugMessage,
               gasConsumed,
               output: result.isOk && message.returnType
-                ? createTypeUnsafe(this.registry, message.returnType.type, [result.asOk.data.toU8a()], { isPedantic: true })
+                ? createTypeUnsafe(this.registry, message.returnType.type, [result.asOk.data], { isPedantic: true })
                 : null,
               result
             }))
