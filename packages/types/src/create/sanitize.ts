d227a4a93e93a86a5482c920206d76c799027d42
Jaco Greeff <jacogr@gmail.com>
2021-05-18 15:19:22 +0300

    Bytes

--- packages/types/src/create/sanitize.ts
+++ packages/types/src/create/sanitize.ts
@@ -29,10 +29,10 @@ const mappings: Mapper[] = [
   removeGenerics(),
   // alias String -> Text (compat with jsonrpc methods)
   alias('String', 'Text'),
-  alias("&'static[u8]", 'Text'),
   // alias Vec<u8> -> Bytes
   alias('Vec<u8>', 'Bytes'),
   alias('&\\[u8\\]', 'Bytes'),
+  alias("&'static\\[u8\\]", 'Bytes'),
   // alias RawAddress -> Address
   alias('RawAddress', 'Address'),
   // lookups, mapped to Address/AccountId as appropriate in runtime
