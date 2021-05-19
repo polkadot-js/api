a2182526d87f82595614c273908d5e65382b1c5d
Jaco Greeff <jacogr@gmail.com>
2021-05-19 16:04:36 +0300

    DisallowSigned extension

--- packages/types/src/extrinsic/signedExtensions/index.ts
+++ packages/types/src/extrinsic/signedExtensions/index.ts
@@ -4,13 +4,18 @@
 import type { ExtDef, ExtInfo, ExtTypes } from './types';
 
 import { polkadot } from './polkadot';
+import { shell } from './shell';
 import { substrate } from './substrate';
 
 // A mapping of the known signed extensions to the extra fields that they contain. Unlike in the actual extensions,
 // we define the extra fields not as a Tuple, but rather as a struct so they can be named. These will be expanded
 // into the various fields when added to the payload (we only support V4 onwards with these, V3 and earlier are
 // deemed fixed and non-changeable)
-const allExtensions: ExtDef = { ...substrate, ...polkadot };
+const allExtensions: ExtDef = {
+  ...substrate,
+  ...shell,
+  ...polkadot
+};
 
 // the v4 signed extensions (the order is important here, as applied by default)
 const defaultExtensions: Array<keyof typeof allExtensions> = [
---
+++ packages/types/src/extrinsic/signedExtensions/shell.ts
@@ -0,0 +1,10 @@
+// Copyright 2017-2021 @polkadot/types authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import type { ExtDef } from './types';
+
+import { emptyCheck } from './emptyCheck';
+
+export const shell: ExtDef = {
+  DisallowSigned: emptyCheck
+};
