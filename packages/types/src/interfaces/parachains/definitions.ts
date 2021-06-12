33dcc9c8aa0c3666aaec4efc6b1abbf6be79ce31
André Silva <123550+andresilva@users.noreply.github.com>
2021-06-12 15:52:20 +0100

    add support for ValidationCodeHash (#3640)

--- packages/types/src/interfaces/parachains/definitions.ts
+++ packages/types/src/interfaces/parachains/definitions.ts
@@ -111,7 +111,7 @@ export default {
       erasureRoot: 'Hash',
       signature: 'CollatorSignature',
       paraHead: 'Hash',
-      validationCodeHash: 'Hash'
+      validationCodeHash: 'ValidationCodeHash'
     },
     CandidateHash: 'Hash',
     CandidateInfo: {
@@ -381,6 +381,7 @@ export default {
       codeUpgradeAllowed: 'Option<RelayChainBlockNumber>'
     },
     ValidationCode: 'Bytes',
+    ValidationCodeHash: 'Hash',
     ValidationData: {
       persisted: 'PersistedValidationData',
       transient: 'TransientValidationData'
