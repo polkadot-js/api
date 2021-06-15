0510f58cabdcbb06c423ce180bc648331650b5fe
Jaco Greeff <jacogr@gmail.com>
2021-06-15 10:07:50 +0300

    Additional check for API connection & decoration in contracts

--- packages/api-contract/src/base/Base.ts
+++ packages/api-contract/src/base/Base.ts
@@ -26,6 +26,7 @@ export abstract class Base<ApiType extends ApiTypes> {
     this.registry = this.abi.registry;
     this._decorateMethod = decorateMethod;
 
-    assert(isFunction(api.tx.contracts?.instantiateWithCode), 'You need to connect to a chain with a runtime with a V3 contracts module. The runtime does not expose api.tx.contracts.instantiateWithCode');
+    assert(!!(api && api.isConnected && api.tx && api.tx.contracts && Object.keys(api.tx.contracts).length), 'Your API has not been initialized correctly and it not decorated with the runtime interfaces for contracts as retrieved from the on-chain runtime');
+    assert(isFunction(api.tx.contracts.instantiateWithCode), 'You need to connect to a chain with a runtime with a V3 contracts module. The runtime does not expose api.tx.contracts.instantiateWithCode');
   }
 }
