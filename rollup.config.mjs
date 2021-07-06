3c0631523969c6570c82c0a795c06fd56f9ec30a
Jaco Greeff <jacogr@gmail.com>
2021-07-06 08:35:46 +0300

    use createBundle

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,76 +1,37 @@
 // Copyright 2019-2021 @polkadot/wasm-crypto authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { createInput, createOutput, createPlugins } from '@polkadot/dev/config/rollup';
+import { createBundle } from '@polkadot/dev/config/rollup';
 
-const external = [
+const pkgs = [
   '@polkadot/api',
   '@polkadot/api-contract',
   '@polkadot/api-derive',
-  '@polkadot/keyring',
   '@polkadot/metadata',
-  '@polkadot/networks',
   '@polkadot/rpc-core',
   '@polkadot/rpc-provider',
   '@polkadot/types',
-  '@polkadot/types-known',
+  '@polkadot/types-known'
+];
+
+const external = [
+  ...pkgs,
+  '@polkadot/keyring',
+  '@polkadot/networks',
   '@polkadot/util',
   '@polkadot/util-crypto',
   '@polkadot/wasm-crypto',
   '@polkadot/x-fetch',
   '@polkadot/x-global',
   '@polkadot/x-randomvalues',
+  '@polkadot/x-rxjs',
   '@polkadot/x-textdecoder',
-  '@polkadot/x-textencoder'
+  '@polkadot/x-textencoder',
+  '@polkadot/x-ws'
 ];
 
-export default [
-  {
-    external,
-    input: createInput('api'),
-    output: createOutput('api', external),
-    plugins: createPlugins()
-  },
-  {
-    external,
-    input: createInput('api-contract'),
-    output: createOutput('api-contract', external),
-    plugins: createPlugins()
-  },
-  {
-    external,
-    input: createInput('api-derive'),
-    output: createOutput('api-derive', external),
-    plugins: createPlugins()
-  },
-  {
-    external,
-    input: createInput('metadata'),
-    output: createOutput('metadata', external),
-    plugins: createPlugins()
-  },
-  {
-    external,
-    input: createInput('rpc-core'),
-    output: createOutput('rpc-core', external),
-    plugins: createPlugins()
-  },
-  {
-    external,
-    input: createInput('rpc-provider'),
-    output: createOutput('rpc-provider', external),
-    plugins: createPlugins()
-  },
-  {
-    external,
-    input: createInput('types'),
-    output: createOutput('types', external),
-    plugins: createPlugins()
-  },
-  {
-    external,
-    input: createInput('types-known'),
-    output: createOutput('types-known', external),
-    plugins: createPlugins()
-  }
-];
+const overrides = {};
+
+export default pkgs.map((pkg) =>
+  createBundle({ ...(overrides[pkg] || {}), external, pkg })
+);
