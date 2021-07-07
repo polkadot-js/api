2fd8b4da233bad4d19b8b6a6c57c37a228b57859
Jaco Greeff <jacogr@gmail.com>
2021-07-07 13:02:26 +0300

    Internal helpers

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,17 +1,15 @@
 // Copyright 2017-2021 @polkadot/api authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+import path from 'path';
+
 import { createBundle } from '@polkadot/dev/config/rollup';
 
 const pkgs = [
   '@polkadot/api',
   '@polkadot/api-contract',
-  '@polkadot/api-derive',
   '@polkadot/metadata',
-  '@polkadot/rpc-core',
-  '@polkadot/rpc-provider',
-  '@polkadot/types',
-  '@polkadot/types-known'
+  '@polkadot/types'
 ];
 
 const external = [
@@ -23,8 +21,20 @@ const external = [
   '@polkadot/wasm-crypto'
 ];
 
+const entries = ['api-derive', 'rpc-core', 'rpc-provider', 'types-known'].map((p) => ({
+  find: `@polkadot/${p}`,
+  replacement: path.resolve(process.cwd(), `packages/${p}/build`)
+}));
+
 const overrides = {};
 
-export default pkgs.map((pkg) =>
-  createBundle({ external, pkg, ...(overrides[pkg] || {}) })
-);
+export default pkgs.map((pkg) => {
+  const override = (overrides[pkg] || {});
+
+  return createBundle({
+    external,
+    pkg,
+    ...override,
+    entries: entries.concat(...(override.entries || []))
+  });
+});
