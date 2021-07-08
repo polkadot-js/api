18a0ba8e3a25ab9810f7010ef7680ecab9ade001
Jaco Greeff <jacogr@gmail.com>
2021-07-08 08:44:28 +0300

    Cleanups

--- packages/api-derive/src/crowdloan/contributions.ts
+++ packages/api-derive/src/crowdloan/contributions.ts
@@ -4,7 +4,7 @@
 import type { Observable } from 'rxjs';
 import type { ApiInterfaceRx } from '@polkadot/api/types';
 import type { Option, StorageKey } from '@polkadot/types';
-import type { FundInfo, ParaId, TrieIndex } from '@polkadot/types/interfaces';
+import type { EventRecord, FundInfo, ParaId, TrieIndex } from '@polkadot/types/interfaces';
 import type { DeriveContributions } from '../types';
 
 import { BehaviorSubject, combineLatest, EMPTY, map, of, startWith, switchMap, tap, toArray } from 'rxjs';
@@ -16,17 +16,8 @@ import { memo } from '../util';
 
 interface Changes {
   added: string[];
-  blockHash: string;
   removed: string[];
 }
-
-const EMPTY_RESULT: DeriveContributions = {
-  blockHash: '-',
-  childKey: '',
-  contributorsHex: [],
-  contributorsMap: {},
-  hasKeys: true
-};
 
 const PAGE_SIZE_K = 1000; // limit aligned with the 1k on the node (trie lookups are heavy)
 
@@ -41,30 +32,42 @@ function createChildKey (trieIndex: TrieIndex): string {
   );
 }
 
+function _extractChanges (paraId: string | number | ParaId, events: EventRecord[]): Changes {
+  return events
+    .filter(({ event: { data: [, eventParaId], method, section } }) =>
+      section === 'crowdloan' &&
+      ['Contributed', 'Withdrew'].includes(method) &&
+      eventParaId.eq(paraId)
+    )
+    .reduce((result: Changes, { event: { data: [accountId], method } }): Changes => {
+      if (method === 'Contributed') {
+        result.added.push(accountId.toHex());
+      } else {
+        result.removed.push(accountId.toHex());
+      }
+
+      return result;
+    }, { added: [], removed: [] });
+}
+
 function _getUpdates (api: ApiInterfaceRx, paraId: string | number | ParaId): Observable<Changes> {
+  let added: string[] = [];
+  let removed: string[] = [];
+
   return api.query.system.events().pipe(
     switchMap((events): Observable<Changes> => {
-      const changes = events
-        .filter(({ event: { data: [, eventParaId], method, section } }) =>
-          section === 'crowdloan' &&
-          ['Contributed', 'Withdrew'].includes(method) &&
-          eventParaId.eq(paraId)
-        )
-        .reduce((result: Changes, { event: { data: [accountId], method } }): Changes => {
-          if (method === 'Contributed') {
-            result.added.push(accountId.toHex());
-          } else {
-            result.removed.push(accountId.toHex());
-          }
+      const changes = _extractChanges(paraId, events);
 
-          return result;
-        }, { added: [], blockHash: events.createdAtHash?.toHex() || '-', removed: [] });
+      if (changes.added.length || changes.removed.length) {
+        added = added.concat(...changes.added);
+        removed = removed.concat(...changes.removed);
 
-      return changes.added.length || changes.removed.length
-        ? of(changes)
-        : EMPTY;
+        return of({ added, removed });
+      }
+
+      return EMPTY;
     }),
-    startWith({ added: [], blockHash: '-', removed: [] })
+    startWith({ added, removed })
   );
 }
 
@@ -106,15 +109,14 @@ function _getKeysPaged (api: ApiInterfaceRx, childKey: string): Observable<Stora
 
 function _getAll (api: ApiInterfaceRx, paraId: string | number | ParaId, childKey: string): Observable<DeriveContributions> {
   return _eventTriggerAll(api, paraId).pipe(
-    switchMap((blockHash) => combineLatest([
-      of(blockHash),
-      // FIXME This needs testing and enabling
+    switchMap(() =>
+      // FIXME Needs testing and being enabled
       // eslint-disable-next-line no-constant-condition
       isFunction(api.rpc.childstate.getKeysPaged) && false
         ? _getKeysPaged(api, childKey)
         : api.rpc.childstate.getKeys(childKey, '0x')
-    ])),
-    map(([blockHash, keys]): DeriveContributions => {
+    ),
+    map((keys): DeriveContributions => {
       const contributorsMap: Record<string, boolean> = {};
 
       keys.forEach((k): void => {
@@ -122,32 +124,23 @@ function _getAll (api: ApiInterfaceRx, paraId: string | number | ParaId, childKe
       });
 
       return {
-        blockHash,
-        childKey,
-        contributorsHex: [], // to be filled-in
-        contributorsMap,
-        hasKeys: true
+        contributorsHex: [], // to be filled-in by the result mapping
+        contributorsMap
       };
     })
   );
 }
 
 function _contributions (api: ApiInterfaceRx, paraId: string | number | ParaId, { trieIndex }: FundInfo): Observable<DeriveContributions> {
-  let extraAdded: string[] = [];
-  let extraRemoved: string[] = [];
-
   return combineLatest([
     _getAll(api, paraId, createChildKey(trieIndex)),
     _getUpdates(api, paraId)
   ]).pipe(
-    map(([full, changes]: [DeriveContributions, Changes]): DeriveContributions => {
-      extraAdded = extraAdded.concat(...changes.added);
-      extraRemoved = extraRemoved.concat(...changes.removed);
-
-      extraAdded.forEach((a): void => {
+    map(([full, { added, removed }]: [DeriveContributions, Changes]): DeriveContributions => {
+      added.forEach((a): void => {
         full.contributorsMap[a] = true;
       });
-      extraRemoved.forEach((a): void => {
+      removed.forEach((a): void => {
         delete full.contributorsMap[a];
       });
 
@@ -164,7 +157,7 @@ export function contributions (instanceId: string, api: ApiInterfaceRx): (paraId
       switchMap((optInfo) =>
         optInfo.isSome
           ? _contributions(api, paraId, optInfo.unwrap())
-          : of(EMPTY_RESULT)
+          : of({ contributorsHex: [], contributorsMap: {} })
       )
     )
   );
--- packages/api-derive/src/crowdloan/types.ts
+++ packages/api-derive/src/crowdloan/types.ts
@@ -2,9 +2,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
 export interface DeriveContributions {
-  blockHash: string;
-  childKey: string;
   contributorsHex: string[];
   contributorsMap: Record<string, boolean>;
-  hasKeys: boolean;
 }
