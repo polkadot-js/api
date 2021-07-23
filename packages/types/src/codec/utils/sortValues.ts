15e260ff72c1935aed7dc7fcb3ed018dfb7caa79
Leszek Wiesner <leszek@jsgenesis.com>
2021-07-23 21:04:52 +0200

    Cleanup sorting function

--- packages/types/src/codec/utils/sortValues.ts
+++ packages/types/src/codec/utils/sortValues.ts
@@ -5,28 +5,39 @@ import type { Codec } from '../../types';
 
 import { AbstractArray } from '../AbstractArray';
 import { AbstractInt } from '../AbstractInt';
-import { Enum } from '../Enum';
-import { Struct } from '../Struct';
+import { Enum, Struct } from '..';
+
+type SortArg = Codec | Codec[] | number[] | number | Uint8Array
+
+/** @internal **/
+function isArrayLike (arg: SortArg): arg is AbstractArray<Codec> | Uint8Array | Codec[] | number[] {
+  return arg instanceof AbstractArray || arg instanceof Uint8Array || Array.isArray(arg);
+}
+
+/** @internal **/
+function isCodec (arg: SortArg): arg is Codec {
+  return !!(arg && typeof (arg as Codec).toU8a === 'function');
+}
 
 /**
 * Sort keys/values of BTreeSet/BTreeMap in acending order for encoding compatibility with Rust's BTreeSet/BTreeMap
 * (https://doc.rust-lang.org/stable/std/collections/struct.BTreeSet.html)
 * (https://doc.rust-lang.org/stable/std/collections/struct.BTreeMap.html)
 */
-export function sortAsc<V extends Codec | Codec[] | number[] | number | Uint8Array = Codec> (a: V, b: V): number {
-  if (typeof a === 'number') {
-    return a - (b as typeof a);
-  } else if (a instanceof Struct) {
-    return sortAsc(Array.from(a.values()), Array.from((b as Struct).values()));
-  } else if (a instanceof Enum) {
-    return sortAsc(a.index, (b as Enum).index) || sortAsc(a.value, (b as Enum).value);
-  } else if (a instanceof AbstractArray || a instanceof Uint8Array || Array.isArray(a)) {
+export function sortAsc<V extends SortArg = Codec> (a: V, b: V): number {
+  if (typeof a === 'number' && typeof b === 'number') {
+    return a - b;
+  } else if (a instanceof Struct && b instanceof Struct) {
+    return sortAsc(Array.from(a.values()), Array.from(b.values()));
+  } else if (a instanceof Enum && b instanceof Enum) {
+    return sortAsc(a.index, b.index) || sortAsc(a.value, b.value);
+  } else if (isArrayLike(a) && isArrayLike(b)) {
     // Vec, Tuple, Bytes etc.
-    let sortRes = 0; const lenA = a.length; const lenB = (b as typeof a).length;
+    let sortRes = 0; const lenA = a.length; const lenB = b.length;
     const minLen = Math.min(lenA, lenB);
 
     for (let i = 0; i < minLen; ++i) {
-      sortRes = sortAsc(a[i], (b as typeof a)[i]);
+      sortRes = sortAsc(a[i], b[i]);
 
       if (sortRes !== 0) {
         return sortRes;
@@ -34,11 +45,11 @@ export function sortAsc<V extends Codec | Codec[] | number[] | number | Uint8Arr
     }
 
     return lenA - lenB;
-  } else if (a instanceof AbstractInt) {
-    return a.eq(b) ? 0 : a.lt(b as typeof a) ? -1 : 1;
-  } else if (a && typeof a.toU8a === 'function') {
-    // Fallback (Bytes, Text)
-    return sortAsc(a.toU8a(true), (b as Codec).toU8a(true));
+  } else if (a instanceof AbstractInt && b instanceof AbstractInt) {
+    return a.cmp(b);
+  } else if (isCodec(a) && isCodec(b)) {
+    // Text, Bool etc.
+    return sortAsc(a.toU8a(true), b.toU8a(true));
   } else {
     throw new Error(`Attempting to sort unrecognized value: ${JSON.stringify(a)}`);
   }
