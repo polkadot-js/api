36b217463a51fd43743964c95cb404646ed2fc6e
Jaco <jacogr@gmail.com>
2021-07-29 14:00:05 +0300

    (some) Bridges types (#3823)

--- packages/types/src/interfaces/bridges/types.ts
+++ packages/types/src/interfaces/bridges/types.ts
@@ -1,7 +1,7 @@
 // Auto-generated via `yarn polkadot-types-from-defs`, do not edit
 /* eslint-disable */
 
-import type { Struct, bool } from '@polkadot/types';
+import type { Struct, U8aFixed, bool, u64 } from '@polkadot/types';
 import type { AuthorityList, SetId } from '@polkadot/types/interfaces/grandpa';
 import type { BlockNumber, H256, Header } from '@polkadot/types/interfaces/runtime';
 
@@ -14,6 +14,12 @@ export interface BridgedBlockNumber extends BlockNumber {}
 /** @name BridgedHeader */
 export interface BridgedHeader extends Header {}
 
+/** @name ChainId */
+export interface ChainId extends Id {}
+
+/** @name Id */
+export interface Id extends U8aFixed {}
+
 /** @name InitializationData */
 export interface InitializationData extends Struct {
   readonly header: Header;
@@ -21,5 +27,17 @@ export interface InitializationData extends Struct {
   readonly setId: SetId;
   readonly isHalted: bool;
 }
+
+/** @name LaneId */
+export interface LaneId extends Id {}
+
+/** @name MessageKey */
+export interface MessageKey extends Struct {
+  readonly laneId: LaneId;
+  readonly nonce: MessageNonce;
+}
+
+/** @name MessageNonce */
+export interface MessageNonce extends u64 {}
 
 export type PHANTOM_BRIDGES = 'bridges';
