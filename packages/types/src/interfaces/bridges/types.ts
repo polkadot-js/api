3e67a10be2d9f583b0aaf2d732a0ab561a08f091
Jaco Greeff <jacogr@gmail.com>
2021-08-03 10:21:22 +0300

    Update bridges types

--- packages/types/src/interfaces/bridges/types.ts
+++ packages/types/src/interfaces/bridges/types.ts
@@ -1,9 +1,12 @@
 // Auto-generated via `yarn polkadot-types-from-defs`, do not edit
 /* eslint-disable */
 
-import type { Struct, U8aFixed, bool, u64 } from '@polkadot/types';
+import type { Bytes, Enum, Null, Struct, U8aFixed, Vec, bool, u32, u64, u8 } from '@polkadot/types';
+import type { ITuple } from '@polkadot/types/types';
+import type { BlockHash } from '@polkadot/types/interfaces/chain';
+import type { MultiSignature } from '@polkadot/types/interfaces/extrinsics';
 import type { AuthorityList, SetId } from '@polkadot/types/interfaces/grandpa';
-import type { BlockNumber, H256, Header } from '@polkadot/types/interfaces/runtime';
+import type { AccountId, Balance, BlockNumber, H256, Header, MultiSigner, Weight } from '@polkadot/types/interfaces/runtime';
 
 /** @name BridgedBlockHash */
 export interface BridgedBlockHash extends H256 {}
@@ -14,11 +17,36 @@ export interface BridgedBlockNumber extends BlockNumber {}
 /** @name BridgedHeader */
 export interface BridgedHeader extends Header {}
 
-/** @name ChainId */
-export interface ChainId extends Id {}
+/** @name CallOrigin */
+export interface CallOrigin extends Enum {
+  readonly isSourceRoot: boolean;
+  readonly isTargetAccount: boolean;
+  readonly asTargetAccount: ITuple<[AccountId, MultiSigner, MultiSignature]>;
+  readonly isSourceAccount: boolean;
+  readonly asSourceAccount: AccountId;
+}
 
-/** @name Id */
-export interface Id extends U8aFixed {}
+/** @name DeliveredMessages */
+export interface DeliveredMessages extends Struct {
+  readonly begin: MessageNonce;
+  readonly end: MessageNonce;
+  readonly dispatchResults: u8;
+}
+
+/** @name DispatchFeePayment */
+export interface DispatchFeePayment extends Enum {
+  readonly isAtSourceChain: boolean;
+  readonly isAtTargetChain: boolean;
+}
+
+/** @name InboundLaneData */
+export interface InboundLaneData extends Struct {
+  readonly relayers: Vec<UnrewardedRelayer>;
+  readonly lastConfirmedNonce: MessageNonce;
+}
+
+/** @name InboundRelayer */
+export interface InboundRelayer extends AccountId {}
 
 /** @name InitializationData */
 export interface InitializationData extends Struct {
@@ -29,7 +57,13 @@ export interface InitializationData extends Struct {
 }
 
 /** @name LaneId */
-export interface LaneId extends Id {}
+export interface LaneId extends U8aFixed {}
+
+/** @name MessageData */
+export interface MessageData extends Struct {
+  readonly payload: Bytes;
+  readonly fee: Balance;
+}
 
 /** @name MessageKey */
 export interface MessageKey extends Struct {
@@ -39,5 +73,66 @@ export interface MessageKey extends Struct {
 
 /** @name MessageNonce */
 export interface MessageNonce extends u64 {}
+
+/** @name MessagesDeliveryProofOf */
+export interface MessagesDeliveryProofOf extends Struct {
+  readonly bridgedHeaderHash: BlockHash;
+  readonly storageProof: Vec<Bytes>;
+  readonly lane: LaneId;
+}
+
+/** @name MessagesProofOf */
+export interface MessagesProofOf extends Struct {
+  readonly bridgedHeaderHash: BridgedBlockHash;
+  readonly storageProof: Vec<Bytes>;
+  readonly lane: LaneId;
+  readonly noncesStart: MessageNonce;
+  readonly noncesEnd: MessageNonce;
+}
+
+/** @name OperatingMode */
+export interface OperatingMode extends Enum {
+  readonly isNormal: boolean;
+  readonly isRejectingOutboundMessages: boolean;
+  readonly isHalted: boolean;
+}
+
+/** @name OutboundLaneData */
+export interface OutboundLaneData extends Struct {
+  readonly latestGeneratedNonce: MessageNonce;
+  readonly latestReceivedNonce: MessageNonce;
+  readonly oldestUnprunedNonce: MessageNonce;
+}
+
+/** @name OutboundMessageFee */
+export interface OutboundMessageFee extends Balance {}
+
+/** @name OutboundPayload */
+export interface OutboundPayload extends Struct {
+  readonly specVersion: u32;
+  readonly weight: Weight;
+  readonly origin: CallOrigin;
+  readonly dispatchFeePayment: DispatchFeePayment;
+  readonly call: Bytes;
+}
+
+/** @name Parameter */
+export interface Parameter extends Null {}
+
+/** @name RelayerId */
+export interface RelayerId extends AccountId {}
+
+/** @name UnrewardedRelayer */
+export interface UnrewardedRelayer extends Struct {
+  readonly relayer: RelayerId;
+  readonly messages: DeliveredMessages;
+}
+
+/** @name UnrewardedRelayersState */
+export interface UnrewardedRelayersState extends Struct {
+  readonly unrewardedRelayer_Entries: MessageNonce;
+  readonly messagesInOldestEntry: MessageNonce;
+  readonly totalMessages: MessageNonce;
+}
 
 export type PHANTOM_BRIDGES = 'bridges';
