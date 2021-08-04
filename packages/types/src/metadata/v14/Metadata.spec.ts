224b390dbcf3bba4e3ebe2de399b090f9880cccc
Jaco Greeff <jacogr@gmail.com>
2021-08-04 10:36:08 +0300

    testMeta for v14 (local)

--- packages/types/src/metadata/v14/Metadata.spec.ts
+++ packages/types/src/metadata/v14/Metadata.spec.ts
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import type { MetadataAll } from '../../interfaces/metadata';
+import type { Check } from '../util/types';
 
 import kusamaData from '@polkadot/types-support/metadata/v14/kusama-hex';
 import kusamaJson from '@polkadot/types-support/metadata/v14/kusama-json.json';
@@ -13,44 +14,58 @@ import substrateTypes from '@polkadot/types-support/metadata/v14/substrate-types
 import { TypeRegistry } from '../../create';
 import { Metadata } from '../Metadata';
 
-function runTest (type: string, staticData: string, staticJson: any, staticTypes: any): void {
-  describe(`MetadataV14 (${type})`, (): void => {
-    // FIXME This is to be replaced with the proper version (here as a stop-gap)
-    it('parses', (): void => {
-      const registry = new TypeRegistry();
-      const metadata = new Metadata(registry, staticData);
-      const all = metadata.get('metadata') as MetadataAll;
-      const v14 = all.asV14;
-      const jsonStruct = v14.toJSON();
-      const jsonTypes = v14.lookup.types.toJSON();
+function testMeta (version: number, matchers: Record<string, Check>): void {
+  describe(`MetadataV${version}`, (): void => {
+    describe.each(Object.keys(matchers))('%s', (type): void => {
+      const { compare, data, types } = matchers[type];
 
-      delete (jsonStruct as Record<string, unknown>).lookup;
+      // FIXME This is to be replaced with the proper version (here as a stop-gap)
+      it('parses', (): void => {
+        const registry = new TypeRegistry();
+        const metadata = new Metadata(registry, data);
+        const all = metadata.get('metadata') as MetadataAll;
+        const v14 = all.asV14;
+        const jsonStruct = v14.toJSON();
+        const jsonTypes = v14.lookup.types.toJSON();
 
-      const allJson = {
-        magicNumber: 1635018093,
-        metadata: {
-          v14: jsonStruct
+        delete (jsonStruct as Record<string, unknown>).lookup;
+
+        const allJson = {
+          magicNumber: 1635018093,
+          metadata: {
+            v14: jsonStruct
+          }
+        };
+
+        try {
+          expect(allJson).toEqual(compare);
+        } catch (error) {
+          console.error(JSON.stringify(allJson));
+
+          throw error;
         }
-      };
 
-      try {
-        expect(allJson).toEqual(staticJson);
-      } catch (error) {
-        console.error(JSON.stringify(allJson));
+        try {
+          expect(jsonTypes).toEqual(types);
+        } catch (error) {
+          console.error(JSON.stringify(jsonTypes));
 
-        throw error;
-      }
-
-      try {
-        expect(jsonTypes).toEqual(staticTypes);
-      } catch (error) {
-        console.error(JSON.stringify(jsonTypes));
-
-        throw error;
-      }
+          throw error;
+        }
+      });
     });
   });
 }
 
-runTest('substrate', substrateData, substrateJson, substrateTypes);
-runTest('kusama', kusamaData, kusamaJson, kusamaTypes);
+testMeta(14, {
+  kusama: {
+    compare: kusamaJson as Record<string, unknown>,
+    data: kusamaData,
+    types: kusamaTypes as Record<string, unknown>
+  },
+  substrate: {
+    compare: substrateJson as Record<string, unknown>,
+    data: substrateData,
+    types: substrateTypes as Record<string, unknown>
+  }
+});
