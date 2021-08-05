fc66159ed402440d9b9f6b016de82018538dc731
Jaco Greeff <jacogr@gmail.com>
2021-08-05 09:50:27 +0300

    type as 'OtherType' cast removal

--- packages/types/src/checkTypes.manual.ts
+++ packages/types/src/checkTypes.manual.ts
@@ -4,19 +4,30 @@
 import type { u32 } from './primitive';
 import type { Registry } from './types/registry';
 
+import { assert } from '@polkadot/util';
+
 import { TypeRegistry } from './create';
 
 function registryCheck (registry: Registry): void {
   // Should be AccountId
   const aa = registry.createType('AccountId');
+
+  assert(aa.isAscii, 'All ok');
+
   // Should be Codec
   const bb = registry.createType('Something');
+
+  assert(bb.toHuman(), 'All ok');
+
   // Should be u32
   const cc = registry.createType('Something' as 'u32');
+
+  assert(cc.divn(123), 'All ok');
+
   // Should be u32
   const dd = registry.createType<u32>('Something');
 
-  console.log([aa, bb, cc, dd].map((c) => c.toHuman()));
+  assert(dd.divn(123), 'All ok');
 }
 
 registryCheck(new TypeRegistry());
