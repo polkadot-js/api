2e8e5a93ca55ddf0b010fd52df0c2434abbf0826
Jaco <jacogr@gmail.com>
2021-08-25 12:32:54 +0300

    Rang now correctly specified (#3902)

--- packages/types/src/interfaces/scaleInfo/definitions.ts
+++ packages/types/src/interfaces/scaleInfo/definitions.ts
@@ -40,11 +40,9 @@ export default {
         Sequence: 'SiTypeDefSequence',
         Array: 'SiTypeDefArray',
         Tuple: 'SiTypeDefTuple',
-        // Range: 'SiTypeDefRange',
         Primitive: 'SiTypeDefPrimitive',
         Compact: 'SiTypeDefCompact',
         BitSequence: 'SiTypeDefBitSequence',
-        // FIXME Move to commented-out area as soon as available on Substrate master
         Range: 'SiTypeDefRange',
         // NOTE: This is specific to the implementation for pre-v14 metadata
         // compatibility (always keep this as the last entry in the enum)
