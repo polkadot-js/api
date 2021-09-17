dcb6199f53928927f27a27b723608b8992f7b127
Jaco Greeff <jacogr@gmail.com>
2021-09-17 07:25:54 +0300

    Additional augmentation fixes

--- packages/api/src/augment/index.ts
+++ packages/api/src/augment/index.ts
@@ -1,6 +1,7 @@
 // Copyright 2017-2021 @polkadot/api authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+import '@polkadot/types/augment';
 import './consts';
 import './errors';
 import './events';
