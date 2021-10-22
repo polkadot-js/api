a624e25760fe461871398b4c814975068f44e4c9
Jaco <jacogr@gmail.com>
2021-10-22 09:34:57 +0300

    Ensure rpcCore correctly decorates .json/.raw

--- packages/rpc-core/src/types.jsonrpc.ts
+++ packages/rpc-core/src/types.jsonrpc.ts
@@ -3,9 +3,14 @@
 
 // FIXME, this whole file needs to move to API
 
+import type { Observable } from 'rxjs';
+import type { Json, Raw } from '@polkadot/types';
 import type { AnyFunction } from '@polkadot/types/types';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface RpcInterface {}
 
-export type AugmentedRpc<F extends AnyFunction> = F;
+export type AugmentedRpc<F extends AnyFunction> = F & {
+  json: (...params: Parameters<F>) => Observable<Json>;
+  raw: (...params: Parameters<F>) => Observable<Raw>;
+};
