66215890dd26d11e64caf239ff7265d50eb7baa0
Jaco <jacogr@gmail.com>
2021-10-26 12:20:54 +0200

    Correct construction of palletVersion keys (#4115)

--- packages/types/src/metadata/decorate/storage/substrate.ts
+++ packages/types/src/metadata/decorate/storage/substrate.ts
@@ -3,29 +3,34 @@
 
 import type { StorageEntry } from '../../../primitive/types';
 import type { Registry } from '../../../types';
+import type { ManualMetadata } from './util';
 
 import { createRuntimeFunction } from './util';
 
 type Creator = (registry: Registry) => StorageEntry;
 
+function createSubstrateFn (method: string, key: string, meta: ManualMetadata): Creator {
+  return createRuntimeFunction({ method, prefix: 'Substrate', section: 'substrate' }, key, meta);
+}
+
 export const substrate: Record<string, Creator> = {
-  changesTrieConfig: createRuntimeFunction('Substrate', 'changesTrieConfig', ':changes_trie', {
+  changesTrieConfig: createSubstrateFn('changesTrieConfig', ':changes_trie', {
     docs: ' Changes trie configuration is stored under this key.',
     type: 'u32'
   }),
-  childStorageKeyPrefix: createRuntimeFunction('Substrate', 'childStorageKeyPrefix', ':child_storage:', {
+  childStorageKeyPrefix: createSubstrateFn('childStorageKeyPrefix', ':child_storage:', {
     docs: ' Prefix of child storage keys.',
     type: 'u32'
   }),
-  code: createRuntimeFunction('Substrate', 'code', ':code', {
+  code: createSubstrateFn('code', ':code', {
     docs: ' Wasm code of the runtime.',
     type: 'Bytes'
   }),
-  extrinsicIndex: createRuntimeFunction('Substrate', 'extrinsicIndex', ':extrinsic_index', {
+  extrinsicIndex: createSubstrateFn('extrinsicIndex', ':extrinsic_index', {
     docs: ' Current extrinsic index (u32) is stored under this key.',
     type: 'u32'
   }),
-  heapPages: createRuntimeFunction('Substrate', 'heapPages', ':heappages', {
+  heapPages: createSubstrateFn('heapPages', ':heappages', {
     docs: ' Number of wasm linear memory pages required for execution of the runtime.',
     type: 'u64'
   })
