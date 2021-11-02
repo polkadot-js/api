49d5a33b96e250b6a280207a5e56fbe808145f04
Jaco <jacogr@gmail.com>
2021-11-02 11:13:01 +0200

    Lazy decoration (part 5, augmentObject)

--- packages/api/src/util/augmentObject.ts
+++ packages/api/src/util/augmentObject.ts
@@ -1,11 +1,20 @@
 // Copyright 2017-2021 @polkadot/api authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+import { lazyMethod } from '@polkadot/types';
 import { logger } from '@polkadot/util';
 
 type StringsStrings = [string[], string[]];
 
 const l = logger('api/augment');
+
+function clearObject (obj: Record<string, unknown>): void {
+  const keys = Object.keys(obj);
+
+  for (let i = 0; i < keys.length; i++) {
+    delete obj[keys[i]];
+  }
+}
 
 function logLength (type: 'added' | 'removed', values: string[], and: string[] = []): string {
   return values.length
@@ -34,7 +43,7 @@ function findSectionIncludes (a: string[], b: string[]): string[] {
   return a.filter((s) => b.includes(s));
 }
 
-function extractSections (src: Record<string, Record<string, any>>, dst: Record<string, Record<string, any>>): StringsStrings {
+function extractSections (src: Record<string, Record<string, unknown>>, dst: Record<string, Record<string, unknown>>): StringsStrings {
   const srcSections = Object.keys(src);
   const dstSections = Object.keys(dst);
 
@@ -44,7 +53,7 @@ function extractSections (src: Record<string, Record<string, any>>, dst: Record<
   ];
 }
 
-function findMethodExcludes (src: Record<string, Record<string, any>>, dst: Record<string, Record<string, any>>): string[] {
+function findMethodExcludes (src: Record<string, Record<string, unknown>>, dst: Record<string, Record<string, unknown>>): string[] {
   const srcSections = Object.keys(src);
   const dstSections = findSectionIncludes(Object.keys(dst), srcSections);
   const excludes: string[] = [];
@@ -64,7 +73,7 @@ function findMethodExcludes (src: Record<string, Record<string, any>>, dst: Reco
   return excludes;
 }
 
-function extractMethods (src: Record<string, Record<string, any>>, dst: Record<string, Record<string, any>>): StringsStrings {
+function extractMethods (src: Record<string, Record<string, unknown>>, dst: Record<string, Record<string, unknown>>): StringsStrings {
   return [
     findMethodExcludes(dst, src),
     findMethodExcludes(src, dst)
@@ -77,14 +86,35 @@ function extractMethods (src: Record<string, Record<string, any>>, dst: Record<s
  * @internal
  */
 export function augmentObject (prefix: string | null, src: Record<string, Record<string, unknown>>, dst: Record<string, Record<string, unknown>>, fromEmpty = false): Record<string, Record<string, any>> {
-  if (fromEmpty) {
-    const dstKeys = Object.keys(dst);
-
-    for (let k = 0; k < dstKeys.length; k++) {
-      delete dst[dstKeys[k]];
+  const lazySection = (section: string): void => {
+    // NOTE: Unlike the other lazy decorations, here we actually do decorate the
+    // sections, just the internal form is not available (i.e. we skip lazyMethods)
+    // The initial issue is that we have a confict between "this already exists" and
+    // "this needs to start clean", so for a first round we skip...
+    if (!dst[section]) {
+      dst[section] = {};
     }
-  }
 
+    const creator = (method: string) => src[section][method];
+    const methods = Object.keys(src[section]);
+
+    for (let m = 0; m < methods.length; m++) {
+      const method = methods[m];
+
+      // Although not recommended, use use the hasOwnPreoperty to not actually
+      // get the value from the object itself (when lazy, this calls)
+      // eslint-disable-next-line no-prototype-builtins
+      if (!dst[section].hasOwnProperty(method)) {
+        lazyMethod(dst[section], method, creator);
+      }
+    }
+  };
+
+  fromEmpty && clearObject(dst);
+
+  // NOTE: This part is slightly problematic since it will get the
+  // values for at least all the sections and the names of the methods
+  // (Since methods won't be decorated before lazy, this _may_ be ok)
   if (prefix && Object.keys(dst).length) {
     warn(prefix, 'modules', extractSections(src, dst));
     warn(prefix, 'calls', extractMethods(src, dst));
@@ -92,23 +122,8 @@ export function augmentObject (prefix: string | null, src: Record<string, Record
 
   const srcKeys = Object.keys(src);
 
-  for (let s = 0; s < srcKeys.length; s++) {
-    const section = srcKeys[s];
-
-    if (!dst[section]) {
-      dst[section] = {};
-    }
-
-    const methods = Object.keys(src[section]);
-
-    for (let m = 0; m < methods.length; m++) {
-      const method = methods[m];
-
-      // TODO When it does match, check the actual details and warn when there are differences
-      if (!dst[section][method]) {
-        dst[section][method] = src[section][method];
-      }
-    }
+  for (let i = 0; i < srcKeys.length; i++) {
+    lazySection(srcKeys[i]);
   }
 
   return dst;
