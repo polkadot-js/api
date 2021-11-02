56be2643cae45f2ec6c6a2373afaf5bbfa576e46
Jaco <jacogr@gmail.com>
2021-11-02 07:24:44 +0200

    lazy from @polkadot/types

--- packages/api-derive/src/util/lazy.ts
+++ packages/api-derive/src/util/lazy.ts
@@ -1,38 +1,11 @@
 // Copyright 2017-2021 @polkadot/api-derive authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+import { lazyMethod, lazyMethods } from '@polkadot/types';
+
 type LazySection <T> = Record<string, T>;
 
 type LazyRecord <T> = Record<string, LazySection<T>>;
-
-function getStringName <K> (data: K): string {
-  return (data as unknown as string).toString();
-}
-
-function lazyMethod <T, K> (result: Record<string, T>, item: K, creator: (d: K) => T, getName: (d: K) => string = getStringName): void {
-  let cached: T | null = null;
-
-  Object.defineProperty(result, getName(item), {
-    enumerable: true,
-    get: (): T => {
-      if (!cached) {
-        cached = creator(item);
-      }
-
-      return cached;
-    }
-  });
-}
-
-function lazyMethods <T, K> (items: K[], creator: (v: K) => T, getName: (m: K) => string = getStringName): Record<string, T> {
-  const result: Record<string, T> = {};
-
-  for (let i = 0; i < items.length; i++) {
-    lazyMethod(result, items[i], creator, getName);
-  }
-
-  return result;
-}
 
 export function lazyDeriveSection <T> (result: LazyRecord<T>, section: string, getKeys: (s: string) => string[], creator: (s: string, m: string) => T): void {
   lazyMethod(result, section, (s) => lazyMethods(getKeys(s), (m) => creator(s, m)));
