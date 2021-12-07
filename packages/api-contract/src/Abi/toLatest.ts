4be37877b852f4197646eee1a7eddf2b4b0fcc2f
Jaco <jacogr@gmail.com>
2021-12-07 08:18:29 +0200

    spreads

--- packages/api-contract/src/Abi/toLatest.ts
+++ packages/api-contract/src/Abi/toLatest.ts
@@ -4,44 +4,41 @@
 import type { ContractMessageParamSpecLatest, ContractMessageParamSpecV0, ContractMetadataLatest, ContractMetadataV1 } from '@polkadot/types/interfaces';
 import type { Registry } from '@polkadot/types/types';
 
+import { objectSpread } from '@polkadot/util';
+
 function convertArgs (registry: Registry, args: ContractMessageParamSpecV0[]): ContractMessageParamSpecLatest[] {
   return args.map((a) =>
-    registry.createType('ContractMessageParamSpecLatest', {
-      ...a,
+    registry.createType('ContractMessageParamSpecLatest', objectSpread({}, a, {
       label: a.name
-    })
+    }))
   );
 }
 
 export function toLatest (registry: Registry, v1: ContractMetadataV1): ContractMetadataLatest {
-  return registry.createType('ContractMetadataLatest', {
-    ...v1,
-    spec: {
+  return registry.createType('ContractMetadataLatest', objectSpread({}, v1, {
+    spec: objectSpread({}, v1.spec, {
       constructors: v1.spec.constructors.map((c) =>
-        registry.createType('ContractConstructorSpecLatest', {
-          ...c,
+        registry.createType('ContractConstructorSpecLatest', objectSpread({}, c, {
           args: convertArgs(registry, c.args),
           label: c.name
-        })
+        }))
       ),
-      docs: v1.spec.docs,
       events: v1.spec.events.map((e) =>
-        registry.createType('ContractEventSpecLatest', {
-          ...e,
-          args: e.args.map((a) => registry.createType('ContractEventParamSpecLatest', {
-            ...a,
-            label: a.name
-          })),
+        registry.createType('ContractEventSpecLatest', objectSpread({}, e, {
+          args: e.args.map((a) =>
+            registry.createType('ContractEventParamSpecLatest', objectSpread({}, a, {
+              label: a.name
+            }))
+          ),
           label: e.name
-        })
+        }))
       ),
       messages: v1.spec.messages.map((c) =>
-        registry.createType('ContractMessageSpecLatest', {
-          ...c,
+        registry.createType('ContractMessageSpecLatest', objectSpread({}, c, {
           args: convertArgs(registry, c.args),
           label: c.name
-        })
+        }))
       )
-    }
-  });
+    })
+  }));
 }
--- packages/api-contract/src/Abi/toV1.ts
+++ packages/api-contract/src/Abi/toV1.ts
@@ -5,10 +5,10 @@ import type { ContractMetadataV0, ContractMetadataV1 } from '@polkadot/types/int
 import type { Registry } from '@polkadot/types/types';
 
 import { convertSiV0toV1 } from '@polkadot/types';
+import { objectSpread } from '@polkadot/util';
 
 export function toV1 (registry: Registry, v0: ContractMetadataV0): ContractMetadataV1 {
-  return registry.createType('ContractMetadataV1', {
-    ...v0,
+  return registry.createType('ContractMetadataV1', objectSpread({}, v0, {
     types: convertSiV0toV1(registry, v0.types)
-  });
+  }));
 }
