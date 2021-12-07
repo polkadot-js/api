dd25b76a7e22329ab27a1bc207626eefe46a6a93
Jaco <jacogr@gmail.com>
2021-12-07 13:06:57 +0200

    Save

--- packages/api-contract/test/contracts/index.ts
+++ packages/api-contract/test/contracts/index.ts
@@ -5,9 +5,9 @@ import ink from './ink';
 import solang from './solang';
 import user from './user';
 
-const all: Record<string, any> = {};
+const all: Record<string, Record<string, unknown>> = {};
 
-function addExport (type: string, abis: Record<string, any>): void {
+function addExport (type: string, abis: Record<string, Record<string, unknown>>): void {
   Object.keys(abis).forEach((key): void => {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     all[`${type}_${key}`] = abis[key];
