b5dd4041dca0da4d17b5553ae5b9531426e48d91
Jaco <jacogr@gmail.com>
2021-12-08 17:53:15 +0200

    Members

--- packages/api-derive/src/council/index.ts
+++ packages/api-derive/src/council/index.ts
@@ -1,11 +1,12 @@
 // Copyright 2017-2021 @polkadot/api-derive authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { hasProposals as collectiveHasProposals, prime as collectivePrime, proposal as collectiveProposal, proposalCount as collectiveProposalCount, proposalHashes as collectiveProposalHashes, proposals as collectiveProposals } from '../collective';
+import { hasProposals as collectiveHasProposals, members as collectiveMembers, prime as collectivePrime, proposal as collectiveProposal, proposalCount as collectiveProposalCount, proposalHashes as collectiveProposalHashes, proposals as collectiveProposals } from '../collective';
 
-export * from './members';
 export * from './votes';
 export * from './votesOf';
+
+export const members = collectiveMembers('council');
 
 export const hasProposals = collectiveHasProposals('council');
 export const proposal = collectiveProposal('council');
--- packages/api-derive/src/membership/index.ts
+++ packages/api-derive/src/membership/index.ts
@@ -1,9 +1,9 @@
 // Copyright 2017-2021 @polkadot/api-derive authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { hasProposals as collectiveHasProposals, prime as collectivePrime, proposal as collectiveProposal, proposalCount as collectiveProposalCount, proposalHashes as collectiveProposalHashes, proposals as collectiveProposals } from '../collective';
+import { hasProposals as collectiveHasProposals, members as collectiveMembers, prime as collectivePrime, proposal as collectiveProposal, proposalCount as collectiveProposalCount, proposalHashes as collectiveProposalHashes, proposals as collectiveProposals } from '../collective';
 
-export * from './members';
+export const members = collectiveMembers('membership');
 
 export const hasProposals = collectiveHasProposals('membership');
 export const proposal = collectiveProposal('membership');
