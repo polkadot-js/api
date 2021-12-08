ee5563941b530043def1385428e76c9f1fc74935
Jaco <jacogr@gmail.com>
2021-12-08 18:14:24 +0200

    Type imports (unused)

--- packages/api-derive/src/technicalCommittee/index.ts
+++ packages/api-derive/src/technicalCommittee/index.ts
@@ -1,6 +1,16 @@
 // Copyright 2017-2021 @polkadot/api-derive authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+// we need these type imports to be available
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import type { ApiInterfaceRx } from '@polkadot/api/types';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import type { AccountId } from '@polkadot/types/interfaces';
+
 import { hasProposals as collectiveHasProposals, members as collectiveMembers, prime as collectivePrime, proposal as collectiveProposal, proposalCount as collectiveProposalCount, proposalHashes as collectiveProposalHashes, proposals as collectiveProposals } from '../collective';
 
 export const members = collectiveMembers('technicalCommittee');
