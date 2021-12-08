fc826872b3aa468536f36d90bcb9e577e54dcf8c
Jaco <jacogr@gmail.com>
2021-12-08 08:32:38 +0200

    Adjust

--- packages/api-contract/src/Abi/toV2.ts
+++ packages/api-contract/src/Abi/toV2.ts
@@ -2,43 +2,35 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import type { Text } from '@polkadot/types';
-import type { ContractMessageParamSpecLatest, ContractMessageParamSpecV0, ContractMetadataV1, ContractMetadataV2 } from '@polkadot/types/interfaces';
+import type { ContractConstructorSpecV2, ContractEventParamSpecV0, ContractEventSpecV2, ContractMessageParamSpecV0, ContractMessageSpecV2, ContractMetadataV1, ContractMetadataV2 } from '@polkadot/types/interfaces';
 import type { Registry } from '@polkadot/types/types';
 
 import { objectSpread } from '@polkadot/util';
 
-function v1Label (orig: { name: Text | Text[] }): { label: Text } {
-  return objectSpread({}, orig, {
-    label: Array.isArray(orig.name)
-      ? orig.name[0]
-      : orig.name
+function v1Label (entry: { name: Text | Text[] }): { label: Text } {
+  return objectSpread({}, entry, {
+    label: Array.isArray(entry.name)
+      ? entry.name[0]
+      : entry.name
   });
 }
 
-function v1LabelArgs (registry: Registry, orig: { args: ContractMessageParamSpecV0[], name: Text | Text[] }): { args: ContractMessageParamSpecLatest[], label: Text } {
-  return objectSpread(v1Label(orig), {
-    args: orig.args.map((a) =>
-      registry.createType('ContractMessageParamSpecV2', v1Label(a))
-    )
-  });
+function v1Labels (registry: Registry, outType: 'ContractConstructorSpecV2' | 'ContractEventSpecV2' | 'ContractMessageSpecV2', argType: 'ContractEventParamSpecV2' | 'ContractMessageParamSpecV2', all: { args: (ContractEventParamSpecV0 | ContractMessageParamSpecV0)[], name: Text | Text[] }[]): (ContractConstructorSpecV2 | ContractEventSpecV2 | ContractMessageSpecV2)[] {
+  return all.map((e) =>
+    registry.createType(outType, objectSpread(v1Label(e), {
+      args: e.args.map((a) =>
+        registry.createType(argType, v1Label(a))
+      )
+    }))
+  );
 }
 
 export function v1ToV2 (registry: Registry, v1: ContractMetadataV1): ContractMetadataV2 {
   return registry.createType('ContractMetadataV2', objectSpread({}, v1, {
     spec: objectSpread({}, v1.spec, {
-      constructors: v1.spec.constructors.map((c) =>
-        registry.createType('ContractConstructorSpecV2', v1LabelArgs(registry, c))
-      ),
-      events: v1.spec.events.map((e) =>
-        registry.createType('ContractEventSpecV2', objectSpread(v1Label(e), {
-          args: e.args.map((a) =>
-            registry.createType('ContractEventParamSpecV2', v1Label(a))
-          )
-        }))
-      ),
-      messages: v1.spec.messages.map((c) =>
-        registry.createType('ContractMessageSpecV2', v1LabelArgs(registry, c))
-      )
+      constructors: v1Labels(registry, 'ContractConstructorSpecV2', 'ContractMessageParamSpecV2', v1.spec.constructors),
+      events: v1Labels(registry, 'ContractEventSpecV2', 'ContractEventParamSpecV2', v1.spec.events),
+      messages: v1Labels(registry, 'ContractMessageSpecV2', 'ContractMessageParamSpecV2', v1.spec.messages)
     })
   }));
 }
