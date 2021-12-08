05084a4ad31d09ce397047a5ce01edd838c72d04
Jaco <jacogr@gmail.com>
2021-12-08 18:26:48 +0200

    Adjust with only local imports

--- packages/api-derive/src/technicalCommittee/index.ts
+++ packages/api-derive/src/technicalCommittee/index.ts
@@ -6,9 +6,6 @@
 // we need these type imports to be available
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-import type { Observable } from 'rxjs';
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
 import type { ApiInterfaceRx } from '@polkadot/api/types';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
@@ -16,9 +13,6 @@ import type { u32 } from '@polkadot/types';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import type { AccountId, Hash } from '@polkadot/types/interfaces';
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import type { DeriveCollectiveProposal } from '../types';
 
 import { hasProposals as collectiveHasProposals, members as collectiveMembers, prime as collectivePrime, proposal as collectiveProposal, proposalCount as collectiveProposalCount, proposalHashes as collectiveProposalHashes, proposals as collectiveProposals } from '../collective';
 
