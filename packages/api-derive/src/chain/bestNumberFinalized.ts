d8f379ae1437cfa248c96c3012a100613c63e11a
Jaco <jacogr@gmail.com>
2021-12-11 09:16:32 +0200

    Unused imports

--- packages/api-derive/src/chain/bestNumberFinalized.ts
+++ packages/api-derive/src/chain/bestNumberFinalized.ts
@@ -1,6 +1,16 @@
 // Copyright 2017-2021 @polkadot/api-derive authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+// We require these imports for the build mapping
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import type { ApiInterfaceRx } from '@polkadot/api/types';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import type { BlockNumber } from '@polkadot/types/interfaces';
+
 import { unwrapBlockNumber } from './util';
 
 /**
