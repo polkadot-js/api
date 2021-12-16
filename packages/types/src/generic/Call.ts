dde681379ee0d3a3fd486930ccb41a5dd4093b0e
Jaco <jacogr@gmail.com>
2021-12-16 07:22:19 +0200

    Adjust

--- packages/types/src/generic/Call.ts
+++ packages/types/src/generic/Call.ts
@@ -144,7 +144,7 @@ export class GenericCall<A extends AnyTuple = AnyTuple> extends Struct implement
    * @description The arguments for the function call
    */
   public get args (): A {
-    return [...this.getTyped<Struct>('args').values()] as A;
+    return [...this.getT<Struct>('args').values()] as A;
   }
 
   /**
@@ -158,21 +158,21 @@ export class GenericCall<A extends AnyTuple = AnyTuple> extends Struct implement
    * @description The argument entries
    */
   public get argsEntries (): [string, Codec][] {
-    return [...this.getTyped<Struct>('args').entries()];
+    return [...this.getT<Struct>('args').entries()];
   }
 
   /**
    * @description The encoded `[sectionIndex, methodIndex]` identifier
    */
   public get callIndex (): Uint8Array {
-    return this.getTyped<GenericCallIndex>('callIndex').toU8a();
+    return this.getT<GenericCallIndex>('callIndex').toU8a();
   }
 
   /**
    * @description The encoded data
    */
   public get data (): Uint8Array {
-    return this.getTyped<Struct>('args').toU8a();
+    return this.getT<Struct>('args').toU8a();
   }
 
   /**
