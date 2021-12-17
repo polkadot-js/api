3fea66a8fb594be90c728bf6daa6aceabb7d796b
Jaco <jacogr@gmail.com>
2021-12-17 06:32:47 +0200

    Move api-derive to correct location

--- packages/api-derive/src/detectPackage.ts
+++ packages/api-derive/src/detectPackage.ts
@@ -1,7 +1,6 @@
 // Copyright 2017-2021 @polkadot/api-derive authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { packageInfo as apiInfo } from '@polkadot/api/packageInfo';
 import { detectPackage } from '@polkadot/util';
 
 import { packageInfo } from './packageInfo';
--- packages/api/src/detectPackage.ts
+++ packages/api/src/detectPackage.ts
@@ -1,6 +1,7 @@
 // Copyright 2017-2021 @polkadot/api authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+import { packageInfo as deriveInfo } from '@polkadot/api-derive/packageInfo';
 import { packageInfo as coreInfo } from '@polkadot/rpc-core/packageInfo';
 import { packageInfo as providerInfo } from '@polkadot/rpc-provider/packageInfo';
 import { packageInfo as typesInfo } from '@polkadot/types/packageInfo';
@@ -11,4 +12,4 @@ import { detectPackage } from '@polkadot/util';
 
 import { packageInfo } from './packageInfo';
 
-detectPackage(packageInfo, typeof __dirname !== 'undefined' && __dirname, [coreInfo, providerInfo, typesInfo, codecInfo, createInfo, knownInfo]);
+detectPackage(packageInfo, typeof __dirname !== 'undefined' && __dirname, [deriveInfo, coreInfo, providerInfo, typesInfo, codecInfo, createInfo, knownInfo]);
