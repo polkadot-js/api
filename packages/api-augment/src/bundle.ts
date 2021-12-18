c1499c9ea77f5bb175dad820f43849c990b15a51
Jaco <jacogr@gmail.com>
2021-12-18 08:35:16 +0200

    Augmentation tweaks

--- packages/api-augment/src/bundle.ts
+++ packages/api-augment/src/bundle.ts
@@ -1,14 +1,14 @@
 // Copyright 2017-2021 @polkadot/api-augment authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import './augment';
+// trigger in the same way as we do externally
+import '@polkadot/api-augment/augment';
 
 export { packageInfo } from './packageInfo';
 
-export * from './consts';
-export * from './errors';
-export * from './events';
-export * from './query';
-export * from './tx';
-
-export * from './rpc';
+// Important: we do the exports here via the full package name
+export * from '@polkadot/api-augment/consts';
+export * from '@polkadot/api-augment/errors';
+export * from '@polkadot/api-augment/events';
+export * from '@polkadot/api-augment/query';
+export * from '@polkadot/api-augment/tx';
--- packages/api-augment/src/tx.ts
+++
@@ -1,6 +0,0 @@
-// Copyright 2017-2021 @polkadot/api-augment authors & contributors
-// SPDX-License-Identifier: Apache-2.0
-
-import './augment/tx';
-
-// intentionally kept empty
