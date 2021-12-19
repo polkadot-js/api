c3a785ab44ad386735ae980c82d5c3a0232b725f
Jaco <jacogr@gmail.com>
2021-12-19 23:51:11 +0200

    tsconfig path linting

--- packages/api/src/detectPackage.ts
+++ packages/api/src/detectPackage.ts
@@ -5,11 +5,9 @@ import { packageInfo as deriveInfo } from '@polkadot/api-derive/packageInfo';
 import { packageInfo as coreInfo } from '@polkadot/rpc-core/packageInfo';
 import { packageInfo as providerInfo } from '@polkadot/rpc-provider/packageInfo';
 import { packageInfo as typesInfo } from '@polkadot/types/packageInfo';
-import { packageInfo as codecInfo } from '@polkadot/types-codec/packageInfo';
-import { packageInfo as createInfo } from '@polkadot/types-create/packageInfo';
 import { packageInfo as knownInfo } from '@polkadot/types-known/packageInfo';
 import { detectPackage } from '@polkadot/util';
 
 import { packageInfo } from './packageInfo';
 
-detectPackage(packageInfo, typeof __dirname !== 'undefined' && __dirname, [deriveInfo, coreInfo, providerInfo, typesInfo, codecInfo, createInfo, knownInfo]);
+detectPackage(packageInfo, typeof __dirname !== 'undefined' && __dirname, [deriveInfo, coreInfo, providerInfo, typesInfo, knownInfo]);
