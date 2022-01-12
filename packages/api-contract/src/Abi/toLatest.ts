49c01c8081f54000437a14434d12edf9408ae3e1
Jaco <jacogr@gmail.com>
2022-01-12 08:17:49 +0200

    Generic converter

--- packages/api-contract/src/Abi/toLatest.ts
+++ packages/api-contract/src/Abi/toLatest.ts
@@ -1,7 +1,7 @@
 // Copyright 2017-2022 @polkadot/api-contract authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import type { ContractMetadataLatest, ContractMetadataV0, ContractMetadataV1, ContractMetadataV2, ContractMetadataV3 } from '@polkadot/types/interfaces';
+import type { ContractMetadataLatest, ContractMetadataV3 } from '@polkadot/types/interfaces';
 import type { Registry } from '@polkadot/types/types';
 
 import { v0ToV1 } from './toV1';
@@ -11,6 +11,19 @@ import { v2ToV3 } from './toV3';
 type Versions = 'V0' | 'V1' | 'V2' | 'V3';
 
 type Converter = (registry: Registry, vx: any) => ContractMetadataLatest;
+
+function createConverter <I, O> (next: (registry: Registry, input: O) => ContractMetadataLatest, step: (registry: Registry, input: I) => O): (registry: Registry, input: I) => ContractMetadataLatest {
+  return (registry: Registry, input: I): ContractMetadataLatest =>
+    next(registry, step(registry, input));
+}
+
+export function v3ToLatest (registry: Registry, v3: ContractMetadataV3): ContractMetadataLatest {
+  return v3;
+}
+
+export const v2ToLatest = createConverter(v3ToLatest, v2ToV3);
+export const v1ToLatest = createConverter(v2ToLatest, v1ToV2);
+export const v0ToLatest = createConverter(v1ToLatest, v0ToV1);
 
 // The versions where an enum is used, aka V0 is missing
 // (Order from newest, i.e. we expect more on newest vs oldest)
@@ -23,19 +36,3 @@ export const convertVersions: [Versions, Converter][] = [
   ['V1', v1ToLatest],
   ['V0', v0ToLatest]
 ];
-
-export function v0ToLatest (registry: Registry, v0: ContractMetadataV0): ContractMetadataLatest {
-  return v1ToLatest(registry, v0ToV1(registry, v0));
-}
-
-export function v1ToLatest (registry: Registry, v1: ContractMetadataV1): ContractMetadataLatest {
-  return v2ToLatest(registry, v1ToV2(registry, v1));
-}
-
-export function v2ToLatest (registry: Registry, v2: ContractMetadataV2): ContractMetadataLatest {
-  return v3ToLatest(registry, v2ToV3(registry, v2));
-}
-
-export function v3ToLatest (registry: Registry, v3: ContractMetadataV3): ContractMetadataLatest {
-  return v3;
-}
