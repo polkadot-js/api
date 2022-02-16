a1ca57ba11cbd1f6934cc87341c7298d397a86ad
Jaco <jacogr@gmail.com>
2022-02-16 13:46:37 +0200

    Adjust

--- packages/types-codec/src/types/registry.ts
+++ packages/types-codec/src/types/registry.ts
@@ -60,7 +60,7 @@ export interface Registry {
   createdAtHash?: IU8a;
 
   findMetaCall (callIndex: Uint8Array): CallFunction;
-  findMetaError (errorIndex: Uint8Array | { error: BN, index: BN }): RegistryError;
+  findMetaError (errorIndex: Uint8Array | { error: BN, index: BN | Uint8Array }): RegistryError;
   findMetaEvent (eventIndex: Uint8Array): CodecClass<any>;
 
   isLookupType (value: string): boolean;
