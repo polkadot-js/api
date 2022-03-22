9399c8d24f6d5f0bc8430ff7248f49e9590a5b92
Jaco <jacogr@gmail.com>
2022-03-22 08:41:17 +0100

    Output BitVec toHuman to LSB (limit support) (#4676)

--- packages/types-codec/src/extended/BitVec.spec.ts
+++ packages/types-codec/src/extended/BitVec.spec.ts
@@ -11,21 +11,38 @@ const TESTS = ['0x00', '0x0817', '0x0837', '0x087b', '0x0c33'];
 const registry = new TypeRegistry();
 
 describe('BitVec', (): void => {
-  TESTS.forEach((test): void => {
-    describe(test, (): void => {
-      const input = hexToU8a(test);
+  describe('decoding known', (): void => {
+    TESTS.forEach((test): void => {
+      describe(test, (): void => {
+        const input = hexToU8a(test);
+        const bitvec = new BitVec(registry, input);
 
-      it('has the right encodedLength', (): void => {
-        expect(
-          new BitVec(registry, input).encodedLength
-        ).toEqual((test.length - 2) / 2);
-      });
+        it('has the right encodedLength', (): void => {
+          expect(
+            bitvec.encodedLength
+          ).toEqual((test.length - 2) / 2);
+        });
 
-      it('re-encodes to the same input value', (): void => {
-        expect(
-          new BitVec(registry, input).toU8a()
-        ).toEqual(input);
+        it('re-encodes to the same input value', (): void => {
+          expect(
+            bitvec.toU8a()
+          ).toEqual(input);
+        });
       });
+    });
+  });
+
+  describe('toHuman() ordering', (): void => {
+    it('defaults to Lsb', (): void => {
+      expect(
+        new BitVec(registry, '0x0100010500').toHuman()
+      ).toEqual('0b10000000_00000000_10000000_10100000_00000000');
+    });
+
+    it('can output to Msb', (): void => {
+      expect(
+        new BitVec(registry, '0x0100010500', true).toHuman()
+      ).toEqual('0b00000001_00000000_00000001_00000101_00000000');
     });
   });
 
