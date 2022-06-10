6672fc79a82adb89d534f2bc275be6ce4e506467
Jaco <jacogr@gmail.com>
2022-06-10 07:25:23 +0300

    Adjust expansion

--- packages/types/src/types/events.ts
+++ packages/types/src/types/events.ts
@@ -1,33 +1,33 @@
 // Copyright 2017-2022 @polkadot/types authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import type { AnyTuple, Codec, ITuple } from '@polkadot/types-codec/types';
+import type { AnyTuple, Codec } from '@polkadot/types-codec/types';
 import type { TypeDef } from '@polkadot/types-create/types';
 import type { EventMetadataLatest } from '../interfaces/metadata';
 import type { Hash } from '../interfaces/runtime';
 import type { EventId, Phase } from '../interfaces/system';
 
-export interface IEventRecord<T extends Codec[]> {
+export interface IEventRecord<T extends Codec[]> extends Codec {
   readonly phase: Phase;
   readonly event: IEvent<T>;
   readonly topics: Hash[];
 }
 
-export interface IEventData {
+export interface IEventData extends Codec {
   readonly meta: EventMetadataLatest;
   readonly method: string;
   readonly section: string;
   readonly typeDef: TypeDef[];
 }
 
-export interface IEventLike {
+export interface IEventLike extends Codec {
   readonly index: unknown;
   readonly method: unknown;
   readonly section: unknown;
 }
 
-export interface IEvent<T extends AnyTuple> extends IEventLike {
-  readonly data: ITuple<T> & IEventData;
+export interface IEvent<T extends AnyTuple> extends IEventLike, Codec {
+  readonly data: T & IEventData;
   readonly index: EventId;
   readonly method: string;
   readonly section: string;
