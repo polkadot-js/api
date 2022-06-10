47bcef87b7b107165a2ce512eb9f380bddf82efe
Jaco <jacogr@gmail.com>
2022-06-10 07:18:25 +0300

    Remove unused code

--- packages/types/src/types/events.ts
+++ packages/types/src/types/events.ts
@@ -7,49 +7,6 @@ import type { EventMetadataLatest } from '../interfaces/metadata';
 import type { Hash } from '../interfaces/runtime';
 import type { EventId, Phase } from '../interfaces/system';
 
-// // https://catchts.com/union-array
-// // https://stackoverflow.com/questions/50374908/transform-union-type-to-intersection-type/50375286
-// type UnionToIntersection<U> =
-//   (U extends any ? (k: U) => void : never) extends (k: infer I) => void
-//     ? I
-//     : never;
-
-// // Converts union to overloaded function
-// type UnionToOvlds<U> = UnionToIntersection<
-// U extends any
-//   ? (f: U) => void
-//   : never
-// >;
-
-// type PopUnion<U> =
-//   UnionToOvlds<U> extends (a: infer A) => void
-//     ? A
-//     : never;
-
-// type IsUnion<T> =
-//   [T] extends [UnionToIntersection<T>]
-//     ? false
-//     : true;
-
-// type UnionToArray<T, A extends unknown[] = []> =
-//   IsUnion<T> extends true
-//     ? UnionToArray<Exclude<T, PopUnion<T>>, [PopUnion<T>, ...A]>
-//     : [T, ...A];
-
-// type StructTypes<T extends Record<string, Codec>, A> =
-//   A extends [infer K, ...infer X]
-//     ? K extends keyof T
-//       ? T[K] extends Codec
-//         ? [T[K], ...StructTypes<T, X>]
-//         : never
-//       : never
-//     : [];
-
-// type ForceTuple<T> =
-//   T extends Codec[]
-//     ? T
-//     : never;
-
 export interface IEventRecord<T extends Codec[]> {
   readonly phase: Phase;
   readonly event: IEvent<T>;
