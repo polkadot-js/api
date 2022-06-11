1f8e322b0dab092cf985ce86e0a6f46c48e5c178
Jaco <jacogr@gmail.com>
2022-06-11 10:58:33 +0300

    Pull westend genesis from knownGenesis

--- packages/types-known/src/upgrades/index.ts
+++ packages/types-known/src/upgrades/index.ts
@@ -5,6 +5,7 @@ import type { ChainUpgrades } from '@polkadot/types/types';
 import type { ChainUpgradesRaw } from './types';
 
 import { selectableNetworks } from '@polkadot/networks';
+import { knownGenesis } from '@polkadot/networks/defaults';
 import { assert, BN, hexToU8a, stringify } from '@polkadot/util';
 
 import kusama from './kusama';
@@ -16,7 +17,7 @@ const allKnown = { kusama, polkadot, westend };
 // testnets are not available in the networks map
 const NET_EXTRA: Record<string, { genesisHash: string[] }> = {
   westend: {
-    genesisHash: ['0xe143f23803ac50e8f6f8e62695d1ce9e4e1d68aa36c1cd2cfd15340213f3423e']
+    genesisHash: knownGenesis.westend
   }
 };
 
