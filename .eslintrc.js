ce8426b91c3a3cd86a7c927fe75a4e8ae3dc0869
Jaco <jacogr@gmail.com>
2022-07-06 00:15:52 +0300

    Additional CI actions

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,8 @@ const base = require('@polkadot/dev/config/eslint.cjs');
 module.exports = {
   ...base,
   ignorePatterns: [
-    ...base.ignorePatterns
+    ...base.ignorePatterns,
+    '**/deno/*.ts'
   ],
   parserOptions: {
     ...base.parserOptions,
---
+++ deno/check.ts
@@ -0,0 +1,18 @@
+// Copyright 2019-2022 @polkadot/api authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+// auto-generated via deno/setup.ts, do not edit
+
+export * as api from 'https://deno.land/x/polkadot/api/mod.ts';
+export * as apiAugment from 'https://deno.land/x/polkadot/api-augment/mod.ts';
+export * as apiBase from 'https://deno.land/x/polkadot/api-base/mod.ts';
+export * as apiContract from 'https://deno.land/x/polkadot/api-contract/mod.ts';
+export * as apiDerive from 'https://deno.land/x/polkadot/api-derive/mod.ts';
+export * as rpcAugment from 'https://deno.land/x/polkadot/rpc-augment/mod.ts';
+export * as rpcCore from 'https://deno.land/x/polkadot/rpc-core/mod.ts';
+export * as rpcProvider from 'https://deno.land/x/polkadot/rpc-provider/mod.ts';
+export * as types from 'https://deno.land/x/polkadot/types/mod.ts';
+export * as typesAugment from 'https://deno.land/x/polkadot/types-augment/mod.ts';
+export * as typesCodec from 'https://deno.land/x/polkadot/types-codec/mod.ts';
+export * as typesCreate from 'https://deno.land/x/polkadot/types-create/mod.ts';
+export * as typesKnown from 'https://deno.land/x/polkadot/types-known/mod.ts';
