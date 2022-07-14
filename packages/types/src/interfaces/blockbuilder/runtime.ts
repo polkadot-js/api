3bf4e905e94c5db33f2b9a542bdcc59041330bff
Jaco <jacogr@gmail.com>
2022-07-14 13:03:15 +0200

    BlockBuilder 2/3/4 & GrandpaApi 2 runtime interface definitions (#5067)

--- packages/types/src/interfaces/blockbuilder/runtime.ts
+++ packages/types/src/interfaces/blockbuilder/runtime.ts
@@ -5,7 +5,29 @@ import type { DefinitionsCall, DefinitionsCallEntry } from '../../types';
 
 import { objectSpread } from '@polkadot/util';
 
-const BB_V5_TO_V6: DefinitionsCallEntry['methods'] = {
+const BB_V2_TO_V4: DefinitionsCallEntry['methods'] = {
+  // this was removed after V4
+  random_seed: {
+    description: 'Generate a random seed.',
+    params: [],
+    type: 'Hash'
+  }
+};
+
+const BB_V2_TO_V5: DefinitionsCallEntry['methods'] = {
+  apply_extrinsic: {
+    description: 'Apply the given extrinsic.',
+    params: [
+      {
+        name: 'extrinsic',
+        type: 'Extrinsic'
+      }
+    ],
+    type: 'ApplyExtrinsicResultPre6'
+  }
+};
+
+const BB_V2_TO_V6: DefinitionsCallEntry['methods'] = {
   check_inherents: {
     description: 'Check that the inherents are valid.',
     params: [
@@ -20,11 +42,6 @@ const BB_V5_TO_V6: DefinitionsCallEntry['methods'] = {
     ],
     type: 'CheckInherentsResult'
   },
-  finalize_block: {
-    description: 'Finish the current block.',
-    params: [],
-    type: 'Header'
-  },
   inherent_extrinsics: {
     description: 'Generate inherent extrinsics.',
     params: [
@@ -34,6 +51,15 @@ const BB_V5_TO_V6: DefinitionsCallEntry['methods'] = {
       }
     ],
     type: 'Vec<Extrinsic>'
+  }
+};
+
+const BB_V3_TO_V6: DefinitionsCallEntry['methods'] = {
+  // renamed in v3 from finalize_block
+  finalize_block: {
+    description: 'Finish the current block.',
+    params: [],
+    type: 'Header'
   }
 };
 
@@ -51,23 +77,36 @@ export const runtime: DefinitionsCall = {
           ],
           type: 'ApplyExtrinsicResult'
         }
-      }, BB_V5_TO_V6),
+      }, BB_V2_TO_V6, BB_V3_TO_V6),
       version: 6
     },
     {
       methods: objectSpread({
-        apply_extrinsic: {
-          description: 'Apply the given extrinsic.',
-          params: [
-            {
-              name: 'extrinsic',
-              type: 'Extrinsic'
-            }
-          ],
-          type: 'ApplyExtrinsicResultPre6'
-        }
-      }, BB_V5_TO_V6),
+        // apply_extrinsic result changed in 6
+      }, BB_V2_TO_V5, BB_V2_TO_V6, BB_V3_TO_V6),
       version: 5
+    },
+    {
+      methods: objectSpread({
+        // random_seed removed
+      }, BB_V2_TO_V4, BB_V2_TO_V5, BB_V2_TO_V6, BB_V3_TO_V6),
+      version: 4
+    },
+    {
+      methods: objectSpread({
+        // finalize_block renamed
+      }, BB_V2_TO_V4, BB_V2_TO_V6, BB_V3_TO_V6),
+      version: 3
+    },
+    {
+      methods: objectSpread({
+        finalise_block: {
+          description: 'Finish the current block.',
+          params: [],
+          type: 'Header'
+        }
+      }, BB_V2_TO_V4, BB_V2_TO_V6),
+      version: 2
     }
   ]
 };
