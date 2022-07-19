83f9056cba235a6b621709526ebbd7400e3a0562
wirednkod <wirednkod@gmail.com>
2022-07-19 17:55:26 +0200

    Remove comment about ESM-only at bundle

--- packages/rpc-provider/src/bundle.ts
+++ packages/rpc-provider/src/bundle.ts
@@ -4,6 +4,4 @@
 export { HttpProvider } from './http';
 export { packageInfo } from './packageInfo';
 export { WsProvider } from './ws';
-
-// ESM-only, only export top-level when we have dual versions
 export { ScProvider } from './substrate-connect';
