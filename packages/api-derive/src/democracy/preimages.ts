139eb5be6048e4b9167f6c9550786df3f898f201
Jaco <jacogr@gmail.com>
2022-10-20 12:09:08 +0300

    Retrieve preimages from new pallet

--- packages/api-derive/src/democracy/preimages.ts
+++ packages/api-derive/src/democracy/preimages.ts
@@ -2,17 +2,19 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import type { Observable } from 'rxjs';
-import type { AccountId, Balance, BlockNumber, Hash, PreimageStatus, Proposal } from '@polkadot/types/interfaces';
-import type { FrameSupportPreimagesBounded } from '@polkadot/types/lookup';
+import type { AccountId, Balance, BlockNumber, Call, Hash, PreimageStatus } from '@polkadot/types/interfaces';
+import type { FrameSupportPreimagesBounded, PalletPreimageRequestStatus } from '@polkadot/types/lookup';
 import type { Bytes, Option } from '@polkadot/types-codec';
 import type { ITuple } from '@polkadot/types-codec/types';
+import type { HexString } from '@polkadot/util/types';
 import type { DeriveApi, DeriveProposalImage } from '../types';
 
-import { map, of } from 'rxjs';
+import { map, of, switchMap } from 'rxjs';
 
-import { isFunction } from '@polkadot/util';
+import { BN_ZERO, isFunction } from '@polkadot/util';
 
 import { firstMemo, memo } from '../util';
+import { getImageHashBounded } from './util';
 
 type PreimageInfo = [Bytes, AccountId, Balance, BlockNumber];
 type OldPreimage = ITuple<PreimageInfo>;
@@ -22,7 +24,7 @@ function isDemocracyPreimage (api: DeriveApi, imageOpt: Option<OldPreimage> | Op
 }
 
 function constructProposal (api: DeriveApi, [bytes, proposer, balance, at]: PreimageInfo): DeriveProposalImage {
-  let proposal: Proposal | undefined;
+  let proposal: Call | undefined;
 
   try {
     proposal = api.registry.createType('Call', bytes.toU8a(true));
@@ -53,6 +55,27 @@ function parseDemocracy (api: DeriveApi, imageOpt: Option<OldPreimage> | Option<
   return constructProposal(api, imageOpt.unwrap());
 }
 
+function parseImage (api: DeriveApi, [proposalHash, status, bytes]: [HexString, PalletPreimageRequestStatus | null, Bytes | null]): DeriveProposalImage | undefined {
+  if (!status) {
+    return undefined;
+  }
+
+  const [proposer, balance] = status.isUnrequested
+    ? status.asUnrequested.deposit
+    : status.asRequested.deposit.unwrapOrDefault();
+  let proposal: Call | undefined;
+
+  if (bytes) {
+    try {
+      proposal = api.registry.createType('Call', bytes.toU8a(true));
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  return { at: BN_ZERO, balance, proposal, proposalHash, proposer };
+}
+
 function getDemocracyImages (api: DeriveApi, hashes: (Hash | Uint8Array | string)[]): Observable<(DeriveProposalImage | undefined)[]> {
   return api.query.democracy.preimages.multi<Option<PreimageStatus>>(hashes).pipe(
     map((images): (DeriveProposalImage | undefined)[] =>
@@ -61,9 +84,37 @@ function getDemocracyImages (api: DeriveApi, hashes: (Hash | Uint8Array | string
   );
 }
 
-function getImages (api: DeriveApi, hashes: FrameSupportPreimagesBounded[]): Observable<(DeriveProposalImage | undefined)[]> {
-  // TODO: We want to create the actual derives - for now this is not supported
-  return of(hashes.map(() => undefined));
+function getImages (api: DeriveApi, bounded: FrameSupportPreimagesBounded[]): Observable<(DeriveProposalImage | undefined)[]> {
+  const hashes = bounded.map((b) => getImageHashBounded(b));
+
+  return api.query.preimage.statusFor.multi(hashes).pipe(
+    switchMap((optStatus) => {
+      const statuses = optStatus.map((o) => o.unwrapOr(null));
+      const keys = statuses
+        .map((s, i) =>
+          s
+            ? s.isRequested
+              ? [hashes[i], s.asRequested.len.unwrapOr(0)]
+              : [hashes[i], s.asUnrequested.len]
+            : null
+        )
+        .filter((p) => !!p);
+
+      return api.query.preimage.preimageFor.multi(keys).pipe(
+        map((optBytes) => {
+          let ptr = -1;
+
+          return statuses
+            .map((s, i): [HexString, PalletPreimageRequestStatus | null, Bytes | null] =>
+              s
+                ? [hashes[i], s, optBytes[++ptr].unwrapOr(null)]
+                : [hashes[i], null, null]
+            )
+            .map((v) => parseImage(api, v));
+        })
+      );
+    })
+  );
 }
 
 export function preimages (instanceId: string, api: DeriveApi): (hashes: (Hash | Uint8Array | string | FrameSupportPreimagesBounded)[]) => Observable<(DeriveProposalImage | undefined)[]> {
