1da7ffa591b4d6ff06b556389de800a1cf046c8c
jasl <jasl9187@hotmail.com>
2022-11-11 02:40:05 +0800

    Apply suggestion

--- packages/api-contract/src/types.ts
+++ packages/api-contract/src/types.ts
@@ -3,8 +3,8 @@
 
 import type { ApiBase } from '@polkadot/api/base';
 import type { ApiTypes } from '@polkadot/api/types';
-import type { Text, u64 } from '@polkadot/types';
-import type { ContractExecResultResult, ContractSelector, StorageDeposit, WeightV2 } from '@polkadot/types/interfaces';
+import type { Text } from '@polkadot/types';
+import type { ContractExecResultResult, ContractSelector, StorageDeposit, Weight, WeightV2 } from '@polkadot/types/interfaces';
 import type { Codec, TypeDef } from '@polkadot/types/types';
 import type { BN } from '@polkadot/util';
 import type { Abi } from '.';
@@ -55,8 +55,8 @@ export interface InterfaceContractCalls {
 
 export interface ContractCallOutcome {
   debugMessage: Text;
-  gasConsumed: u64 | WeightV2;
-  gasRequired: u64 | WeightV2;
+  gasConsumed: Weight;
+  gasRequired: Weight;
   output: Codec | null;
   result: ContractExecResultResult;
   storageDeposit: StorageDeposit;
