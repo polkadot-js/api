56f8527d03ce7d0165128716548b6b1cd14a4cde
Jaco <jacogr@gmail.com>
2022-11-11 17:06:57 +0200

    ContractsApi v2 support

--- packages/types/src/interfaces/contracts/runtime.ts
+++ packages/types/src/interfaces/contracts/runtime.ts
@@ -1,12 +1,118 @@
 // Copyright 2017-2022 @polkadot/types authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import type { DefinitionsCall } from '../../types';
+import type { DefinitionCall, DefinitionsCall } from '../../types';
+
+import { objectSpread } from '@polkadot/util';
+
+const SHARED_V1_V2: Record<string, DefinitionCall> = {
+  get_storage: {
+    description: 'Query a given storage key in a given contract.',
+    params: [
+      {
+        name: 'address',
+        type: 'AccountId'
+      },
+      {
+        name: 'key',
+        type: 'Bytes'
+      }
+    ],
+    type: 'Option<Bytes>'
+  },
+  upload_code: {
+    description: 'Upload new code without instantiating a contract from it.',
+    params: [
+      {
+        name: 'origin',
+        type: 'AccountId'
+      },
+      {
+        name: 'code',
+        type: 'Bytes'
+      },
+      {
+        name: 'storageDepositLimit',
+        type: 'Option<Balance>'
+      }
+    ],
+    type: 'CodeUploadResult'
+  }
+};
 
 export const runtime: DefinitionsCall = {
   ContractsApi: [
     {
-      methods: {
+      methods: objectSpread({
+        call: {
+          description: 'Perform a call from a specified account to a given contract.',
+          params: [
+            {
+              name: 'origin',
+              type: 'AccountId'
+            },
+            {
+              name: 'dest',
+              type: 'AccountId'
+            },
+            {
+              name: 'value',
+              type: 'Balance'
+            },
+            {
+              name: 'gasLimit',
+              type: 'Option<WeightV2>'
+            },
+            {
+              name: 'storageDepositLimit',
+              type: 'Option<Balance>'
+            },
+            {
+              name: 'inputData',
+              type: 'Vec<u8>'
+            }
+          ],
+          type: 'ContractExecResult'
+        },
+        instantiate: {
+          description: 'Instantiate a new contract.',
+          params: [
+            {
+              name: 'origin',
+              type: 'AccountId'
+            },
+            {
+              name: 'value',
+              type: 'Balance'
+            },
+            {
+              name: 'gasLimit',
+              type: 'Option<WeightV2>'
+            },
+            {
+              name: 'storageDepositLimit',
+              type: 'Option<Balance>'
+            },
+            {
+              name: 'code',
+              type: 'CodeSource'
+            },
+            {
+              name: 'data',
+              type: 'Bytes'
+            },
+            {
+              name: 'salt',
+              type: 'Bytes'
+            }
+          ],
+          type: 'ContractInstantiateResult'
+        }
+      }, SHARED_V1_V2),
+      version: 2
+    },
+    {
+      methods: objectSpread({
         call: {
           description: 'Perform a call from a specified account to a given contract.',
           params: [
@@ -35,21 +141,7 @@ export const runtime: DefinitionsCall = {
               type: 'Vec<u8>'
             }
           ],
-          type: 'ContractExecResult'
-        },
-        get_storage: {
-          description: 'Query a given storage key in a given contract.',
-          params: [
-            {
-              name: 'address',
-              type: 'AccountId'
-            },
-            {
-              name: 'key',
-              type: 'Bytes'
-            }
-          ],
-          type: 'Option<Bytes>'
+          type: 'ContractExecResultU64'
         },
         instantiate: {
           description: 'Instantiate a new contract.',
@@ -83,27 +175,9 @@ export const runtime: DefinitionsCall = {
               type: 'Bytes'
             }
           ],
-          type: 'ContractInstantiateResult'
-        },
-        upload_code: {
-          description: 'Upload new code without instantiating a contract from it.',
-          params: [
-            {
-              name: 'origin',
-              type: 'AccountId'
-            },
-            {
-              name: 'code',
-              type: 'Bytes'
-            },
-            {
-              name: 'storageDepositLimit',
-              type: 'Option<Balance>'
-            }
-          ],
-          type: 'CodeUploadResult'
+          type: 'ContractInstantiateResultU64'
         }
-      },
+      }, SHARED_V1_V2),
       version: 1
     }
   ]
