349d4e98bc0f5cac6a395768bfd26566c086023b
Jaco <jacogr@gmail.com>
2022-11-23 09:37:02 +0200

    Adjust for easier revert in the future

--- packages/rpc-provider/src/mod.ts
+++ packages/rpc-provider/src/mod.ts
@@ -1,7 +1,9 @@
 // Copyright 2017-2022 @polkadot/rpc-provider authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-// NOTE: Generally that file _should_ only `export * from './index'`, however
+// export * from './index';
+//
+// NOTE: Generally this file _should_ only `export * from './index'`, however
 // for the time being we are not exporting sc-provider directly. Hence we are
 // using a combination of index.ts & bundle.ts to generate the contents of this
 
