7b17a4495d7eec04aa1d44bfb2b3d959b746006f
Jaco <jacogr@gmail.com>
2022-12-13 11:09:37 +0200

    registry.hash(...)

--- packages/types/src/extrinsic/util.ts
+++ packages/types/src/extrinsic/util.ts
@@ -8,7 +8,7 @@ import type { IKeyringPair } from '../types';
 // a helper function for both types of payloads, Raw and metadata-known
 export function sign (registry: Registry, signerPair: IKeyringPair, u8a: Uint8Array, options?: SignOptions): Uint8Array {
   const encoded = u8a.length > 256
-    ? registry.$hash(u8a)
+    ? registry.hash(u8a)
     : u8a;
 
   return signerPair.sign(encoded, options);
