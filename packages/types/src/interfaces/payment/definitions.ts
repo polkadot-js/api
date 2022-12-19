fb65012f7161ac47c8c71017e0f166fddec5fd4b
Jaco <jacogr@gmail.com>
2022-12-19 14:57:42 +0200

    Add fallbacks for paymentInfo (non-fixed) results

--- packages/types/src/interfaces/payment/definitions.ts
+++ packages/types/src/interfaces/payment/definitions.ts
@@ -29,6 +29,13 @@ export default {
       partialFee: 'Balance'
     },
     RuntimeDispatchInfoV1: {
+      // NOTE: Generally we always point high -> low, however in this case we do have some _potential_
+      // discrepancies where some nodes may not return the correct RPC responses. Basically any node that
+      // is on WeightV2 and does not yet include https://github.com/paritytech/substrate/pull/12633 would
+      // have unparsable results. So effectively this makes the API not fail in those cases, however it
+      // will create "some" chaos on the side of the users who does expect the correct response.
+      // TL:DR Somebody _will_ still break since in cases where the RPCs are "between fixes"
+      __fallback: 'RuntimeDispatchInfoV2',
       weight: 'WeightV1',
       class: 'DispatchClass',
       partialFee: 'Balance'
