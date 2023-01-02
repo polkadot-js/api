d046ebc58ed7b861470784870870981b9e2958a7
Ken Vu <ken.vu@centrality.ai>
2023-01-03 10:43:46 +1300

    Address eslint issues plus add guard for Compact type

--- packages/api/src/util/filterEvents.ts
+++ packages/api/src/util/filterEvents.ts
@@ -3,6 +3,8 @@
 
 import type { BlockNumber, EventRecord, ExtrinsicStatus, H256, SignedBlock } from '@polkadot/types/interfaces';
 
+import { isCompact } from '@polkadot/util';
+
 import { l } from './logging';
 
 export function filterEvents (txHash: H256, { block: { extrinsics, header } }: SignedBlock, allEvents: EventRecord[], status: ExtrinsicStatus): { events?: EventRecord[], txIndex?: number, blockNumber?: BlockNumber } {
@@ -10,12 +12,12 @@ export function filterEvents (txHash: H256, { block: { extrinsics, header } }: S
   for (const [txIndex, x] of extrinsics.entries()) {
     if (x.hash.eq(txHash)) {
       return {
+        blockNumber: isCompact<BlockNumber>(header.number) ? header.number.unwrap() : header.number,
         events: allEvents.filter(({ phase }) =>
           phase.isApplyExtrinsic &&
           phase.asApplyExtrinsic.eqn(txIndex)
         ),
-        txIndex,
-        blockNumber: header.number.unwrap()
+        txIndex
       };
     }
   }
