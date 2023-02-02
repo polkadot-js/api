7c24998e28ca5459c6b9ae548536a55b26eb27f7
Jaco <jacogr@gmail.com>
2023-02-02 13:03:48 +0200

    Adjust PortableRegistry aliases (w/o any Weight) (#5466)

--- packages/types/src/metadata/PortableRegistry/PortableRegistry.ts
+++ packages/types/src/metadata/PortableRegistry/PortableRegistry.ts
@@ -47,8 +47,6 @@ const PATHS_ALIAS = splitNamespace([
   'sp_core::crypto::AccountId32',
   'sp_runtime::generic::era::Era',
   'sp_runtime::multiaddress::MultiAddress',
-  // weights 1.5+ is a structure can be flatenned
-  'frame_support::weights::weight_v2::Weight',
   // ethereum overrides (Frontier, Moonbeam, Polkadot claims)
   'account::AccountId20',
   'polkadot_runtime_common::claims::EthereumAddress',
