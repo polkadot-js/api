3d74b64b78c5c1cec2cc18602cb3dec7dfc9fc96
Jaco <jacogr@gmail.com>
2023-02-27 16:12:06 +0200

    Adjust & actual run

--- packages/typegen/scripts/polkadot-types-chain-info.mjs
+++ packages/typegen/scripts/polkadot-types-chain-info.mjs
@@ -2,4 +2,6 @@
 // Copyright 2017-2023 @polkadot/typegen authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-require('./execWithSrc.cjs')('extractChain');
+import { main } from '@polkadot/typegen/extractChain';
+
+main();
