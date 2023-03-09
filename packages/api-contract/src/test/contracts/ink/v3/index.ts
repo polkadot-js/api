6093d0e651f2d46fbe499e867e8a9cb76898afe0
Jaco <jacogr@gmail.com>
2023-03-09 19:48:46 +0200

    Fix assertions

--- packages/api-contract/src/test/contracts/ink/v3/index.ts
+++ packages/api-contract/src/test/contracts/ink/v3/index.ts
@@ -1,6 +1,6 @@
 // Copyright 2017-2023 @polkadot/api-contract authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-export { default as flipper } from './flipper.contract.json' assert { type: 'json.js' };
+export { default as flipper } from './flipper.contract.json' assert { type: 'json' };
 // A complex contract example with traits.
-export { default as traitErc20 } from './trait_erc20.contract.json' assert { type: 'json.js' };
+export { default as traitErc20 } from './trait_erc20.contract.json' assert { type: 'json' };
--- packages/api-contract/src/test/contracts/user/v0/index.ts
+++ packages/api-contract/src/test/contracts/user/v0/index.ts
@@ -1,7 +1,7 @@
 // Copyright 2017-2023 @polkadot/api-contract authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-export { default as assetTransfer } from './assetTransfer.json' assert { type: 'json.js' };
-export { default as enumExample } from './enumExample.json' assert { type: 'json.js' };
-export { default as recursive } from './recursive.contract.json' assert { type: 'json.js' };
-export { default as withString } from './withString.json' assert { type: 'json.js' };
+export { default as assetTransfer } from './assetTransfer.json' assert { type: 'json' };
+export { default as enumExample } from './enumExample.json' assert { type: 'json' };
+export { default as recursive } from './recursive.contract.json' assert { type: 'json' };
+export { default as withString } from './withString.json' assert { type: 'json' };
