3520e029bd62d1bf919e04a9e67c607a27438a32
Rob Thijssen <rthijssen@gmail.com>
2023-03-27 08:58:18 +0300

    implicit type for session.validators
    we don't want to change this aspect of the implementation

--- packages/api-derive/src/chain/util.ts
+++ packages/api-derive/src/chain/util.ts
@@ -9,7 +9,6 @@ import type { Codec, IOption } from '@polkadot/types/types';
 import type { DeriveApi } from '../types.js';
 
 import { combineLatest, map, of } from 'rxjs';
-
 import { memo, unwrapBlockNumber } from '../util/index.js';
 
 export type BlockNumberDerive = (instanceId: string, api: DeriveApi) => () => Observable<BlockNumber>;
@@ -25,10 +24,10 @@ export function createBlockNumberDerive <T extends { number: Compact<BlockNumber
 
 export function getAuthorDetails (header: Header, queryAt: QueryableStorage<'rxjs'>): Observable<[Header, Vec<AccountId> | null, AccountId | null]> {
   const validators = queryAt.session
-    ? queryAt.session.validators<Vec<AccountId>>()
+    ? queryAt.session.validators()
     : of(null);
 
-  // nimbus consensus stores authorship in the header logs
+  // nimbus consensus stores the session key of the block author in header logs
   const { logs: [log] } = header.digest;
   const loggedAuthor = (log && (
     (log.isConsensus && log.asConsensus[0].isNimbus && log.asConsensus[1]) ||
@@ -36,6 +35,7 @@ export function getAuthorDetails (header: Header, queryAt: QueryableStorage<'rxj
   ));
 
   if (loggedAuthor) {
+
     // use the author mapping pallet if available (ie: moonbeam, moonriver)
     if (queryAt.authorMapping && queryAt.authorMapping.mappingWithDeposit) {
       return combineLatest([
@@ -51,8 +51,8 @@ export function getAuthorDetails (header: Header, queryAt: QueryableStorage<'rxj
       return combineLatest([
         of(header),
         validators,
-        queryAt.session.queuedKeys<Array<[AccountId, { nimbus: Address }]>>().pipe(
-          map((x) => x.find((t) => t[1].nimbus.toHex() === loggedAuthor.toHex())),
+        queryAt.session.queuedKeys<[AccountId, { nimbus: Address }][]>().pipe(
+          map((x) => x.find(t => t[1].nimbus.toHex() === loggedAuthor.toHex())),
           map((x) => (x) ? x[0] : null)
         )
       ]);
