4c8e7135dee100f18c1ed12aa7db61bcfc46de18
Branislav Kontur <bkontur@gmail.com>
2023-04-14 10:14:08 +0200

    Fungibles runtime api for statemine/statemint/westmint (#5584)
    * Fungibles runtime api for statemine/statemint/westmint
    
    * Update packages/types/src/interfaces/fungibles/definitions.ts
    
    * export `./fungibles/definitions.js`
    
    ---------
    
    Co-authored-by: Jaco <jacogr@gmail.com>

---
+++ packages/types/src/interfaces/fungibles/runtime.ts
@@ -0,0 +1,24 @@
+// Copyright 2017-2023 @polkadot/types authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import type { DefinitionsCall } from '../../types/index.js';
+
+export const runtime: DefinitionsCall = {
+  FungiblesApi: [
+    {
+      methods: {
+        query_account_balances: {
+          description: 'Returns the list of all `MultiAsset` that an `AccountId` has',
+          params: [
+            {
+              name: 'account',
+              type: 'AccountId'
+            }
+          ],
+          type: 'Result<Vec<MultiAsset>, FungiblesAccessError>'
+        }
+      },
+      version: 1
+    }
+  ]
+};
