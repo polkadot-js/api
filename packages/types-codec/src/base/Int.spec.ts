136ec7c60014495ce0b93d8927b56b8bf26e3dad
Jaco <jacogr@gmail.com>
2023-08-24 13:20:03 +0300

    Group Int/Uint tests, size check compare for Int (#5709)
    * Group In/Uint tests, size check compare for Int
    
    * Adjust

--- packages/types-codec/src/base/Int.spec.ts
+++ packages/types-codec/src/base/Int.spec.ts
@@ -3,29 +3,26 @@
 
 /// <reference types="@polkadot/dev-test/globals.d.ts" />
 
+import type { UIntBitLength } from '../types/index.js';
+
 import { TypeRegistry } from '@polkadot/types';
 import { Int } from '@polkadot/types-codec';
+import { BN } from '@polkadot/util';
+
+const TESTS: [bitLength: UIntBitLength, value: string | number | Uint8Array, expected?: string][] = [
+  [8, 0],
+  [8, 127],
+  [8, -128],
+  [8, new Uint8Array([0])],
+  [8, new Uint8Array([127])],
+  [8, new Uint8Array([128]), '-128'],
+  [32, 0],
+  [32, 2147483647],
+  [32, -2147483648]
+];
 
 describe('Int', (): void => {
   const registry = new TypeRegistry();
-
-  it('provides a toBigInt interface', (): void => {
-    expect(
-      new Int(registry, -1234).toBigInt()
-    ).toEqual(-1234n);
-  });
-
-  it('provides a toBn interface', (): void => {
-    expect(
-      new Int(registry, -1234).toBn().toNumber()
-    ).toEqual(-1234);
-  });
-
-  it('provides a toNumber interface', (): void => {
-    expect(
-      new Int(registry, -1234).toNumber()
-    ).toEqual(-1234);
-  });
 
   it('can construct via a single-entry struct', (): void => {
     expect(
@@ -50,13 +47,6 @@ describe('Int', (): void => {
     ).toEqual(new Uint8Array([46, 251, 255, 255]));
   });
 
-  it('converts to hex/string', (): void => {
-    const i = new Int(registry, '0x12', 16);
-
-    expect(i.toHex()).toEqual('0x0012');
-    expect(i.toString()).toEqual('18');
-  });
-
   it('converts to equivalents', (): void => {
     const a = new Int(registry, '-123');
 
@@ -74,11 +64,38 @@ describe('Int', (): void => {
     ).toEqual(0);
   });
 
-  it('has a sane inspect', (): void => {
-    expect(
-      new Int(registry, '0x12', 16).inspect()
-    ).toEqual({
-      outer: [new Uint8Array([0x12, 0x00])]
+  describe('utilities', (): void => {
+    it('provides a toBigInt interface', (): void => {
+      expect(
+        new Int(registry, -1234).toBigInt()
+      ).toEqual(-1234n);
+    });
+
+    it('provides a toBn interface', (): void => {
+      expect(
+        new Int(registry, -1234).toBn().toNumber()
+      ).toEqual(-1234);
+    });
+
+    it('provides a toNumber interface', (): void => {
+      expect(
+        new Int(registry, -1234).toNumber()
+      ).toEqual(-1234);
+    });
+
+    it('has a sane inspect', (): void => {
+      expect(
+        new Int(registry, '0x12', 16).inspect()
+      ).toEqual({
+        outer: [new Uint8Array([0x12, 0x00])]
+      });
+    });
+
+    it('converts to hex/string', (): void => {
+      const i = new Int(registry, '0x12', 16);
+
+      expect(i.toHex()).toEqual('0x0012');
+      expect(i.toString()).toEqual('18');
     });
   });
 
@@ -93,6 +110,16 @@ describe('Int', (): void => {
       expect(
         new (Int.with(64, 'SomethingElse'))(registry).toRawType()
       ).toEqual('SomethingElse');
+    });
+  });
+
+  describe('conversion tests', (): void => {
+    TESTS.forEach(([bitLength, input, expected], i): void => {
+      it(`#${i}: converts ${input as string}`, (): void => {
+        expect(
+          new Int(registry, Array.isArray(input) ? new Uint8Array(input) : input, bitLength).toString()
+        ).toEqual(expected || new BN(input).toString());
+      });
     });
   });
 });
