e0933b080e7be077ba7e0fa23b768c244804450e
Jaco <jacogr@gmail.com>
2023-12-19 10:40:56 +0200

    Bump dev w/ packageDetect adjustments

--- packages/typegen/src/index.ts
+++ packages/typegen/src/index.ts
@@ -1,6 +1,6 @@
 // Copyright 2017-2023 @polkadot/typegen authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import './detectPackage.js';
+import './packageDetect.js';
 
 export * from './bundle.js';
--- packages/types/src/detectOther.ts
+++
@@ -1,7 +0,0 @@
-// Copyright 2017-2023 @polkadot/types authors & contributors
-// SPDX-License-Identifier: Apache-2.0
-
-import { packageInfo as codecInfo } from '@polkadot/types-codec/packageInfo';
-import { packageInfo as createInfo } from '@polkadot/types-create/packageInfo';
-
-export default [codecInfo, createInfo];
