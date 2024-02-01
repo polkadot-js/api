9f93a19b451b8327d379a2ea5cedf8d28160b05e
peetzweg/ <phil.czek@gmail.com>
2024-02-01 12:53:11 +0100

    adds toV5 boilerplate code draft

---
+++ packages/api-contract/src/Abi/toV5.ts
@@ -0,0 +1,20 @@
+// Copyright 2017-2023 @polkadot/api-contract authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import type { ContractMetadataV4, ContractMetadataV5 } from '@polkadot/types/interfaces';
+import type { Registry } from '@polkadot/types/types';
+
+import { objectSpread } from '@polkadot/util';
+
+export function v4ToV5 (registry: Registry, v4: ContractMetadataV4): ContractMetadataV5 {
+  return registry.createType('ContractMetadataV5', objectSpread({}, v4, {
+    spec: objectSpread({}, v4.spec, {
+      events: v4.spec.events.map((e) =>
+        registry.createType('ContractEventSpecV3', objectSpread({
+          module_path: 'todo::todo',
+          signature_topic: "0xDEADBEEF"
+        }, e))
+      )
+    })
+  }));
+}
