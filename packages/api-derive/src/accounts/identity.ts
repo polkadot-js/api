b9ec338808bfa5f94891ce2a7664236be70d099a
tarikgul <tariksnow37@gmail.com>
2024-03-06 12:53:24 -0500

    adjust types

--- packages/api-derive/src/accounts/identity.ts
+++ packages/api-derive/src/accounts/identity.ts
@@ -73,7 +73,7 @@ function extractIdentity (identityOfOpt?: Option<ITuple<[PalletIdentityRegistrat
   };
 }
 
-function getParent (api: DeriveApi, identityOfOpt: Option<ITuple<[PalletIdentityRegistration, Option<Bytes>]>> | undefined, superOfOpt: Option<ITuple<[AccountId, Data]>> | undefined): Observable<[Option<ITuple<[PalletIdentityRegistration, Option<Bytes>]>> | undefined, [AccountId, Data] | undefined]> {
+function getParent (api: DeriveApi, identityOfOpt: Option<ITuple<[PalletIdentityRegistration, Option<Bytes>]>> | Option<PalletIdentityRegistration> | undefined, superOfOpt: Option<ITuple<[AccountId, Data]>> | undefined): Observable<[Option<ITuple<[PalletIdentityRegistration, Option<Bytes>]>> | Option<PalletIdentityRegistration> | undefined, [AccountId, Data] | undefined]> {
   if (identityOfOpt?.isSome) {
     // this identity has something set
     return of([identityOfOpt, undefined]);
@@ -92,8 +92,8 @@ function getParent (api: DeriveApi, identityOfOpt: Option<ITuple<[PalletIdentity
   return of([undefined, undefined]);
 }
 
-export function _identity (instanceId: string, api: DeriveApi): (accountId?: AccountId | Uint8Array | string) => Observable<[Option<ITuple<[PalletIdentityRegistration, Option<Bytes>]>> | undefined, Option<ITuple<[AccountId, Data]>> | undefined]> {
-  return memo(instanceId, (accountId?: AccountId | Uint8Array | string): Observable<[Option<ITuple<[PalletIdentityRegistration, Option<Bytes>]>> | undefined, Option<ITuple<[AccountId, Data]>> | undefined]> =>
+export function _identity (instanceId: string, api: DeriveApi): (accountId?: AccountId | Uint8Array | string) => Observable<[Option<ITuple<[PalletIdentityRegistration, Option<Bytes>]>> | Option<PalletIdentityRegistration> | undefined, Option<ITuple<[AccountId, Data]>> | undefined]> {
+  return memo(instanceId, (accountId?: AccountId | Uint8Array | string): Observable<[Option<ITuple<[PalletIdentityRegistration, Option<Bytes>]>> | Option<PalletIdentityRegistration> | undefined, Option<ITuple<[AccountId, Data]>> | undefined]> =>
     accountId && api.query.identity?.identityOf
       ? combineLatest([
         api.query.identity.identityOf(accountId),
