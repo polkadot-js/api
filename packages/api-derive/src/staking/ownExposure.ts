aedcf8402d33ba7c12dd5bd3baf6778a95a847be
tarikgul <tariksnow37@gmail.com>
2024-05-06 10:44:10 -0400

    Ensure backwards compat for ownExposure

--- packages/api-derive/src/staking/ownExposure.ts
+++ packages/api-derive/src/staking/ownExposure.ts
@@ -4,7 +4,7 @@
 import type { Observable } from 'rxjs';
 import type { Option, u32 } from '@polkadot/types';
 import type { EraIndex } from '@polkadot/types/interfaces';
-import type { SpStakingExposurePage } from '@polkadot/types/lookup';
+import type { SpStakingExposure, SpStakingExposurePage, SpStakingPagedExposureMetadata } from '@polkadot/types/lookup';
 import type { AnyNumber } from '@polkadot/types-codec/types';
 import type { DeriveApi, DeriveOwnExposure } from '../types.js';
 
@@ -15,13 +15,27 @@ import { erasHistoricApplyAccount } from './util.js';
 
 export function _ownExposures (instanceId: string, api: DeriveApi): (accountId: Uint8Array | string, eras: EraIndex[], withActive: boolean, page: u32 | AnyNumber) => Observable<DeriveOwnExposure[]> {
   return memo(instanceId, (accountId: Uint8Array | string, eras: EraIndex[], _withActive: boolean, page: u32 | AnyNumber): Observable<DeriveOwnExposure[]> => {
+    const emptyStakingExposure = api.registry.createType<SpStakingExposure>('Exposure');
+    // The reason we don't explicitly make the actual types is for compatibility. If the chain doesn't have the noted type it will fail
+    // on construction. Therefore we just make an empty option.
+    const emptyOptionPage = api.registry.createType<Option<SpStakingExposurePage>>('Option<Null>');
+    const emptyOptionMeta = api.registry.createType<Option<SpStakingPagedExposureMetadata>>('Option<Null>');
+
     return eras.length
       ? combineLatest([
         // Backwards and forward compat for historical integrity when using `erasHistoricApplyAccount`
-        combineLatest(eras.map((e) => api.query.staking.erasStakersClipped(e, accountId))),
-        combineLatest(eras.map((e) => api.query.staking.erasStakers(e, accountId))),
-        combineLatest(eras.map((e) => api.query.staking.erasStakersPaged<Option<SpStakingExposurePage>>(e, accountId, page))),
-        combineLatest(eras.map((e) => api.query.staking.erasStakersOverview(e, accountId)))
+        api.query.staking.erasStakersClipped
+          ? combineLatest(eras.map((e) => api.query.staking.erasStakersClipped(e, accountId)))
+          : of(eras.map((_) => emptyStakingExposure)),
+        api.query.staking.erasStakers
+          ? combineLatest(eras.map((e) => api.query.staking.erasStakers(e, accountId)))
+          : of(eras.map((_) => emptyStakingExposure)),
+        api.query.staking.erasStakersPaged
+          ? combineLatest(eras.map((e) => api.query.staking.erasStakersPaged<Option<SpStakingExposurePage>>(e, accountId, page)))
+          : of(eras.map((_) => emptyOptionPage)),
+        api.query.staking.erasStakersOverview
+          ? combineLatest(eras.map((e) => api.query.staking.erasStakersOverview(e, accountId)))
+          : of(eras.map((_) => emptyOptionMeta))
       ]).pipe(
         map(([clp, exp, paged, expMeta]): DeriveOwnExposure[] =>
           eras.map((era, index) => ({ clipped: clp[index], era, exposure: exp[index], exposureMeta: expMeta[index], exposurePaged: paged[index] }))
