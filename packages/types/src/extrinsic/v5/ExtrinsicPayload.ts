9a371948ef1bfb0d70dd8321b7d2ce983ab46237
tarikgul <tariksnow37@gmail.com>
2024-09-20 23:48:40 -0400

    Decouple payload v5 from v4

--- packages/types/src/extrinsic/v5/ExtrinsicPayload.ts
+++ packages/types/src/extrinsic/v5/ExtrinsicPayload.ts
@@ -2,14 +2,18 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import type { SignOptions } from '@polkadot/keyring/types';
-import type { Registry } from '@polkadot/types-codec/types';
+import type { Hash, MultiLocation } from '@polkadot/types/interfaces';
+import type { Bytes } from '@polkadot/types-codec';
+import type { Inspect, Registry } from '@polkadot/types-codec/types';
 import type { HexString } from '@polkadot/util/types';
-import type { ExtrinsicPayloadValue, IKeyringPair } from '../../types/index.js';
+import type { BlockHash } from '../../interfaces/chain/index.js';
+import type { ExtrinsicEra } from '../../interfaces/extrinsics/index.js';
+import type { ExtrinsicPayloadValue, ICompact, IKeyringPair, INumber, IOption } from '../../types/index.js';
 
-import { Enum } from '@polkadot/types-codec';
+import { Enum, Struct } from '@polkadot/types-codec';
+import { objectSpread } from '@polkadot/util';
 
 import { signV5 } from '../util.js';
-import { GenericExtrinsicPayloadV4 } from '../v4/ExtrinsicPayload.js';
 
 /**
  * @name GenericExtrinsicPayloadV5
@@ -17,11 +21,15 @@ import { GenericExtrinsicPayloadV4 } from '../v4/ExtrinsicPayload.js';
  * A signing payload for an [[Extrinsic]]. For the final encoding, it is
  * variable length based on the contents included
  */
-export class GenericExtrinsicPayloadV5 extends GenericExtrinsicPayloadV4 {
+export class GenericExtrinsicPayloadV5 extends Struct {
   #signOptions: SignOptions;
 
   constructor (registry: Registry, value?: ExtrinsicPayloadValue | Uint8Array | HexString) {
-    super(registry, value);
+    super(registry, objectSpread(
+      { method: 'Bytes' },
+      registry.getSignedExtensionTypes(),
+      registry.getSignedExtensionExtra()
+    ), value);
 
     // Do detection for the type of extrinsic, in the case of MultiSignature
     // this is an enum, in the case of AnySignature, this is a Hash only
@@ -32,9 +40,86 @@ export class GenericExtrinsicPayloadV5 extends GenericExtrinsicPayloadV4 {
   }
 
   /**
+   * @description Returns a breakdown of the hex encoding for this Codec
+   */
+  public override inspect (): Inspect {
+    return super.inspect({ method: true });
+  }
+
+  /**
+   * @description The block [[BlockHash]] the signature applies to (mortal/immortal)
+   */
+  public get blockHash (): BlockHash {
+    return this.getT('blockHash');
+  }
+
+  /**
+   * @description The [[ExtrinsicEra]]
+   */
+  public get era (): ExtrinsicEra {
+    return this.getT('era');
+  }
+
+  /**
+   * @description The genesis [[BlockHash]] the signature applies to (mortal/immortal)
+   */
+  public get genesisHash (): BlockHash {
+    return this.getT('genesisHash');
+  }
+
+  /**
+   * @description The [[Bytes]] contained in the payload
+   */
+  public get method (): Bytes {
+    return this.getT('method');
+  }
+
+  /**
+   * @description The [[Index]]
+   */
+  public get nonce (): ICompact<INumber> {
+    return this.getT('nonce');
+  }
+
+  /**
+   * @description The specVersion for this signature
+   */
+  public get specVersion (): INumber {
+    return this.getT('specVersion');
+  }
+
+  /**
+   * @description The tip [[Balance]]
+   */
+  public get tip (): ICompact<INumber> {
+    return this.getT('tip');
+  }
+
+  /**
+   * @description The transactionVersion for this signature
+   */
+  public get transactionVersion (): INumber {
+    return this.getT('transactionVersion');
+  }
+
+  /**
+   * @description The (optional) asset id for this signature for chains that support transaction fees in assets
+   */
+  public get assetId (): IOption<INumber | MultiLocation> {
+    return this.getT('assetId');
+  }
+
+  /**
+   * @description The (optional) asset id for this signature for chains that support transaction fees in assets
+   */
+  public get metadataHash (): IOption<Hash> {
+    return this.getT('metadataHash');
+  }
+
+  /**
    * @description Sign the payload with the keypair
    */
-  public override sign (signerPair: IKeyringPair): Uint8Array {
+  public sign (signerPair: IKeyringPair): Uint8Array {
     // NOTE The `toU8a({ method: true })` argument is absolutely critical, we
     // don't want the method (Bytes) to have the length prefix included. This
     // means that the data-as-signed is un-decodable, but is also doesn't need
