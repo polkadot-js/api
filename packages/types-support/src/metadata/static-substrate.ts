833b8f2e2002cd06e01527128ffac207bc292a37
tarikgul <tariksnow37@gmail.com>
2024-10-03 11:59:37 -0400

    support metadata files

--- packages/types-support/src/metadata/static-substrate.ts
+++ packages/types-support/src/metadata/static-substrate.ts
@@ -1,9 +1,9 @@
 // Copyright 2017-2024 @polkadot/types-support authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import metadata from './v15/substrate-hex.js';
-import rpc from './v15/substrate-rpc.js';
-import version from './v15/substrate-ver.js';
+import metadata from './v14/substrate-hex.js';
+import rpc from './v14/substrate-rpc.js';
+import version from './v14/substrate-ver.js';
 
 export { rpc, version };
 
