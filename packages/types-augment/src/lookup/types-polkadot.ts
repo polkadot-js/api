96823b9e27313f137e0191b89f56d8ddedf2c4ae
Valentin Fernandez <tinchofernandez8@gmail.com>
2025-05-13 12:14:34 -0300

    Fix StagingXcmV5

--- packages/types-augment/src/lookup/types-polkadot.ts
+++ packages/types-augment/src/lookup/types-polkadot.ts
@@ -278,21 +278,21 @@ declare module '@polkadot/types/lookup' {
   interface StagingXcmV5Junctions extends Enum {
     readonly isHere: boolean;
     readonly isX1: boolean;
-    readonly asX1: StagingXcmV5Junction;
+    readonly asX1: Vec<StagingXcmV5Junction>;
     readonly isX2: boolean;
-    readonly asX2: StagingXcmV5Junction;
+    readonly asX2: Vec<StagingXcmV5Junction>;
     readonly isX3: boolean;
-    readonly asX3: StagingXcmV5Junction;
+    readonly asX3: Vec<StagingXcmV5Junction>;
     readonly isX4: boolean;
-    readonly asX4: StagingXcmV5Junction;
+    readonly asX4: Vec<StagingXcmV5Junction>;
     readonly isX5: boolean;
-    readonly asX5: StagingXcmV5Junction;
+    readonly asX5: Vec<StagingXcmV5Junction>;
     readonly isX6: boolean;
-    readonly asX6: StagingXcmV5Junction;
+    readonly asX6: Vec<StagingXcmV5Junction>;
     readonly isX7: boolean;
-    readonly asX7: StagingXcmV5Junction;
+    readonly asX7: Vec<StagingXcmV5Junction>;
     readonly isX8: boolean;
-    readonly asX8: StagingXcmV5Junction;
+    readonly asX8: Vec<StagingXcmV5Junction>;
     readonly type: 'Here' | 'X1' | 'X2' | 'X3' | 'X4' | 'X5' | 'X6' | 'X7' | 'X8';
   }
 
