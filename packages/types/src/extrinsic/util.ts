38b4b69d3b81ea86489fabeeb38d4108825eb8dd
zerox-toml <passionkaizen96111@gmail.com>
2025-07-07 17:28:13 +0000

    fix: Refactor signing functions to use blake2AsU8a for hashing instead of registry.hash in conditional sign of extrinsic payload

--- packages/types/src/extrinsic/util.ts
+++ packages/types/src/extrinsic/util.ts
@@ -4,18 +4,19 @@
 import type { SignOptions } from '@polkadot/keyring/types';
 import type { Registry } from '@polkadot/types-codec/types';
 import type { IKeyringPair } from '../types/index.js';
+import { blake2AsU8a } from '@polkadot/util-crypto';
 
 // a helper function for both types of payloads, Raw and metadata-known
 export function sign (registry: Registry, signerPair: IKeyringPair, u8a: Uint8Array, options?: SignOptions): Uint8Array {
   const encoded = u8a.length > 256
-    ? registry.hash(u8a)
+    ? blake2AsU8a(u8a)
     : u8a;
 
   return signerPair.sign(encoded, options);
 }
 
 export function signGeneral (registry: Registry, u8a: Uint8Array): Uint8Array {
-  const encoded = registry.hash(u8a);
+  const encoded = blake2AsU8a(u8a);
 
   return encoded;
 }
