15fc2007d636f7fee6aef32f8d6d56e0bff72265
rajk93 <raj@blockdeep.io>
2025-11-25 11:32:12 +0530

    fix: iterate through all preRuntime logs and use the first recognized engine (#6230)

--- packages/api-derive/src/type/util.ts
+++ packages/api-derive/src/type/util.ts
@@ -5,17 +5,24 @@ import type { AccountId, Digest } from '@polkadot/types/interfaces';
 
 export function extractAuthor (digest: Digest, sessionValidators: AccountId[]): AccountId | undefined {
   const [citem] = digest.logs.filter((e) => e.isConsensus);
-  const [pitem] = digest.logs.filter((e) => e.isPreRuntime);
+  const preRuntimeItems = digest.logs.filter((e) => e.isPreRuntime);
   const [sitem] = digest.logs.filter((e) => e.isSeal);
   let accountId: AccountId | undefined;
 
   try {
     // This is critical to be first for BABE (before Consensus)
     // If not first, we end up dropping the author at session-end
-    if (pitem) {
+    // Iterate through ALL preRuntime logs to find a recognized engine
+    // (parachains may have multiple preRuntime logs, e.g., CMLS + aura)
+    for (const pitem of preRuntimeItems) {
       const [engine, data] = pitem.asPreRuntime;
 
       accountId = engine.extractAuthor(data, sessionValidators);
+
+      // Stop at first recognized engine that returns an author
+      if (accountId) {
+        return accountId;
+      }
     }
 
     if (!accountId && citem) {
